68a556f3f06778e917acc57d274cbc930505ef88
张镭银 <2043869032@qq.com>
2020-06-05 16:07:04 +0800

    树

---
+++ DataStructureAndAlgorithm/twotree/tree.js
@@ -0,0 +1,64 @@
+function Node (data, left, right) {
+  this.data = data;
+  this.left = left;
+  this.right = right;
+  this.show = show;
+}
+
+//显示当前节点数据
+function show () {
+  return this.data;
+}
+
+function BST () {
+  this.root = null;;
+  this.insert = insert;
+  this.inOrder = inOrder;
+}
+
+
+function insert (data) {
+  var n = new Node(data, null, null);
+  if (this.root == null) {
+    this.root = n;
+  } else {
+    var current = this.root;
+    var parent;
+    while (true) {
+      parent = current;
+      if (data < current.data) {
+        current = current.left;
+        if (current == null) {
+          parent.left = n;
+          break;
+        }
+      }
+      else {
+        current = current.right;
+        if (current == null) {
+          parent.right = n;
+          break;
+        }
+      }
+    }
+  }
+}
+
+
+function inOrder (node) {
+  if (node != null) {
+    inOrder(node.left);
+    console.log(node.show() + " ");
+    inOrder(node.right);
+  }
+}
+
+var nums = new BST();
+nums.insert(56);
+nums.insert(22);
+nums.insert(81);
+nums.insert(10);
+nums.insert(30);
+nums.insert(77);
+nums.insert(92);
+inOrder(nums.root);
