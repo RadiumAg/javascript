725ee9629ca19827d2d0ff2c0e03c1b49c58604c
Radium <2043869032@qq.com>
2020-10-27 10:11:37 +0800

    fix(图的深度遍历和广度遍历)

---
+++ DataStructureAndAlgorithm/Graphs/dist/graphs.dev.js
@@ -0,0 +1,92 @@
+'use strict'; // 表示顶点
+
+function Vertex(label) {
+  this.label = label;
+}
+
+function addEdge(v, w) {
+  this.adj[v].push(w);
+  this.adj[w].push(v);
+  this.edges++;
+}
+
+function showGraph() {
+  for (var i = 0; i < this.vertices; ++i) {
+    var a = i + "->";
+
+    for (var j = 0; j < this.vertices; ++j) {
+      if (this.adj[i][j] != undefined) console.log(a, this.adj[i][j]);
+    }
+  }
+}
+
+function bfs(s) {
+  var queue = [];
+  this.marked[s] = true;
+  queue.push(s); // 添加到队尾
+
+  while (queue.length > 0) {
+    var v = queue.shift(); // 从队首移除
+
+    if (v == undefined) {
+      console.log("Visited vertex: " + v);
+    }
+
+    for (var w in this.adj[v]) {
+      if (!this.marked[w]) {
+        this.edgeTo[w] = v;
+        this.marked[w] = true;
+        queue.push(w);
+      }
+    }
+  }
+}
+
+function dfs(v) {
+  this.marked[v] = true;
+
+  if (this.adj[v] != undefined) {
+    console.log("Visited vertex: " + v);
+  }
+
+  for (var w in this.adj[v]) {
+    if (!this.marked[w]) {
+      this.dfs(w);
+    }
+  }
+}
+
+function Graph(v) {
+  this.vertices = v;
+  this.edges = 0;
+  this.adj = [];
+
+  for (var i = 0; i < this.vertices; i++) {
+    this.adj[i] = [];
+    this.adj[i].push("");
+  }
+
+  this.addEdge = addEdge;
+  this.showGraph = showGraph;
+  this.dfs = dfs;
+  this.bfs = bfs;
+  this.marked = [];
+
+  for (var i = 0; i < this.vertices; ++i) {
+    this.marked[i] = false;
+  }
+}
+
+var g = new Graph(5);
+g.addEdge(0, 1);
+g.addEdge(0, 2);
+g.addEdge(1, 3);
+g.addEdge(2, 4);
+g.showGraph();
+g.dfs(0);
+g.bfs(0); // 程序的输出结果为0 -> 1 2 1 -> 0 3 2 -> 0 4 3 -> 1 4 -> 2 
+// 表示顶点0有到顶点1和顶点2的边
+// 顶点1有到顶带你0和顶点3的边
+// 顶点3有到顶点1的边
+// 顶点4有到顶点的边
+// 深度优先搜索
--- DataStructureAndAlgorithm/Graphs/graphs.js
+++ DataStructureAndAlgorithm/Graphs/graphs.js
@@ -13,13 +13,45 @@ function addEdge(v, w) {
 
 function showGraph() {
     for (var i = 0; i < this.vertices; ++i) {
-        console.log(i + "->");
+        let a  = i + "->";
         for (var j = 0; j < this.vertices; ++j) {
             if (this.adj[i][j] != undefined)
-                console.log(this.adj[i][j] + '');
+                console.log(a,this.adj[i][j]);
         }
     }
-    console.log(this.adj);
+}
+
+function bfs(s) {
+    let queue = [];
+    this.marked[s] = true;
+    queue.push(s); // 添加到队尾
+    while (queue.length > 0) {
+        let v = queue.shift(); // 从队首移除
+        if (v == undefined) {
+            console.log("Visited vertex: " + v);
+        }
+
+        for (const w in this.adj[v]) {
+            if (!this.marked[w]) {
+                this.edgeTo[w] = v;
+                this.marked[w] = true;
+                queue.push(w);
+            }
+        }
+    }
+}
+
+function dfs(v) {
+    this.marked[v] = true;
+    if (this.adj[v] != undefined) {
+        console.log("Visited vertex: " + v);
+    }
+
+    for (var w in this.adj[v]) {
+        if (!this.marked[w]) {
+            this.dfs(w);
+        }
+    }
 }
 
 function Graph(v) {
@@ -30,18 +62,24 @@ function Graph(v) {
         this.adj[i] = [];
         this.adj[i].push("");
     }
-    console.log(this.adj);
     this.addEdge = addEdge;
     this.showGraph = showGraph;
+    this.dfs = dfs;
+    this.bfs = bfs;
+    this.marked = [];
+    for (var i = 0; i < this.vertices; ++i) {
+        this.marked[i] = false;
+    }
 }
 
- 
 var g = new Graph(5);
-g.addEdge(0,1);
-g.addEdge(0,2);
-g.addEdge(1,3);
-g.addEdge(2,4);
+g.addEdge(0, 1);
+g.addEdge(0, 2);
+g.addEdge(1, 3);
+g.addEdge(2, 4);
 g.showGraph();
+g.dfs(0);
+g.bfs(0);
 
 // 程序的输出结果为0 -> 1 2 1 -> 0 3 2 -> 0 4 3 -> 1 4 -> 2 
 // 表示顶点0有到顶点1和顶点2的边
@@ -51,6 +89,4 @@ g.showGraph();
 
 
 // 深度优先搜索
-
-
 
