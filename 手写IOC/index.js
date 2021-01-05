3f5eeeeff4abf9e34ff8f893017e0ac096bdeb9f
Radium <2043869032@qq.com>
2021-01-05 10:37:34 +0800

    实现一个简单的依赖注入

---
+++ 手写IOC/index.js
@@ -0,0 +1,28 @@
+const services = [
+   function A() { console.log("I'm A"); },
+   function B() { console.log("I'm B"); },
+   function C() { }
+]
+const funRegex = /^function\s*[^\(]*\(\s*([^\)]*)\)/m;
+function getDependices(fun = "") {
+    const match = fun.toString().match(funRegex)[1];
+    if (match != "") {
+        match.replace("\s", "");
+    }
+    return match.split(',');
+}
+
+
+function getServices(params) {
+    for (const key in params) {
+        params[key] = services[key];
+    }
+    return params;
+}
+
+function service(A, B) {
+    A();
+    B();
+}
+
+service.apply(this, getServices(getDependices(service)));
