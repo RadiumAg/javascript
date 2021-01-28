7466752eb9ed1623d76a77ac53b050d7b1e51ab4
Radium <2043869032@qq.com>
2021-01-28 18:36:32 +0800

    chore(增加面试题)

--- index.js
+++ index.js
@@ -22,29 +22,57 @@
 // console.log(1);
 
 
-const arr1 = [1];
-const arr2 = [1];
-const flagList = [],map = new Map(),map1 = new Map();
-let flag;
+// const arr1 = [1];
+// const arr2 = [1];
+// const flagList = [], map = new Map(), map1 = new Map();
+// let flag;
 
-arr1.forEach((value,index)=>{
-    map.set(value,value);
+// arr1.forEach((value, index) => {
+//     map.set(value, value);
+// });
+
+// arr2.forEach((value, index) => {
+//     map1.set(value, value);
+// });
+
+// Array.from(map).forEach((_value, index) => {
+//     Array.from(map1).forEach((value, index) => {
+//         if (value[0] === _value[1]) {
+//             flagList.push(true);
+//         }
+//     })
+// });
+
+// if (map1.size === flagList.length) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+// async function a2() {
+// }
+
+// async function a1() {
+//     await a2();  // then后边的事情
+// }
+
+// async function a3() {
+// }
+
+// async function a4() {
+//     await a3();  // then后边的事情
+// }
+
+
+// Promise.all([a1,a4]).then(x=>{
+//     console.log(x)
+// });
+// console.log(4);
+
+let a = [1,2,4];
+a.forEach((x,i,b)=>{
+    console.log(b);
+    b.splice(x,1);
 });
 
-arr2.forEach((value,index)=>{
-    map1.set(value,value);
-});
-
-Array.from(map).forEach((_value, index) => {
-    Array.from(map1).forEach((value, index) => {
-        if (value[0] === _value[1]) {
-            flagList.push(true);
-        }
-    })
-});
-
-if (map1.size === flagList.length) {
-    console.log(true);
-} else {
-    console.log(false);
-}
+let net = require('net')
---
+++ 面试题/第十一题/index.js
@@ -0,0 +1,20 @@
+var arr = [[1, 2, 2], [3, 4, 5, 5], [6, 7, 8, 9, [11, 12, [12, 13, [14]]]], 10];
+let set = new Set();
+
+function getArray(arr = []) {
+    arr.reduce((total, current, currentIndex, arr) => {
+        if (current instanceof Object) {
+            if (currentIndex === arr.length) {
+                reutrn;
+            }
+            getArray(current);
+        } else {
+            set.add(current);
+        }
+    },0);
+}
+
+getArray(arr);
+let result =  Array.of(set).sort( (x,y)=> x - y ).map(x=>x);
+console.log(set);
+// arr.toString().split(",").sort((a,b)=>{ return a-b})
