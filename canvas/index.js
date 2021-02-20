f7f628e831213ed3617136ac090350e4a53f1130
Radium <2043869032@qq.com>
2021-02-20 18:35:21 +0800

    增加tslint  配置

--- canvas/index.js
+++ canvas/index.js
@@ -1,28 +1,28 @@
-'use strict'
-const net = require('nodejs-websocket')
-const connList = []// 连接的列表
+'use strict';
+const net = require('nodejs-websocket');
+const connList = [];// 连接的列表
 const server = net.createServer((connection) => {
-  connList.push(connection)
-  console.log(connList)
+  connList.push(connection);
+  console.log(connList);
   connection.on('text', (data) => {
     if (connList[1]) {
-      connList[1].sendText(data)
+      connList[1].sendText(data);
     }
-  })
+  });
 
   connection.on('close', () => {
-    console.log('链接断开')
-  })
+    console.log('链接断开');
+  });
 
   connection.on('error', (code) => {
-    console.log(code)
-  })
+    console.log(code);
+  });
 
   connection.on('connect', (socket) => {
-    console.log(connList)
-  })
-})
+    console.log(connList);
+  });
+});
 
 server.listen(8124, () => {
-  console.log('server bound')
-})
+  console.log('server bound');
+});
