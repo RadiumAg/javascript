65a97cbe7022f2e4049cdbfec7142a07ae0732b0
Radium <2043869032@qq.com>
2021-03-05 11:12:26 +0800

    tijiao

--- 测试框架/Mocha/demo05/promise.test.js
+++ 测试框架/Mocha/demo05/promise.test.js
@@ -1,12 +1,12 @@
-var fetch = require('node-fetch');
-var expect = require('chai').expect;
+const fetch = require('node-fetch');
+const expect = require('chai').expect;
 
-describe('promise.test.js - 异步测试', function() {
-  it('异步请求应该返回一个对象', function() {
+describe('promise.test.js - 异步测试', function () {
+  it('异步请求应该返回一个对象', function () {
     return fetch('https://api.github.com')
-      .then(function(res) {
+      .then(function (res) {
         return res.json();
-      }).then(function(json) {
+      }).then(function (json) {
         expect(json).to.be.an('object');
       });
   });
