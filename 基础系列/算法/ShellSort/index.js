a59e71583177e3fdb97382644f7f4a3a9a4e77c5
Radium <2043869032@qq.com>
2021-05-11 17:20:56 +0800

    fixed 增加内存泄漏

--- 基础系列/算法/ShellSort/index.js
+++ 基础系列/算法/ShellSort/index.js
@@ -1,29 +1,26 @@
-function shellSort(array = []) {
-    let gap = array.length / 2;
-    for (let i = 1; gap >= i; gap /= 2) {
-        for (let j = 0; j < array.length; j++) {
-            let d = j;
-            // 步长数组插入排序
-            while (d > 0 && d-gap >= 0) {
-                if (array[d] < array[d - gap]) {
-                    swap(array, d, gap);
-                }
-                d -= gap;
-            }
+function shellSort (array = []) {
+  let gap = array.length / 2;
+  for (let i = 1; gap >= i; gap /= 2) {
+    for (let j = 0; j < array.length; j++) {
+      let d = j;
+      // 步长数组插入排序
+      while (d > 0 && d - gap >= 0) {
+        if (array[d] < array[d - gap]) {
+          swap(array, d, gap);
         }
-    };
+        d -= gap;
+      }
+    }
+  };
 }
 
-let a = [7, 6, 9, 3, 1, 5, 2, 4];
+const a = [7, 6, 9, 3, 1, 5, 2, 4];
 shellSort(a);
 console.log(a);
 
-
-
 // 交换
-function swap(array, d, gap) {
-    let temp = array[d];
-    array[d] = array[d - gap];
-    array[d - gap] = temp;
+function swap (array, d, gap) {
+  const temp = array[d];
+  array[d] = array[d - gap];
+  array[d - gap] = temp;
 }
-
