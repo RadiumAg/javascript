d67e7b9cad927ec1f61c46c16c787ae086fd912a
Radium <2043869032@qq.com>
2021-07-14 18:31:37 +0800

    commit

--- 力扣/567.js
+++ 力扣/567.js
@@ -3,19 +3,19 @@
  * @param {string} s2
  * @return {boolean}
  */
-var checkInclusion = function (s1 = '', s2 = '') {
-    let length = 0;
-    // Array.from(s1).forEach((value, index) => {
-    //     Array.from(s2).forEach((v, index) => {
-    //         if (value === v) length++;
-    //     });
-    // });
+const checkInclusion = function (s1 = '', s2 = '') {
+  let length = 0;
+  // Array.from(s1).forEach((value, index) => {
+  //     Array.from(s2).forEach((v, index) => {
+  //         if (value === v) length++;
+  //     });
+  // });
 
-    for (const value of s1) {
-        for (const v of s2) {
-            if (value === v) length++;
-        }
+  for (const value of s1) {
+    for (const v of s2) {
+      if (value === v) length++;
     }
-    if (length === s1.length) return true;
-    else return false;
+  }
+  if (length === s1.length) return true;
+  else return false;
 };
