504ed056ab02c72cc86e7def53749eb8fe43b314
Radium <2043869032@qq.com>
2021-07-20 14:12:31 +0800

    commit

---
+++ 通读系列/ECMAScript 6 入门/class的继承/index.js
@@ -0,0 +1,82 @@
+
+/**
+ * super表示父类的constructor
+ */
+(() => {
+  class Point {}
+  class ColorPoint extends Point {
+    constructor (x, y, color) {
+      super(x, y); //
+      this.color = color;
+    }
+
+    toString () {
+      return this.color + ' ' + super.toString(); // 调用父类toString()
+    }
+  }
+})();
+
+/**
+ * 只有调用super之后，才可以使用this关键字
+ */
+(() => {
+  class Point {
+    constructor (x, y) {
+      this.x = x;
+      this.y = y;
+    }
+  }
+  class ColorPoint extends Point {
+    constructor (x, y, color) {
+      this.color = color;
+      super(x, y); //
+      this.color = color;
+    }
+
+    toString () {
+      return this.color + ' ' + super.toString(); // 调用父类toString()
+    }
+  }
+})();
+
+/**
+   * 只有调用super之后，才可以使用this关键字
+*/
+(() => {
+  class Point {
+    constructor (x, y) {
+      this.x = x;
+      this.y = y;
+    }
+  }
+  class ColorPoint extends Point {
+    constructor (x, y, color) {
+      this.color = color;
+      super(x, y); //
+      this.color = color;
+    }
+
+    toString () {
+      return this.color + ' ' + super.toString(); // 调用父类toString()
+    }
+  }
+})();
+
+/**
+   *  super既可以当函数使用，也可以当对象使用,但是定义在父类实例上的方法或属性，是无法通过super调用的。
+*/
+(() => {
+  class A {
+    P () {
+      return 2;
+    }
+  }
+  class B extends A {
+    constructor () {
+      super();
+      console.log(super.P());
+    }
+  }
+
+  console.log(new B());
+})();
