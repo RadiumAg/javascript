ef62b38cfad2d1b78b87e048da3b22acd2f01ae5
Radium <2043869032@qq.com>
2021-08-04 18:30:29 +0800

    commit

---
+++ 基础系列/现代javaScript教程/toPrimitive/index.js
@@ -0,0 +1,19 @@
+(() => {
+  /**
+   *  a 为对象时，先调用`Symbol.toPrimitive`,无法得到原始值，则调用valueOf,如果无法无法得到原始值，则调用toString()
+   *  hint为"number"|"string"|"default" 表示原始值的预期类型
+   */
+  const a = {
+    toString () {
+      return 2;
+    },
+    valueOf () {
+      return 1;
+    },
+    [Symbol.toPrimitive] (hint) {
+      console.log(hint);
+      return 3;
+    }
+  };
+  console.log(a + 1);
+})();
