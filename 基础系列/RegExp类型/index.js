92186e38d115794c3d51cf0d4f08546adc823441
zly <2043869032@qq.com>
2021-09-08 22:58:32 +0800

    commit

--- 基础系列/RegExp类型/index.js
+++ 基础系列/RegExp类型/index.js
@@ -18,3 +18,46 @@ const pattern2 = /\[bc\]at/i;
 (() => {
   const c = new RegExp('\\[bc\\]at', 'gi');
 })();
+
+// 现代javascript教程
+(() => {
+  // 构造函数
+  const regexp1 = new RegExp('pattern', 'g');
+  const regexp2 = /pattern/;
+  const regexp3 = /pattern/gim;
+})();
+
+// 用法
+(() => {
+  const str = 'I love JavaScript!'; // 将在这里搜索
+  const regexp = /love/;
+  console.log(str.search(regexp));
+})();
+
+// or:选择符号并非在字符集别生效，而是在表达式级别
+(() => {
+  const reg = /html|php|css|java(script)?/gi;
+  const str = 'First HTML appeared, then CSS, then JavaScript';
+  console.log(str.match(reg));
+})();
+
+(() => {
+  const reg = /".+"/g;
+  const str = 'a "witch" and her "broom" is one';
+  console.log(str.match(reg));
+
+  // 懒惰搜索
+})();
+
+(() => {
+  // const reg = /".+"/g;
+  // const str = 'a "witch" and her "broom" is one';
+  // console.log(str.match(reg));
+  // 懒惰搜索
+  const reg = /".+?"/g;
+  const str = 'a "witch" and her "broom" is one';
+  console.log(str.match(reg)); // witch, broom
+})();
+
+// 前瞻断言
+(() => {})();
---
+++ 通读系列/javascript权威指南/表达式/readme.md
@@ -0,0 +1,60 @@
+# 表达式
+
+#### 定义：
+
+JavaScript中的一个短语，JavaScript会将其计算出一个结果。
+
+##### 举例：
+
+1. 常量
+2. 变量名
+3. 复杂表达式
+
+#### 原始表达式
+
+##### 定义
+
+1. 常量,直接量
+
+   1.23  "hello"
+
+2. 关键字
+
+   true,false,null
+
+3. 变量
+
+   i  sum undefined(undefined是全局变量，和null不同，它不是一个关键字)
+
+#### 对象和数组的初始化表达式
+
+##### 定义
+
+数组：
+
+`[ ]    [1,2,3,4]`
+
+对象：
+
+`let  a = { x:2,y:-1 }`
+
+
+
+#### 函数定义表达式
+
+```javascript
+const a = function(x) {}
+```
+
+
+
+#### 属性访问表达式
+
+```javascript
+expression.identifier
+expression [ expression ]
+```
+
+
+
+#### 调用表达式
