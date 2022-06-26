e6fbb5f794e7f99e095cec6b5c1b49d090a06d00
Radium <2043869032@qq.com>
2022-06-27 00:39:36 +0800

    commit

--- 基础系列/原型链/index.js
+++ 基础系列/原型链/index.js
@@ -1,4 +1,3 @@
-
 (() => {
   function Cat () {
     this.name = '猫';
@@ -33,7 +32,7 @@
   };
 
   const instance = new SubType();
-  console.log(instance.getSuperValue());
+  // console.log(instance.getSuperValue());
 })();
 
 // 借用构造函数
@@ -48,14 +47,42 @@
 
   const instance1 = new SubType();
   instance1.colors.push('black');
-  console.log(instance1.colors);
+  // console.log(instance1.colors);
   const instance2 = new SubType();
   instance2.colors.push('white');
-  console.log(instance2.colors);
+  // console.log(instance2.colors);
 })();
 
 // 组合继承
 
 (() => {
-  function Supertype () {}
+  function Supertype (name) {
+    this.name = name;
+    this.colors = ['red', 'blue', 'green'];
+  }
+
+  Supertype.prototype.sayName = function () {
+    console.log(this);
+  };
+
+  function SubType (name, age) {
+    Supertype.call(this, name);
+    this.age = age;
+  }
+
+  SubType.prototype = new Supertype();
+  SubType.prototype.sayAge = function () {
+    console.log(this);
+  };
+
+  const instance1 = new SubType('Nicholas', 29);
+  instance1.colors.push('black');
+  console.log(instance1.colors);
+  instance1.sayName();
+  instance1.sayAge();
+
+  // const instance2 = new SubType('Greg', 27);
+  // console.log(instance2.colors);
+  // instance2.sayName();
+  // instance1.sayAge();
 })();
