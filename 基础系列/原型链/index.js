14b1eed08aff83a60ab79fa48c3bdfec909f651c
Radium <2043869032@qq.com>
2022-06-28 00:30:48 +0800

    commit

--- 基础系列/原型链/index.js
+++ 基础系列/原型链/index.js
@@ -86,3 +86,25 @@
   // instance2.sayName();
   // instance1.sayAge();
 })();
+
+(() => {
+  function object (o) {
+    function F () {}
+    F.prototype = o;
+    return new F();
+  }
+
+  const person = {
+    name: 'Nicholas',
+    friends: ['Shelby', 'Count', 'Van']
+  };
+
+  const anotherPerson = object(person);
+  anotherPerson.name = 'Greg';
+  anotherPerson.friends.push('Rob');
+
+  const yetAnotherPerson = object(person);
+  yetAnotherPerson.name = 'Linda';
+  yetAnotherPerson.friends.push('Barbie');
+  console.log(person.friends);
+})();
