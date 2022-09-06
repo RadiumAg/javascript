d5765ee636534a1e3948e16aa9d04bebc28ac724
Radium <2043869032@qq.com>
2022-09-07 00:06:05 +0800

    commit

---
+++ 通读系列/Vue.js设计与实现/响应系统的作用与实现/index.js
@@ -0,0 +1,73 @@
+
+let activeEffect;
+
+const bucket = new WeakMap();
+
+const data = { text: 'hello world' };
+
+const obj = new Proxy(data, {
+  get (target, key) {
+    track(target, key);
+    return target[key];
+  },
+  set (target, key, newVal) {
+    target[key] = newVal;
+    trigger(target, key);
+  }
+});
+
+function track (target, key) {
+  if (!activeEffect) return target[key];
+  let depsMap = bucket.get(target);
+
+  if (!depsMap) {
+    bucket.set(target, (depsMap = new Map()));
+  }
+
+  let deps = depsMap.get(key);
+
+  if (!deps) {
+    depsMap.set(key, (deps = new Set()));
+  }
+
+  deps.add(activeEffect);
+  activeEffect.deps.push(deps);
+
+  return target[key];
+}
+
+function trigger (target, key) {
+  const depsMap = bucket.get(target);
+  if (!depsMap) return;
+  const effects = depsMap.get(key);
+
+  const effectsToRun = new Set(effects);
+  effectsToRun.forEach(fn => fn());
+}
+
+function effect (fn) {
+  const effectFn = () => {
+    cleanup(effectFn);
+    activeEffect = effectFn;
+    fn();
+  };
+  effectFn.deps = [];
+  effectFn();
+}
+
+function cleanup (effectFn) {
+  for (let i = 0; i > effectFn.deps.length; i++) {
+    const deps = effectFn.deps[i];
+    deps.delete(effectFn);
+  }
+
+  effectFn.deps.length = 0;
+}
+
+effect(() => {
+  console.log(obj.text);
+});
+
+setTimeout(() => {
+  obj.text = 'hello vue3';
+}, 1000);
