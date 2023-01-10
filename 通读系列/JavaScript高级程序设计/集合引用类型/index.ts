ab9195b333d22114faa65a6284524e5147235be9
Radium <2043869032@qq.com>
2023-01-10 22:26:49 +0800

    chore: commit

--- 通读系列/JavaScript高级程序设计/集合引用类型/index.ts
+++ 通读系列/JavaScript高级程序设计/集合引用类型/index.ts
@@ -3,3 +3,41 @@ zeroes.fill(5);
 zeroes.fill(0);
 zeroes.fill(7, 1, 3);
 console.log(zeroes);
+
+// copyWithin
+let inits: number[] = [],
+  reset = () => (inits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+reset();
+
+inits.copyWithin(4, 0, 3);
+console.log(inits);
+
+const person1 = {
+  toLocaleString() {
+    return 'Nikolaos';
+  },
+  toString() {
+    return 'Nicholas';
+  },
+};
+
+const person2 = {
+  toLocaleString() {
+    return 'Grigorios';
+  },
+  toString() {
+    return 'Greg';
+  },
+};
+
+const people = [person1, person2];
+console.log(people);
+
+console.log(people.toString()); //都以逗号分隔
+console.log(people.toLocaleString()); //都以逗号分隔
+
+// conact
+const colors = ['red', 'green', 'blue'];
+const colors2 = colors.concat('yellow', ['black', 'brown']);
+console.log(colors2);
