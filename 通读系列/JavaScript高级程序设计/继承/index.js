0869d1cb19bd1b584ea72e02c1c30f725bd8545f
Radium <2043869032@qq.com>
2023-01-21 23:10:51 +0800

    chore: commit

--- 通读系列/JavaScript高级程序设计/继承/index.js
+++ 通读系列/JavaScript高级程序设计/继承/index.js
@@ -232,3 +232,19 @@
     }
   }
 };
+
+// new
+() => {
+  class Vehicle {
+    constructor() {
+      console.log(new.target);
+      if (new.target === Vehicle) {
+        throw new Error('Vehicle cannot be directly instantiated');
+      }
+    }
+  }
+
+  class Bus extends Vehicle {}
+  new Bus();
+  new Vehicle();
+};
