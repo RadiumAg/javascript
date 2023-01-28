0b9be2704f43bc6269ed04ff00f76dfcb7194c01
2043869032@qq.com <2043869032@qq.com>
2023-01-28 10:51:38 +0800

    chore: commit

--- 通读系列/JavaScript高级程序设计/Promise/index.js
+++ 通读系列/JavaScript高级程序设计/Promise/index.js
@@ -1,3 +1,5 @@
+const { readSync } = require('fs');
+
 // 同步/异步执行的二元性
 () => {
   try {
@@ -70,3 +72,38 @@
   p1.finally(() => setTimeout(console.log, 0, 7));
   p1.finally(() => setTimeout(console.log, 0, 8));
 };
+
+(() => {
+  class TrackablePromise extends Promise {
+    constructor(executor) {
+      const notifyHandlers = [];
+      super((resolve, reject) => {
+        return executor(resolve, reject, status => {
+          notifyHandlers.map(handler => handler(status));
+        });
+      });
+      this.notifyHandlers = notifyHandlers;
+    }
+
+    notify(notifyHandler) {
+      this.notifyHandlers.push(notifyHandler);
+      return this;
+    }
+  }
+
+  const p = new TrackablePromise((resolve, reject, notify) => {
+    function countdown(x) {
+      if (x > 0) {
+        notify(`${20 * x}% remaining`);
+        setTimeout(() => countdown(x - 1), 1000);
+      } else {
+        resolve();
+      }
+    }
+
+    countdown(5);
+  });
+
+  p.notify(x => setTimeout(console.log, 0, 'progress:', x));
+  p.then(() => setTimeout(console.log, 0, 'completed'));
+})();
