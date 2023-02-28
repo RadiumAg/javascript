c77233229aa832914291bae97a878481e62d9235
Radium <2043869032@qq.com>
2023-02-28 23:12:13 +0800

    chore: commit

--- 通读系列/Vue.js设计与实现/纯js/effect.js
+++ 通读系列/Vue.js设计与实现/纯js/effect.js
@@ -1,18 +1,50 @@
-const bucket = new Set();
+const bucket = new WeakMap();
 
 const data = { text: 'hellow world' };
 
-function effect() {
-  document.body.textContent = obj.text;
+let activeEffect;
+function effect(fn) {
+  activeEffect = fn;
+  fn();
 }
 
 const obj = new Proxy(data, {
   get(target, key) {
-    bucket.add(effect);
-    return target[key];
+    track(target, key);
   },
   set(target, key, newVal) {
     target[key] = newVal;
-    bucket.forEach(fn => fn());
+    trigger(target, key);
   },
 });
+
+effect(() => {
+  document.body.textContent = obj.text;
+});
+
+function track(target, key) {
+  if (!activeEffect) return target[key];
+
+  let depsMap = bucket.get(target);
+
+  if (!depsMap) {
+    bucket.set(target, (depsMap = new Map()));
+  }
+
+  let deps = depsMap.get(key);
+
+  if (!deps) {
+    depsMap.set(key, (deps = new Set()));
+  }
+
+  deps.add(activeEffect);
+  return target[key];
+}
+
+function trigger(target, key) {
+  const depsMap = bucket.get(target);
+  if (!depsMap) return;
+
+  const effects = depsMap.get(key);
+  effects && effects.forEach(fn => fn());
+}
