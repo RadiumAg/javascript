3dffdc316d7be5ea9cc796308151df79fe2e222e
2043869032@qq.com <2043869032@qq.com>
2023-03-01 11:50:01 +0800

    chore: commit

--- 通读系列/Vue.js设计与实现/纯js/effect.js
+++ 通读系列/Vue.js设计与实现/纯js/effect.js
@@ -1,31 +1,74 @@
-const bucket = new WeakMap();
+const { dir } = require('console');
 
-const data = { text: 'hellow world' };
+const bucket = new WeakMap();
 
 let activeEffect;
 
-function effect(fn) {
-  const effectFn = () => {
-    cleanup(effectFn);
-    activeEffect = fn;
-    fn();
-  };
+const effectStack = [];
+const jobQueue = new Set();
+const p = Promise.resolve();
+let isFlushing = false;
 
-  effectFn.deps = [];
-  effectFn();
+function flushJob() {
+  if (isFlushing) return;
+  isFlushing = true;
+  p.then(() => {
+    jobQueue.forEach(job => job());
+  }).finally(() => {
+    isFlushing = false;
+  });
 }
 
-const obj = new Proxy(data, {
-  get(target, key) {
-    track(target, key);
-  },
-  set(target, key, newVal) {
-    target[key] = newVal;
-    trigger(target, key);
+function effect(fn, options = {}) {
+  const effectFn = () => {
+    cleanup(effectFn);
+    activeEffect = effectFn;
 
-    return true;
-  },
-});
+    effectStack.push(effectFn);
+    const res = fn();
+    // 当前副作用函数执行完毕后，将当前副作用函数弹出栈，并把activeEffect还原为之前的值
+    effectStack.pop();
+
+    activeEffect = effectStack.at(-1);
+    return res;
+  };
+
+  // 将options 挂载倒 effectFn 上
+  effectFn.options = options;
+  effectFn.deps = [];
+
+  if (!options.lazy) {
+    effectFn();
+  }
+  return effectFn;
+}
+
+function computed(getter) {
+  let value;
+  let dirty = true;
+
+  const effectFn = effect(getter, {
+    lazy: true,
+    scheduler() {
+      dirty = true;
+      trigger(obj, 'value');
+    },
+  });
+
+  const obj = {
+    get value() {
+      if (dirty) {
+        value = effectFn();
+        dirty = false;
+      }
+
+      track(obj, 'value');
+      return value;
+    },
+  };
+
+  return obj;
+}
 
 // 收集依赖
 function track(target, key) {
@@ -53,8 +96,23 @@ function trigger(target, key) {
   if (!depsMap) return;
 
   const effects = depsMap.get(key);
-  const effectsToRun = new Set(effects);
-  effectsToRun.forEach(effectFn => effectFn());
+  const effectsToRun = new Set();
+
+  effects &&
+    effects.forEach(effectFn => {
+      if (effectFn !== activeEffect) {
+        effectsToRun.add(effectFn);
+      }
+    });
+
+  effectsToRun.forEach(effectFn => {
+    // 如果trigger触发执行的副作用函数与当前正在执行的副作用函数相同，则不触发执行
+    if (effectFn.options.scheduler) {
+      effectFn.options.scheduler(effectFn);
+    } else {
+      effectFn();
+    }
+  });
 }
 
 function cleanup(effectFn) {
@@ -67,9 +125,32 @@ function cleanup(effectFn) {
   effectFn.deps.length = 0;
 }
 
-effect(() => {
-  document.body.textContent = obj.ok ? obj.text : 'not';
+const data = { foo: 1 };
+
+const obj = new Proxy(data, {
+  get(target, key) {
+    return track(target, key);
+  },
+  set(target, key, newVal) {
+    target[key] = newVal;
+    trigger(target, key);
+
+    return true;
+  },
 });
 
-obj.ok = false;
-obj.text = 12;
+effect(
+  () => {
+    console.log(obj.foo);
+  },
+  {
+    scheduler(fn) {
+      jobQueue.add(fn);
+      flushJob();
+    },
+  },
+);
+
+obj.foo++; // 2
+obj.foo++; // 3
+console.log('结束了');
