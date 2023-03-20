f51ae66f9bef3adf0e1a430dc81139361a0f78fd
Radium <2043869032@qq.com>
2023-03-20 23:26:02 +0800

    chore: commit

--- 通读系列/Vue.js设计与实现/纯js/html-translate.js
+++ 通读系列/Vue.js设计与实现/纯js/html-translate.js
@@ -20,20 +20,58 @@ function parse(str) {
 }
 
 function parseChildren(context, ancetors) {
-  const nodes = {};
+  const nodes = [];
 
   const { mode, source } = context;
 
   while (!isEnd(context, ancetors)) {
-    if (
-      (mode === TextModes.DATA || mode === TextModes.RCDATA) &&
-      mode === TextModes.DATA &&
-      source[0] === '<' &&
-      source[1] === '!' &&
-      source[1] === '!' &&
-      source.startsWith('<![CDATA[')
-    ) {
-      node = parseCDATA(context, ancetors);
+    let node;
+
+    if (mode === TextModes.DATA || mode === TextModes.RCDATA) {
+      if (mode === TextModes.DATA && source[0] === '<') {
+        if (source[1] === '!') {
+          if (source.startsWith('<!--')) {
+            node = parseComment(context);
+          } else if (source.startsWith('<![CDATA[')) {
+            node = parseCDATA(context, ancetors);
+          }
+        } else if (source[1] === '/') {
+          console.error('无效的结束标签');
+          continue;
+        } else if (/[a-z]/i.test(source[1])) {
+          node = parseElement(context, ancetors);
+        }
+      } else if (source.startsWith('{{')) {
+        node = parseInterpolation(context);
+      }
     }
+
+    if (!node) {
+      node = parseText(context);
+    }
+    nodes.push(node);
   }
+
+  return nodes;
+}
+
+function parseElement() {
+  const element = parseTag();
+  element.children = parseChildren();
+  parseEndTag();
+
+  return element;
+}
+
+function isEnd(context, ancetors) {
+  if (!context.source) return true;
+  const parent = ancetors[ancetors.length - 1];
+  if (parent && context.source.startsWith(`<${parent.tag}`)) {
+    return true;
+  }
+}
+
+function parseTag(context, type = 'start') {
+  const { advanceBy, advanceSpaces } = context;
+  const match = type === 'start' ? /^([a-z])/;
 }
