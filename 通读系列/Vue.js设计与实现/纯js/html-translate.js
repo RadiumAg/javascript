8a701cfcbe380f9706e022a65b8c9a01226429ee
Radium <2043869032@qq.com>
2023-03-24 23:58:06 +0800

    chore: commit

--- 通读系列/Vue.js设计与实现/纯js/html-translate.js
+++ 通读系列/Vue.js设计与实现/纯js/html-translate.js
@@ -12,6 +12,35 @@ const namedCharacterReferences = {
   'lt;': '<',
   'ltcc;': '⪦',
 };
+const CCR_REPLACEMENTS = {
+  0x80: 0x20ac,
+  0x82: 0x201a,
+  0x83: 0x0192,
+  0x84: 0x201e,
+  0x85: 0x2026,
+  0x86: 0x2020,
+  0x87: 0x2021,
+  0x88: 0x02c6,
+  0x89: 0x2030,
+  0x8a: 0x0160,
+  0x8b: 0x2039,
+  0x8c: 0x0152,
+  0x8e: 0x017d,
+  0x91: 0x2018,
+  0x92: 0x2019,
+  0x93: 0x201c,
+  0x94: 0x201d,
+  0x95: 0x2022,
+  0x96: 0x2013,
+  0x97: 0x2014,
+  0x98: 0x02dc,
+  0x99: 0x2122,
+  0x9a: 0x0161,
+  0x9b: 0x203a,
+  0x9c: 0x0153,
+  0x9e: 0x017e,
+  0x9f: 0x0178,
+};
 
 function parse(str) {
   const context = {
@@ -181,7 +210,7 @@ function parseText(context) {
 
   return {
     type: 'Text',
-    content,
+    content: decodeHtml(content),
   };
 }
 
@@ -220,7 +249,7 @@ function decodeHtml(rawText, asAttr = false) {
 
     // 如果满足条件，则说明是命名字符引用，否则为数字字符引用
     if (head[0] === '&') {
-      const name = '';
+      let name = '';
       let value;
 
       if (
@@ -237,8 +266,36 @@ function decodeHtml(rawText, asAttr = false) {
         name = rawText.substr(1, length);
         value = namedCharacterReferences[name];
       }
+
+      // 如果找到了对应项的值，说明解码成功
+      if (value) {
+        // 检查实体名称的最后一个匹配字符是否是分号
+        const semi = name.endsWith(';');
+
+        if (
+          (asAttr && !semi && /=z-z0-9/i.test(rawText[name.length + 1])) ||
+          ''
+        ) {
+          decodedText += `&${name}`;
+          advance(1 + name.length);
+        } else {
+          // 其它情况下，正常使用解码后的内容拼接到 decodeText 上
+          decodedText += value;
+          advance(1 + name.length);
+        }
+      } else {
+        // 如果没有找到对应的值，说明解码失败
+        decodedText += `&${name}`;
+        advance(1 + name.length);
+      }
+    } else {
+      // 如果字符 & 的下一个字符不是 ASCII 字母或数字，则字符 & 作为普通文本
+      decodedText += '&';
+      advance(1);
     }
   }
+
+  return decodedText;
 }
 
 export { parse };
