99c13c41109915a71d44fa82b7e94174a2bf98a0
Radium <2043869032@qq.com>
2023-05-02 14:04:11 +0800

    chore: commit

--- 通读系列/ECMAScript 6 入门/Promise对象/index.ts
+++ 通读系列/ECMAScript 6 入门/Promise对象/index.ts
@@ -39,3 +39,63 @@
     console.log(error);
   });
 })();
+
+// Promise.resolve 产生现有的fullfill Promise 对象
+
+(() => {
+  Promise.resolve('foo');
+  new Promise(resolve => resolve('foo'));
+  // 两个等价
+})();
+
+// generator 函数与 Promise的结合
+(() => {
+  function getFoo() {
+    return new Promise((resolve, reject) => {
+      resolve('foo');
+    });
+  }
+
+  const g = function* () {
+    try {
+      const foo = yield getFoo();
+      console.log(foo);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  function run(generator) {
+    const it = generator();
+
+    function go(result) {
+      if (result.done) return result.value;
+
+      return result.value.then(
+        value => {
+          return go(it.next(value));
+        },
+        error => {
+          return go(it.throw(error));
+        },
+      );
+    }
+
+    go(it.next());
+  }
+
+  run(g);
+})();
+
+// Generator yield表示暂停的地方
+// 向Generator函数体内输入数据
+(() => {
+  function* gen(x) {
+    const y = yield x + 2; // y 会变成输入的2
+    return y;
+  }
+
+  const g = gen(1);
+  g.next();
+  g.next(2);
+})();
