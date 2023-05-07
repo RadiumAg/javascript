439a2338ca63496f52974ff9ec67f96fe89e78e9
Radium <2043869032@qq.com>
2023-05-07 21:45:00 +0800

    chore: commit

--- 通读系列/ECMAScript 6 入门/Symbol/index.js
+++ 通读系列/ECMAScript 6 入门/Symbol/index.js
@@ -26,3 +26,41 @@
 
   getArea(shapeType.triangle, { width: 100, height: 100 });
 })();
+
+// 属性名的遍历
+// Object.getOwnPropertySymbols返回一个数组，成员是当前对象的所有用作属性名的Symbol值
+(() => {
+  const obj = {};
+  const a = Symbol('a');
+  const b = Symbol.for('b');
+
+  obj[a] = 'Hello';
+  obj[b] = 'World';
+
+  const objectSymbols = Object.getOwnPropertySymbols(obj);
+
+  console.log(objectSymbols); //[ Symbol(a), Symbol(b) ]
+  console.log(Object.getOwnPropertyNames(obj));
+})();
+
+// Reflect.ownKeys方法可以返回所有类型的键名
+(() => {
+  const obj = {
+    [Symbol('my_key')]: 1,
+    enum: 2,
+    nonEnum: 3,
+  };
+
+  console.log(Reflect.ownKeys(obj));
+})();
+
+// symbol.for('') 注册同一个symbol值
+(() => {
+  Symbol.for('bar') === Symbol.for('bar');
+})();
+
+// symbol.keyFor,返回一个已登记的Symbol类型值的key
+(() => {
+  const s1 = Symbol.for('foo');
+  Symbol.keyFor(s1); //"foo"
+})();
