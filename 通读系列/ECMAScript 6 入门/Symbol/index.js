662c128d0f4f6b89ed7c857c256e2dd78b646ae5
Radium <2043869032@qq.com>
2023-05-07 21:31:57 +0800

    chore: commit

--- 通读系列/ECMAScript 6 入门/Symbol/index.js
+++ 通读系列/ECMAScript 6 入门/Symbol/index.js
@@ -8,5 +8,21 @@
 
 // 消除魔术字符串
 (() => {
-  function getArea(shape) {}
+  const shapeType = {
+    triangle: Symbol(),
+  };
+
+  function getArea(shape, options) {
+    let area = 0;
+
+    switch (shape) {
+      case shapeType.triangle:
+        area = 0.5 * options.width * options.height;
+        break;
+    }
+
+    return area;
+  }
+
+  getArea(shapeType.triangle, { width: 100, height: 100 });
 })();
