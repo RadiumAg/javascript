a0ac63a03fa90c3aec3814ac9fb825e991c31606
Radium <2043869032@qq.com>
2023-05-08 22:32:41 +0800

    chore: commit

--- 通读系列/ECMAScript 6 入门/变量的解构赋值/index.js
+++ 通读系列/ECMAScript 6 入门/变量的解构赋值/index.js
@@ -17,3 +17,37 @@
   const [x1, y1, ...z1] = ['a'];
   console.log(x1, y1, z1);
 })();
+
+// 实际上具备iterator接口也可以
+(() => {
+  const [x, y, z] = new Set(['a', 'b', 'c']);
+
+  console.log(x, y, z);
+
+  // 极简斐波那契
+  function* fibs() {
+    let a = 0;
+    let b = 1;
+
+    while (true) {
+      yield a;
+      [a, b] = [b, a + b];
+    }
+  }
+
+  const [first, second, third, fouth] = fibs();
+  console.log(first, second, third, fouth);
+})();
+
+// 解构赋值可以引用其它变量，但该变量必须已经声明
+(() => {
+  const [x = 1, y = x] = [];
+  console.log(x, y);
+})();
+
+// 对象的解构赋值
+(() => {
+  const { foo, bar } = { foo: 'aaa', bar: 'bbb' };
+  console.log(foo);
+  console.log(bar);
+})();
