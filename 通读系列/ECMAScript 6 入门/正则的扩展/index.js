ff64eb33cca7fe68ef8b9ff663cd97e8b2a5ba3e
Radium <2043869032@qq.com>
2023-05-10 21:47:57 +0800

    chore: commit

--- 通读系列/ECMAScript 6 入门/正则的扩展/index.js
+++ 通读系列/ECMAScript 6 入门/正则的扩展/index.js
@@ -1,2 +1,35 @@
 //ES6 允许 RegExp构造函数接受正则表达式作为参数
-(() => {})();
+(() => {
+  const regex = new RegExp(/xyz/i);
+})();
+
+// u修饰符,ES6 添加了 u修饰符，含义为'Unicode模式'
+(() => {
+  // eslint-disable-next-line prettier/prettier, unicorn/prefer-string-starts-ends-with
+    /^\uD83D/u.test('\uD83D\uDC2A');
+
+  // eslint-disable-next-line prettier/prettier, unicorn/prefer-string-starts-ends-with
+    /^\uD83D/.test('\uD83D\uDC2A')
+})();
+
+// u修饰符会影响 点字符
+(() => {
+  const s = '\u2222';
+  /^.$/.test(s);
+  /^.$/u.test(s);
+})();
+
+// Unicode字符表示法
+(() => {
+  /u{61}/.test('a');
+  /u{61}/u.test('a');
+  /\u{20BB7}/u.test('');
+})();
+
+// 量词
+(() => {
+  /a{2}/.test('aa'); //true
+  /a{2}/.test('aa'); // true
+  /𠮷{2}/.test('𠮷𠮷'); //false
+  /𠮷{2}/u.test('𠮷𠮷'); // true
+})();
