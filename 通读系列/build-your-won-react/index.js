006fa77891fadcab59a867eb800ea5f67d5e1813
Radium <2043869032@qq.com>
2023-05-14 21:32:56 +0800

    chore: commit

--- 通读系列/build-your-won-react/index.js
+++ 通读系列/build-your-won-react/index.js
@@ -1,19 +1,90 @@
-const element = {
-  type: 'h1',
-  props: {
-    title: 'foo',
-    children: 'Hello',
-  },
+let nextUnitOfWork = null;
+
+function workLoop(deadline) {
+  const shouldYield = false;
+
+  while (nextUnitOfWork && !shouldYield) {
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+    shouldYield = deadline.timeRemaining() < 1;
+  }
+
+  requestIdleCallback(workLoop);
+}
+
+requestIdleCallback(workLoop);
+
+// 每个fiber只做三件事
+// 把element添加到DOM上
+// 跳出下一个任务单元
+function performUnitOfWork(fiber) {
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber);
+  }
+
+  if (fiber.parent) {
+    fiber.parent.dom.append(fiber.dom);
+  }
+
+  const elements = fiber.props.children;
+}
+
+function createDom(fiber) {
+  const dom =
+    element.type === 'TEXT_ELEMENT'
+      ? document.createTextNode('')
+      : document.createElement(element.type);
+
+  const isProperty = key => key !== 'children';
+
+  Object.keys(element.props)
+    .filter(element => isProperty(element))
+    .forEach(name => {
+      dom[name] = element.props[name];
+    });
+
+  return dom;
+}
+
+function render(element, container) {
+  nextUnitOfWork = {
+    dom: container,
+    props: {
+      children: [element],
+    },
+  };
+}
+
+const Didact = {
+  createElement,
+  render,
 };
 
-const container = document.querySelector('#root');
+function createTextElement(text) {
+  return {
+    type: 'TEXT_ELEMENT',
+    props: {
+      nodeValue: text,
+      children: [],
+    },
+  };
+}
 
-const node = document.createElement(element.type);
+function createElement(type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map(child =>
+        typeof child === 'object' ? child : createTextElement(child),
+      ),
+    },
+  };
+}
 
-node['title'] = element.props.title;
-
-const text = document.createTextNode('');
-text['nodeValue'] = element.props.children;
-
-node.append(text);
-container.append(node);
+const element = Didact.createElement(
+  'div',
+  { id: 'foo' },
+  Didact.createElement('a', null, 'bar'),
+  Didact.createElement('b'),
+);
+Didact.render(element, document.querySelector('#root'));
