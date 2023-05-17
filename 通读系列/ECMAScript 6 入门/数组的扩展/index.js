35b53ac1a0f8d5337d3e7251e708e68db577cba9
2043869032@qq.com <2043869032@qq.com>
2023-05-17 20:00:23 +0800

    chore: commit

---
+++ 通读系列/ECMAScript 6 入门/数组的扩展/index.js
@@ -0,0 +1,39 @@
+(() => {
+  const arrayLike = {
+    0: 'a',
+    1: 'b',
+    2: 'c',
+    length: 3,
+  };
+
+  const arr1 = Array.prototype.slice.call(arrayLike); // ["a","b","c"]
+  const arr2 = Array.from(arrayLike); // ['a','b','c']
+
+  console.log(arr1, arr2);
+})();
+
+// NodeList  对象
+(() => {
+  const ps = document.querySelectorAll('p');
+
+  Array.from(ps).forEach(p => {
+    console.log(p);
+  });
+})();
+
+(() => {
+  Array.from('hello');
+  const nameSet = new Set(['a', 'b']);
+  Array.from(nameSet);
+})();
+
+(() => {
+  function foo() {
+    const args = [...arguments];
+  }
+
+  const toArray = () =>
+    Array.from ? Array.from : obj => Array.prototype.slice.call(obj);
+})();
+
+(() => {})();
