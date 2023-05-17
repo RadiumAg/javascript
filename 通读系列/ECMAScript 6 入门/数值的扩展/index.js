26dbbcabb7d37fde237b3baac426c9a85f375b76
2043869032@qq.com <2043869032@qq.com>
2023-05-17 16:50:59 +0800

    chore: commit

--- 通读系列/ECMAScript 6 入门/数值的扩展/index.js
+++ 通读系列/ECMAScript 6 入门/数值的扩展/index.js
@@ -103,3 +103,100 @@
   Number.MIN_SAFE_INTEGER === -Number.MAX_SAFE_INTEGER;
   Number.MIN_SAFE_INTEGER = -9007199254740991;
 })();
+
+// Math对象的扩展
+
+// Math.trunc 去除一个数的小数部分
+(() => {
+  console.log(Math.trunc(4.1)); // 4
+  console.log(Math.trunc(4.9)); // 4
+  console.log(Math.trunc(-4.1)); // -4
+  console.log(Math.trunc(-4.9)); // -4
+  console.log(Math.trunc(-0.1234)); // -0
+})();
+
+(() => {
+  Math.trunc('123.456');
+  Math.trunc(Number.NaN); // NaN
+  Math.trunc('foo'); // NaN
+  Math.trunc(); // NaN
+
+  Math.trunc =
+    Math.trunc ||
+    (x => {
+      return x < 0 ? Math.ceil(x) : Math.floor(x);
+    });
+})();
+
+// Math.sign(-5) 用于判断一个数到到底是正数，负数还是零
+(() => {
+  Math.sign(-5); // -1
+  Math.sign(5); // +1
+
+  Math.sign =
+    Math.sign ||
+    function (x) {
+      x = +x;
+      if (x === 0 || Number.isNaN(x)) {
+        return x;
+      }
+
+      return x > 0 ? 1 : -1;
+    };
+})();
+
+// Math.cbrt 用于计算一个数的立方根
+(() => {
+  console.log(Math.cbrt(-1)); // -1
+  console.log(Math.cbrt(0)); // 0
+  console.log(Math.cbrt(1)); // 1
+  console.log(Math.cbrt(2));
+})();
+
+// Math.clz32(); 整数使用32位进制形式表示
+(() => {
+  console.log(Math.clz32(0));
+  console.log(Math.clz32(1));
+  console.log(Math.clz32(1000));
+  console.log(Math.clz32(0b01000000000000000000000000));
+  console.log(Math.clz32(0b00100000000000000000000000));
+})();
+
+// Math.imul()
+(() => {
+  Math.imul(2, 4); // 8
+  Math.imul(-1, 8); // -8
+  Math.imul(-2, -2); // 4
+  Math.imul(0x7fffffff, 0x7fffffff); // 1
+})();
+
+// Math.fround() 返回一个数的单精度表示形式
+(() => {
+  Math.fround(0); // 0
+  Math.fround(1); // 1
+  Math.fround(1.337); // 1.5
+  Math.fround(Number.NaN); // NaN
+
+  Math.fround =
+    Math.fround ||
+    function (x) {
+      return new Float32Array([x])[0];
+    };
+})();
+
+// Math.hypot() 返回所有参数的平方和的平方根
+(() => {
+  Math.hypot(3, 4);
+  Math.hypot(3, 4, 5);
+  Math.hypot();
+  Math.hypot(Number.NaN);
+  Math.hypot(3, 4, 'foo');
+  Math.hypot(3, 4, '5');
+  Math.hypot(-3);
+})();
+
+// 指数运算符 **
+(() => {
+  console.log(2 ** 2);
+  console.log(2 ** 3);
+})();
