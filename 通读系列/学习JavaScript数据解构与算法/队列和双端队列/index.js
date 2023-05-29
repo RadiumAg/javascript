268d72e4318fd033b18f20d409550d6e671e9fb8
2043869032@qq.com <2043869032@qq.com>
2023-05-29 20:31:09 +0800

    chore:增加 队列

--- 通读系列/学习JavaScript数据解构与算法/队列和双端队列/index.js
+++ 通读系列/学习JavaScript数据解构与算法/队列和双端队列/index.js
@@ -5,4 +5,55 @@ class Queue {
     this.lowestCount = 0;
     this.items = {};
   }
+
+  enqueue(element) {
+    this.items[this.count] = element;
+    this.count++;
+  }
+
+  denqueue() {
+    if (this.isEmpty()) {
+      return undefined;
+    }
+
+    const result = this.items[this.lowestCount];
+    delete this.items[this.lowestCount];
+    this.lowestCount++;
+    return result;
+  }
+
+  peek() {
+    if (this.isEmpty()) {
+      return undefined;
+    }
+
+    return this.items[this.lowestCount];
+  }
+
+  isEmpty() {
+    return this.count - this.lowestCount === 0;
+  }
+
+  size() {
+    return this.count - this.lowestCount;
+  }
+
+  clear() {
+    this.items = {};
+    this.count = 0;
+    this.lowestCount = 0;
+  }
+
+  toString() {
+    if (this.isEmpty()) {
+      return '';
+    }
+
+    const objString = `${this.items[this.lowestCount]}`;
+
+    for (let i = this.lowestCount + 1; i < this.count; i++) {
+      objString = `${objString}，${this.items[i]}`;
+    }
+    return objString;
+  }
 }
