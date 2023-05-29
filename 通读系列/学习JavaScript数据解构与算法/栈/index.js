d94a9ec576949fa656122381c0d87a6c93151af3
2043869032@qq.com <2043869032@qq.com>
2023-05-29 20:16:14 +0800

    chore: commit

--- 通读系列/学习JavaScript数据解构与算法/栈/index.js
+++ 通读系列/学习JavaScript数据解构与算法/栈/index.js
@@ -118,4 +118,9 @@ console.log(stack.toString());
 
     return baseString;
   }
+
+  console.log(baseConverter(100345, 2));
+  console.log(baseConverter(100345, 8));
+  console.log(baseConverter(100345, 16));
+  console.log(baseConverter(100345, 35));
 })();
---
+++ 通读系列/学习JavaScript数据解构与算法/队列和双端队列/index.js
@@ -0,0 +1,8 @@
+// 队是遵循先进先出原则的一组有序的项
+class Queue {
+  constructor() {
+    this.count = 0;
+    this.lowestCount = 0;
+    this.items = {};
+  }
+}
