9582289c7d1f14874a88ce581b475e0e73dec1d3
2043869032@qq.com <2043869032@qq.com>
2023-06-02 13:52:45 +0800

    chore: commit

---
+++ 通读系列/学习JavaScript数据解构与算法/链表数据结构/index.mjs
@@ -0,0 +1,154 @@
+// 链表数据结构
+// 大多数语言中数组的大小是固定的，从数组的起点或中间插入移除项的成本很高
+
+import { defaultEquals } from '../util.mjs';
+
+class Node {
+  constructor(element) {
+    this.element = element;
+    this.next = undefined;
+  }
+}
+
+class LinkedList {
+  constructor(equalFn = defaultEquals) {
+    this.count = 0;
+    this.head = undefined;
+    this.equalFn = equalFn;
+  }
+
+  push(element) {
+    const node = new Node(element);
+    let current;
+
+    if (!this.head) {
+      this.head = node;
+    } else {
+      current = this.head;
+      while (current.next) {
+        // 当current.next 元素为undefined或null时，我们就知道已经到达链表尾部了
+        current = current.next;
+      }
+
+      current.next = node;
+    }
+    this.count++;
+  }
+
+  removeAt(index) {
+    if (index >= 0 && index < this.count) {
+      let current = this.head;
+
+      if (index === 0) {
+        this.head = current.next;
+      } else {
+        const previous = this.getElementAt(index - 1);
+        current = previous.next;
+        // 将previous与current的下一项链接起来，跳过current，从而移除它
+        previous.next = current.next;
+      }
+
+      this.count--;
+      return current.element;
+    }
+
+    return undefined;
+  }
+
+  insert(element, index) {
+    if (index >= 0 && index <= this.count) {
+      const node = new Node(element);
+
+      if (index === 0) {
+        const current = this.head;
+        node.next = current;
+        this.head = node;
+      } else {
+        const previous = this.getElementAt(index - 1);
+        const current = previous.next;
+        node.next = current;
+        previous.next = node;
+      }
+
+      this.count++;
+      return true;
+    }
+
+    return false;
+  }
+
+  getElementAt(index) {
+    if (index > 0 && index <= this.count) {
+      const node = this.node;
+      for (let i = 0; i < index && node != null; i++) {
+        node = node.next;
+      }
+
+      return node;
+    }
+  }
+
+  indexOf(element) {
+    let current = this.head;
+    for (let i = 0; i < this.count && current != null; i++) {
+      if (this.equalFn(element, current.element)) {
+        return i;
+      }
+
+      current = current.next;
+    }
+
+    return -1;
+  }
+
+  size() {
+    return this.count;
+  }
+
+  isEmpty() {
+    return this.size() === 0;
+  }
+
+  getHead() {
+    return this.head;
+  }
+
+  toString() {
+    if (this.head == null) {
+      return '';
+    }
+    let objString = `${this.head.element}`;
+    const current = this.head.next;
+
+    for (let i = 1; i < this.size() && current != null; i++) {
+      objString = `${objString}，${current.element}`;
+      current = current.next;
+    }
+    return objString;
+  }
+}
+
+class DoublyNode extends Node {
+  constructor(element, next, prev) {
+    super(element, next);
+    this.prev = prev;
+  }
+}
+
+class DoublyLinkList extends LinkedList {
+  constructor(equalFn = defaultEquals) {
+    super(equalFn);
+    this.tail = undefined;
+  }
+}
+
+(() => {
+  const list = new LinkedList();
+  list.push(15);
+  // eslint-disable-next-line unicorn/no-array-push-push
+  list.push(10);
+  list.removeAt(0);
+  console.log(list);
+})();
+
+(() => {})();
