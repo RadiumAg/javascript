cd7ced5c90cf94793b0bc34355e4c873f74fe1ef
2043869032@qq.com <2043869032@qq.com>
2023-06-02 14:17:54 +0800

    chore: commit

--- 通读系列/学习JavaScript数据解构与算法/链表数据结构/index.mjs
+++ 通读系列/学习JavaScript数据解构与算法/链表数据结构/index.mjs
@@ -140,6 +140,40 @@ class DoublyLinkList extends LinkedList {
     super(equalFn);
     this.tail = undefined;
   }
+
+  insert(element, index) {
+    if (index >= 0 && index <= this.count) {
+      const node = new DoublyNode(element);
+      const current = this.head;
+
+      if (index === 0) {
+        if (this.head == null) {
+          this.head = node;
+          this.tail = node;
+        } else {
+          node.next = this.head;
+          current.prev = node;
+          this.head = node;
+        }
+      } else if (index === this.count) {
+        current = this.tail;
+        current.next = node;
+        node.prev = current;
+        this.tail = node;
+      } else {
+        const previous = this.getElementAt(index - 1);
+        current = previous.next;
+        node.next = current;
+        previous.next = node;
+        current.prev = node;
+        node.prev = previous;
+      }
+
+      this.count++;
+      return true;
+    }
+    return false;
+  }
 }
 
 (() => {
