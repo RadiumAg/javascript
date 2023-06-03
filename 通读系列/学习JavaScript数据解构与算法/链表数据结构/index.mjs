8854346ff34fe9ea9768cf7bbe8a25beb8d7bb7c
2043869032@qq.com <2043869032@qq.com>
2023-06-03 11:32:07 +0800

    chore: commit

--- 通读系列/学习JavaScript数据解构与算法/链表数据结构/index.mjs
+++ 通读系列/学习JavaScript数据解构与算法/链表数据结构/index.mjs
@@ -133,6 +133,31 @@ class DoublyNode extends Node {
     super(element, next);
     this.prev = prev;
   }
+
+  removeAt(index) {
+    if (index >= 0 && index < this.count) {
+      let current = this.head;
+
+      if (index === 0) {
+        this.head = current.next;
+
+        if (this.count === 1) {
+          this.tail = undefined;
+        } else {
+          this.head.prev = undefined;
+        }
+      } else if (index === this.count - 1) {
+        current = this.tail;
+        this.tail = current.prev;
+        this.tail.next = undefined;
+      } else {
+        current = this.getElementAt(index);
+        const previous = current.prev;
+
+        previous.next = current.ne;
+      }
+    }
+  }
 }
 
 class DoublyLinkList extends LinkedList {
@@ -173,6 +198,34 @@ class DoublyLinkList extends LinkedList {
       return true;
     }
     return false;
+  }
+
+  removeAt(index) {
+    if (index >= 0 && index < this.count) {
+      let current = this.head;
+
+      if (index === 0) {
+        this.head = current.next;
+
+        if (this.count === 1) {
+          this.tail = undefined;
+        } else {
+          this.head.prev = undefined;
+        }
+      } else if (index === this.count - 1) {
+        current = this.tail;
+        this.tail = current.prev;
+        this.tail.next = undefined;
+      } else {
+        current = this.getElementAt(index);
+        const previous = current.prev;
+        previous.next = current.next;
+        current.next.prev = previous;
+      }
+      this.count--;
+      return current.element;
+    }
+    return undefined;
   }
 }
 
