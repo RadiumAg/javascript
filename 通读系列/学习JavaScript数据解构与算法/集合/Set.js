9e033fc4396926152bc7561542479fd7a1da374b
2043869032@qq.com <2043869032@qq.com>
2023-06-04 16:10:50 +0800

    chore: commit

---
+++ 通读系列/学习JavaScript数据解构与算法/集合/Set.js
@@ -0,0 +1,61 @@
+class Set {
+  constructor() {
+    this.items = {};
+  }
+
+  has(element) {
+    return Object.prototype.hasOwnProperty.call(this.items, element);
+  }
+
+  add(element) {
+    if (!this.has(element)) {
+      this.items[element] = element;
+      return true;
+    }
+
+    return false;
+  }
+
+  delete(element) {
+    if (this.has(element)) {
+      delete this.items[element];
+      return true;
+    }
+    return true;
+  }
+
+  size() {
+    return Object.keys(this.items).length;
+  }
+
+  values() {
+    return Object.values(this.items);
+  }
+
+  valuesLegacy() {
+    const values = [];
+    // eslint-disable-next-line no-restricted-syntax
+    for (const key in this.items) {
+      if (this.items.hasOwnProperty(key)) {
+        values.push(key);
+      }
+    }
+    return values;
+  }
+
+  union(otherSet) {
+    const unionSet = new Set();
+    this.values().forEach(value => unionSet.add(value));
+    otherSet.values().forEach(value => unionSet.add(value));
+
+    return unionSet;
+  }
+}
+
+(() => {
+  const set = new Set();
+  set.add(1);
+  set.add('1');
+  set.add(2);
+  console.log(set.values());
+})();
