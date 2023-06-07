dae4dc98bf22eac1823c213fcf14d9b07c9995e4
2043869032@qq.com <2043869032@qq.com>
2023-06-07 11:44:19 +0800

    chore: commit

---
+++ 通读系列/学习JavaScript数据解构与算法/字典/index.mjs
@@ -0,0 +1,41 @@
+import { defaultToString } from '../util';
+
+class ValuePair {
+  constructor(key, value) {
+    this.key = key;
+    this.value = value;
+  }
+
+  toString() {
+    return `[#${this.key}：${this.value}]`;
+  }
+}
+
+class Dictionary {
+  constructor(toStrFn = defaultToString) {
+    this.toStrFn = toStrFn;
+    this.table = {};
+  }
+
+  hasKey(key) {
+    return this.table[this.toStrFn(key)] != null;
+  }
+
+  set(key, value) {
+    if (key != null && value != null) {
+      const tableKey = this.toStrFn(key);
+      this.table[tableKey] = new ValuePair(key, value);
+      return true;
+    }
+    return false;
+  }
+
+  remove(key) {
+    if (this.hasKey(key)) {
+      delete this.table[this.toStrFn(key)];
+      return true;
+    }
+
+    return false;
+  }
+}
