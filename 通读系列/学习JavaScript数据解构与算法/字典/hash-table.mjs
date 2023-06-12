ccd4531cede1da8c074423fb64c71f8f038eac77
Radium <2043869032@qq.com>
2023-06-12 23:38:02 +0800

    chore: commit

--- 通读系列/学习JavaScript数据解构与算法/字典/hash-table.mjs
+++ 通读系列/学习JavaScript数据解构与算法/字典/hash-table.mjs
@@ -91,6 +91,25 @@ class HashTableSeparateChining {
     this.table = {};
   }
 
+  loseloseHashCode(key) {
+    if (typeof key === 'number') {
+      return key;
+    }
+    const tableKey = this.toStrFn(key);
+
+    let hash = 0;
+
+    for (let i = 0; i < tableKey.length; i++) {
+      hash += tableKey.charCodeAt(1);
+    }
+
+    return hash % 37;
+  }
+
+  hashCode(key) {
+    return this.loseloseHashCode(key);
+  }
+
   put(key, value) {
     if (key != null && value != null) {
       const position = this.hashCode(key);
