8985f2ffbee86a507a52cf262dc288ca8c985a7a
2043869032@qq.com <2043869032@qq.com>
2023-06-16 10:42:09 +0800

    chore: commit

--- 通读系列/学习JavaScript数据解构与算法/递归/index.mjs
+++ 通读系列/学习JavaScript数据解构与算法/递归/index.mjs
@@ -1,21 +1,77 @@
-function understandRecursion(doIunderstandRecursion) {
-  const recursionAnswer = confirm('Do ypu understand recursion');
+(() => {
+  function understandRecursion(doIunderstandRecursion) {
+    const recursionAnswer = confirm('Do ypu understand recursion');
 
-  if (recursionAnswer === true) {
-    return true;
+    if (recursionAnswer === true) {
+      return true;
+    }
+
+    understandRecursion(recursionAnswer);
   }
 
-  understandRecursion(recursionAnswer);
-}
+  function factorialIteratvie(number) {
+    if (number < 0) return undefined;
+    let total = 1;
 
-function factorialIteratvie(number) {
-  if (number < 0) return undefined;
-  let total = 1;
+    for (let n = number; n > 1; n--) {
+      total = total * n;
+    }
 
-  for (let n = number; n > 1; n--) {
-    total = total * n;
+    return total;
   }
 
-  return total;
-}
-console.log(factorialIteratvie(5));
+  console.log(factorialIteratvie(5));
+})();
+
+(() => {
+  function factorial(n) {
+    console.trace();
+    if (n === 1 || n === 0) {
+      return 1;
+    }
+
+    return n * factorial(n - 1);
+  }
+
+  console.log(factorial(5));
+})();
+
+(() => {
+  function fibonacciIterative(n) {
+    if (n < 1) return 0;
+    if (n <= 2) return 1;
+
+    let fibNMinus2 = 0;
+    let fibMinus1 = 1;
+
+    let fibN = n;
+
+    for (let i = 2; i <= n; i++) {
+      fibN = fibMinus1 + fibNMinus2;
+      fibNMinus2 = fibMinus1;
+      fibMinus1 = fibN;
+    }
+
+    return fibN;
+  }
+})();
+
+(() => {
+  function fibonacci(n) {
+    if (n < 1) return 0;
+    if (n <= 2) return 1;
+    return fibonacci(n - 1) + fibonacci(n - 2);
+  }
+})();
+
+(() => {
+  function fibonacciMemoization(n) {
+    const memo = [0, 1];
+    const fibonacci = n => {
+      if (memo[n] != null) return memo[n];
+      return (memo[n] = fibonacci(n - 1, memo) + fibonacci(n - 2, memo));
+    };
+
+    return fibonacci;
+  }
+})();
