5a5ade1257585c152d11fc536198e4f04b06eb06
2043869032@qq.com <2043869032@qq.com>
2023-06-16 11:14:17 +0800

    chore: commit

--- 通读系列/学习JavaScript数据解构与算法/tree/index.mjs
+++ 通读系列/学习JavaScript数据解构与算法/tree/index.mjs
@@ -1,3 +1,5 @@
+import { Compare, defaultCompare } from '../util.mjs';
+
 class Node {
   constructor(key) {
     this.key = key;
@@ -5,3 +7,43 @@ class Node {
     this.right = null;
   }
 }
+
+class BinarySearchTree {
+  constructor(compareFn = defaultCompare) {
+    this.compareFn = compareFn;
+    this.root = null;
+  }
+
+  insert(key) {
+    if (this.root == null) {
+      this.root = new Node(key);
+    } else {
+      this.insertNode(this.root, key);
+    }
+  }
+
+  insertNode(node, key) {
+    if (this.compareFn(key, node.key) === Compare.LESS_THAN) {
+      if (node.left == null) {
+        node.left = new Node(key);
+      } else {
+        this.insertNode(node.left, key);
+      }
+    } else if (node.right == null) {
+      node.right = new Node(key);
+    } else {
+      this.insertNode(node.right, key);
+    }
+  }
+}
+
+(() => {
+  const tree = new BinarySearchTree();
+  tree.insert(11);
+  tree.insert(15);
+  tree.insert(5);
+  tree.insert(3);
+  tree.insert(9);
+
+  console.log(tree);
+})();
