e60b92f6de6090288c174737519dc367310afa87
Radium <2043869032@qq.com>
2023-06-19 23:38:21 +0800

    chore: commit

--- 通读系列/学习JavaScript数据解构与算法/排序和搜索算法/index.mjs
+++ 通读系列/学习JavaScript数据解构与算法/排序和搜索算法/index.mjs
@@ -12,3 +12,17 @@ function bubbleSort(array, compareFn = defaultCompare) {
 
   return array;
 }
+
+function createNonSortedArray(size) {
+  const array = [];
+  for (let i = size; i > 0; i--) {
+    array.push(i);
+  }
+
+  return array;
+}
+
+let array = createNonSortedArray(5);
+console.log(array.join());
+array = bubbleSort(array);
+console.log(array);
