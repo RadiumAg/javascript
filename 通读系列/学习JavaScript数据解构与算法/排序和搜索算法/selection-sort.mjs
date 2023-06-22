4ff8cbed3392f0c5ed00b433506386b61beca7da
Radium <2043869032@qq.com>
2023-06-22 21:58:41 +0800

    chore: commit

--- 通读系列/学习JavaScript数据解构与算法/排序和搜索算法/selection-sort.mjs
+++ 通读系列/学习JavaScript数据解构与算法/排序和搜索算法/selection-sort.mjs
@@ -1,5 +1,20 @@
-import { defaultCompare } from '../util.mjs';
+import { Compare, defaultCompare, swap } from '../util.mjs';
 
 function selectionSort(array, compareFn = defaultCompare) {
   const { length } = array;
+  let indexMin;
+
+  for (let i = 0; i < length - 1; i++) {
+    indexMin = i;
+    for (let j = i; i < length; j++) {
+      if (compareFn(array[indexMin], array[j]) === Compare.BIGGER_THAN) {
+        indexMin = j;
+      }
+    }
+    if (i !== indexMin) {
+      swap(array, i, indexMin);
+    }
+  }
+
+  return array;
 }
