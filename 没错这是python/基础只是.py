a2b16cf4f38ec5310d9d1f3f978ac953474ea946
Radium <2043869032@qq.com>
2023-07-02 20:44:38 +0800

    chore: commit

--- 没错这是python/基础只是.py
+++ 没错这是python/基础只是.py
@@ -1,3 +1,28 @@
 x = 3
 
 print(x)
+
+
+def str_repr():
+    print(repr("Hello,\nworld!"))
+    print(str("Hello,\nworld!"))
+
+
+str_repr();
+
+
+def 长字符串():
+    print('''
+    1111
+    21
+    2
+    12
+    12
+
+    ''')
+长字符串()
+
+def 原始字符串():
+    print(r'C:\nwhere')
+
+原始字符串()
