fcaf16fa11854d60784f80afe6ebd69ea2d810bc
Radium <2043869032@qq.com>
2023-07-03 22:27:28 +0800

    chore: commit

--- 没错这是python/使用字符串.py
+++ 没错这是python/使用字符串.py
@@ -5,3 +5,15 @@ def 使用format():
 
 
 使用format()
+
+
+def 基本转换():
+    print("{pi!s} {pi!r} {pi!a}".format(pi = "我"))
+
+基本转换()
+
+def 宽度精度千位分隔符():
+    print(repr("{num:10}".format(num = 3)))
+    print(repr("{name:10}".format(name="Bob")))
+
+宽度精度千位分隔符()
