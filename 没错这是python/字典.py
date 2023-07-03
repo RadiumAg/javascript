31f48b255419462fc9d163d8173e1de05525ad19
Radium <2043869032@qq.com>
2023-07-03 22:34:58 +0800

    chore: commit

---
+++ 没错这是python/字典.py
@@ -0,0 +1,9 @@
+def 创建和使用字典():
+    phonebook = {'Alice':'2341','Beth':'9102','Cecil':'3258'}
+    
+    ## 元组
+    items =[('name','Gumby') , ('age', 32)]
+    d = dict(items)
+    print(d)
+
+创建和使用字典()
