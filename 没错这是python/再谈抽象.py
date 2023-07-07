026ffe872553d55e03963c53fac515f812b71c2d
2043869032@qq.com <2043869032@qq.com>
2023-07-07 10:45:57 +0800

    chore: commit

--- 没错这是python/再谈抽象.py
+++ 没错这是python/再谈抽象.py
@@ -1,3 +1,19 @@
-from random import choice
+class Persion:
+    def set_name(self, name):
+        self.name = name
+        self.__a
 
-x = choice("Hello,world!", [1, 2])
+    def get_name(self):
+        return self.name
+
+    def greet(self):
+        print("Hello,world!,i'm {}".format(self.name))
+
+     __a = 1 ## 私有属性
+
+
+class Filter:
+    pass
+
+class a(Filter):
+    pass
