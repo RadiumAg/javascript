556e317830bbd9bae1d57929d2d248ff7574db01
2043869032@qq.com <2043869032@qq.com>
2023-07-08 14:59:13 +0800

    chore: commit

--- 没错这是python/异常.py
+++ 没错这是python/异常.py
@@ -1,2 +1,27 @@
 def raise语句():
     raise Exception
+
+
+def 不用提供参数():
+    class MuffedCalculator:
+        muffled = False
+
+        def calc(self, expr):
+            try:
+                return eval(expr)
+
+            except ZeroDivisionError:
+                if self.muffled:
+                    print("Division by zero is illegal")
+                else:
+                    raise
+
+    calculator = MuffedCalculator()
+    calculator.calc("10 / 2")
+
+
+不用提供参数()
+
+
+def 不那么异常的情况():
+    
