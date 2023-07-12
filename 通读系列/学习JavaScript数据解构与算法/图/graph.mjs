8e95645c954191284b282ec4eb0f682da3623c06
2043869032@qq.com <2043869032@qq.com>
2023-07-12 17:42:38 +0800

    chore: commit

--- 通读系列/学习JavaScript数据解构与算法/图/graph.mjs
+++ 通读系列/学习JavaScript数据解构与算法/图/graph.mjs
@@ -389,45 +389,19 @@ const minDistance = (dist, visited) => {
   console.log(s);
 };
 
-const minKey = (graph, key, visited) => {
-  let min = INF;
-  let minIndex = 0;
-
-  for (let v = 0; v < graph.length; v++) {
-    if (visited[v] === false && key[v] < min) {
-      min = key[v];
-      minIndex = v;
-    }
-  }
-
-  return minIndex;
-};
-
 // Prim算法
 const prim = graph => {
   const parent = [];
   const key = [];
   const visited = [];
-
   const { length } = graph;
 
   for (let i = 0; i < length; i++) {
-    key[i] = INF;
+    key[i] - INF;
     visited[i] = false;
   }
 
   key[0] = 0;
-
-  parent[0] = -1;
-
-  for (let i = 0; i < length - 1; i++) {
-    const u = minKey(graph, key, visited);
-    visited[u] = true;
-
-    for(let v = 0; v < length; v++) {
-      if(graph[u][v] && !)
-    }
-  }
 };
 
 (() => {
