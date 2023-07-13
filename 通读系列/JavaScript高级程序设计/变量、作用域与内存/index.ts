207d747337e555794a25ccae5ebacf58dafd26aa
Radium <2043869032@qq.com>
2023-07-13 23:59:44 +0800

    chore: commit

---
+++ 通读系列/JavaScript高级程序设计/变量、作用域与内存/index.ts
@@ -0,0 +1,2 @@
+// primitive value 原始值  Undefined ,Null ,Boolean,Number,String,Symbol
+// reference value 引用值
--- 通读系列/JavaScript高级程序设计/语言基础/index.ts
+++ 通读系列/JavaScript高级程序设计/语言基础/index.ts
@@ -3,3 +3,17 @@ const num2 = ~num;
 
 console.log(num2);
 console.log(num.toString(2), num2.toString(2));
+
+(() => {
+  let age = 29;
+  const anotherAge = --age + 2;
+  console.log(age); // 28
+  console.log(anotherAge); // 30
+
+  let num1 = 2;
+  const num2 = 20;
+  const num3 = num1-- + num2;
+  const num4 = num1 + num2;
+  console.log(num3); // 22
+  console.log(num4); // 21
+})(); // 递增/递减操作符
