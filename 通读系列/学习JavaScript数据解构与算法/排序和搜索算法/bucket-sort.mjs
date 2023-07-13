2c6175ef9394c22b02e03d1cd43ef0de78cda6e2
2043869032@qq.com <2043869032@qq.com>
2023-07-13 17:59:34 +0800

    chore: commit

---
+++ 通读系列/学习JavaScript数据解构与算法/排序和搜索算法/bucket-sort.mjs
@@ -0,0 +1,30 @@
+function bucketSort(array, bucketSize = 5) {
+  if (array.length < 2) return array;
+}
+
+function createBuckets(array, bucketSize) {
+  let minValue = array[0];
+  let maxValue = array[0];
+
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] < minValue) {
+      minValue = array[i];
+    } else if (array[i] > maxValue) {
+      maxValue = array[i];
+    }
+  }
+
+  const bucketCount = Math.floor((maxValue - minValue) / bucketSize) + 1;
+  const buckets = [];
+
+  for (let i = 0; i < bucketCount; i++) {
+    buckets[i] = [];
+  }
+
+  for (const element of array) {
+    const index = array.indexOf(element);
+    const bucketIndex = Math.floor((element - minValue) / bucketSize);
+    buckets[bucketIndex].push(array[index]);
+  }
+  return buckets;
+}
