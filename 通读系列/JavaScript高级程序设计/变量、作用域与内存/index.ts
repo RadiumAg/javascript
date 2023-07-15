d981aca65c875056ea935abd4570cbc4a5faef8f
Radium <2043869032@qq.com>
2023-07-16 00:46:58 +0800

    chore: commit

--- 通读系列/JavaScript高级程序设计/变量、作用域与内存/index.ts
+++ 通读系列/JavaScript高级程序设计/变量、作用域与内存/index.ts
@@ -17,3 +17,21 @@
 })();
 
 // 全局上下文是最外层的上下文
+// 每个函数都有自己的上下文
+
+// 作用域增强
+// with 语句
+(() => {
+  function buildUrl() {
+    const qs = '? debug=true';
+    with (location) {
+      const url = href + qs;
+      return url;
+    }
+  }
+})();
+
+// 使用var声明变量时，变量会被自动添加到最接近的上下文
+// 在函数中，最接近的上下就是函数的局部上下文
+
+// 块级作用域
