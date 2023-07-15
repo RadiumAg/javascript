d3f803aa14a4c59094a7e75f15322932108149af
Radium <2043869032@qq.com>
2023-07-15 22:16:39 +0800

    chore: commit

--- 通读系列/JavaScript高级程序设计/变量、作用域与内存/index.ts
+++ 通读系列/JavaScript高级程序设计/变量、作用域与内存/index.ts
@@ -4,3 +4,16 @@
 (() => {
   console.log(typeof null); //object
 })();
+// typeof 是判断原始类型最好的方式
+// instanceof 是判断应用类型的最好方式
+(() => {
+  console.log(typeof 1);
+  console.log(typeof undefined);
+  console.log(typeof true);
+  console.log(typeof '1');
+
+  // eslint-disable-next-line unicorn/no-instanceof-array
+  console.log([] instanceof Array);
+})();
+
+// 全局上下文是最外层的上下文
