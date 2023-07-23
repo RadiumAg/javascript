910c037c7411f2317b23bc17d02f09bf86267594
Radium <2043869032@qq.com>
2023-07-23 22:45:44 +0800

    chore: commit

--- 通读系列/JavaScript高级程序设计/集合引用类型/map.ts
+++ 通读系列/JavaScript高级程序设计/集合引用类型/map.ts
@@ -41,4 +41,22 @@
 })();
 
 // 顺序与迭代器
-(() => {})();
+(() => {
+  const m = new Map([
+    ['key1', 'val1'],
+    ['key2', 'val2'],
+    ['key3', 'val3'],
+  ]);
+
+  for (const pair of m.entries()) {
+    console.log(pair);
+  }
+
+  for (const pair of m[Symbol.iterator]()) {
+    console.log(pair);
+  }
+
+  m.forEach((value, key) => {
+    console.log(`${key} -> ${value}`);
+  });
+})();
