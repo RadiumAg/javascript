788c9d18b4b46eabf519974890777f496c4eb7df
2043869032@qq.com <2043869032@qq.com>
2023-08-01 09:42:09 +0800

    chore: commit

--- 通读系列/JavaScript高级程序设计/对象，类与面向对象编程/object.ts
+++ 通读系列/JavaScript高级程序设计/对象，类与面向对象编程/object.ts
@@ -17,7 +17,7 @@
 };
 
 // configurable
-(() => {
+() => {
   const person = {};
   Object.defineProperty(person, 'name', {
     configurable: false,
@@ -28,10 +28,10 @@
     configurable: true,
     value: 'Nicholas',
   });
-})();
+};
 
 // 访问器属性
-(() => {
+() => {
   const book = {
     year_: 2017,
     edition: 1,
@@ -51,4 +51,58 @@
 
   book.year = 2018;
   console.log(book.edition);
+};
+
+// 定义多个属性
+() => {
+  const book = {};
+  Object.defineProperties(book, {
+    year_: {
+      value: 2017,
+    },
+    edition: {
+      value: 1,
+    },
+    year: {
+      get() {
+        return this.year_;
+      },
+      set(newValue) {
+        if (newValue > 2017) {
+          this.year_ = newValue;
+          this.edition += newValue - 2017;
+        }
+      },
+    },
+  });
+};
+
+// 读取属性的特征
+(() => {
+  const book = {};
+  Object.defineProperties(book, {
+    year_: {
+      value: 2017,
+    },
+    edition: {
+      value: 1,
+    },
+    year: {
+      get() {
+        return this.year_;
+      },
+      set(newValue) {
+        if (newValue > 2017) {
+          this.year_ = newValue;
+          this.edition += newValue - 2017;
+        }
+      },
+    },
+  });
+
+  console.log(Object.getOwnPropertyDescriptor(book, 'year_'));
+  console.log(Object.getOwnPropertyDescriptors(book));
 })();
+
+// 合并对象Object.assign
+(() => {})();
