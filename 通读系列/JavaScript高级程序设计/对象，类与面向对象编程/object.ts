22c5f579ada74a1d70eee867d71e8fa930f84dfb
2043869032@qq.com <2043869032@qq.com>
2023-08-01 09:15:55 +0800

    chore: commit

--- 通读系列/JavaScript高级程序设计/对象，类与面向对象编程/object.ts
+++ 通读系列/JavaScript高级程序设计/对象，类与面向对象编程/object.ts
@@ -29,3 +29,26 @@
     value: 'Nicholas',
   });
 })();
+
+// 访问器属性
+(() => {
+  const book = {
+    year_: 2017,
+    edition: 1,
+  };
+
+  Object.defineProperty(book, 'year', {
+    get() {
+      return this.year_;
+    },
+    set(newValue) {
+      if (newValue > 2017) {
+        this.year_ = newValue;
+        this.edition += newValue - 2017;
+      }
+    },
+  });
+
+  book.year = 2018;
+  console.log(book.edition);
+})();
