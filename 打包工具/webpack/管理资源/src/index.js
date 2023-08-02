8f88a3b80ad418916a5487391ed267c1a76df1d5
2043869032@qq.com <2043869032@qq.com>
2023-08-02 09:56:07 +0800

    chore: commit

--- 打包工具/webpack/管理资源/src/index.js
+++ 打包工具/webpack/管理资源/src/index.js
@@ -1,6 +1,20 @@
 import './style.css';
 import _ from 'lodash';
+import tomal from './data.toml';
+import yaml from './data.yaml';
+import json from './data.json5';
 import Icon from './icon.png';
+import Data from './data.csv';
+import Notes from './data.xml';
+
+console.log(tomal.title);
+console.log(tomal.owner.name);
+
+console.log(yaml.title);
+console.log(yaml.owner.name);
+
+console.log(json.title);
+console.log(json.owner.name);
 
 function component() {
   const elememt = document.createElement('div');
@@ -11,6 +25,9 @@ function component() {
   const myIcon = new Image();
   myIcon.src = Icon;
   elememt.append(myIcon);
+
+  console.log(Data);
+  console.log(Notes);
 
   return elememt;
 }
