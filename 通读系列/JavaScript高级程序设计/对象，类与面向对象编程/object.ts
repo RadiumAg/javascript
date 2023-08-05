98174cdeacb746f5bda3c157cc66f4b0f87de2f5
2043869032@qq.com <2043869032@qq.com>
2023-08-05 15:19:54 +0800

    chore: commit

--- 通读系列/JavaScript高级程序设计/对象，类与面向对象编程/object.ts
+++ 通读系列/JavaScript高级程序设计/对象，类与面向对象编程/object.ts
@@ -258,3 +258,30 @@
 })();
 
 // 创建对象几个方式
+
+// 1.工厂模式
+(() => {
+  function createPerson(name, age, job) {
+    const o = new Object();
+    o.name = name;
+    o.age = age;
+    o.job = job;
+    o.sayName = function () {
+      console.log(this.name);
+    };
+
+    return o;
+  }
+})();
+
+// 构造函数模式
+(() => {
+  function Person(name, age, job) {
+    this.name = name;
+    this.age = age;
+    this.job = job;
+    this.sayName = function () {
+      console.log(this.name);
+    };
+  }
+})();
