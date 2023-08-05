37c6f1155d7cc8dda2fa9e8ed2eaeaebc58eb8ca
2043869032@qq.com <2043869032@qq.com>
2023-08-05 17:34:41 +0800

    chore: commit

---
+++ 面试题/throttle与debounced/index.js
@@ -0,0 +1,28 @@
+// 距离上次点击要经过during秒之后才能触发
+const useThrottle = (fn, during) => {
+  let lastTime = 0;
+
+  return () => {
+    if (Date.now() - lastTime > during) {
+      lastTime = Date.now();
+      fn();
+    }
+  };
+};
+
+// during秒内触发的时间都被取消
+const useDebounced = (fn, during) => {
+  let timer;
+
+  return () => {
+    if (timer) {
+      clearTimeout(timer);
+    }
+
+    timer = setTimeout(() => {
+      fn();
+    }, during);
+  };
+};
+
+export { useThrottle, useDebounced };
