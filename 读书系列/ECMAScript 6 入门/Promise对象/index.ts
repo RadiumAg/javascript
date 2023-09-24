575fb7ddf2db99b19913d5fcadaf541af8f5e812
Radium <2043869032@qq.com>
2023-09-24 23:25:57 +0800

    chore: commit

--- 读书系列/ECMAScript 6 入门/Promise对象/index.ts
+++ 读书系列/ECMAScript 6 入门/Promise对象/index.ts
@@ -1,3 +1,13 @@
+// Promise.race
+(() => {
+  const p = Promise.race([
+    fetch('/resource-that-may-take-a-while'),
+    new Promise((resolve, reject) => {
+      setTimeout(() => reject(new Error('request timeout')), 5000);
+    }),
+  ]);
+})();
+
 // Promise.any
 (() => {
   Promise.any([
