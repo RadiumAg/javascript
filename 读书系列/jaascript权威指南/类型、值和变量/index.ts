371518f7445c466914be3abd607e354c89aa226d
Radium <2043869032@qq.com>
2023-10-02 21:29:34 +0800

    chore: commit

--- 读书系列/jaascript权威指南/类型、值和变量/index.ts
+++ 读书系列/jaascript权威指南/类型、值和变量/index.ts
@@ -4,6 +4,22 @@
 })();
 
 // 布尔值
+// falsy value，下面这些值会转换成false
+(() => {
+  console.log(!!undefined);
+  console.log(!!null);
+  console.log(!!0);
+  console.log(!!-0);
+  console.log(!!Number.NaN);
+  console.log(!!'');
+})();
 
-// commit
-// commit1
+// null认为是一个特殊的对象值，含义是“非对象”
+(() => {
+  console.log(typeof null);
+})();
+
+// undefined 是全局变量，它不是关键字
+
+// 包装对象
+(() => {})();
