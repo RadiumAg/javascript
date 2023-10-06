5abf5f5a8a39dab4a31386bb961a456dd8d9ade8
Radium <2043869032@qq.com>
2023-10-06 20:36:10 +0800

    chore: commit

--- 读书系列/jaascript权威指南/类型、值和变量/index.ts
+++ 读书系列/jaascript权威指南/类型、值和变量/index.ts
@@ -62,5 +62,13 @@
   console.log(!!x);
 
   // toFixed，toExponential，toPrecision 都会适当的四舍五入
-  // commit
+  const n = 123456.789;
+  n.toFixed(0); // 1234567
+  n.toFixed(2); // 123456.79
+  n.toFixed(5); // 123456.78900
+  n.toExponential(1); // 1.2e+5
+  n.toExponential(3); // 1.235e+5
+  n.toPrecision(4); // 1.235e+5
+  n.toPrecision(7); // 123456.8
+  n.toPrecision(10); // 123456.7890
 })();
