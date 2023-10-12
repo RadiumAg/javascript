e80db4dce1eb91b23931f314c0f8f5192097a1d7
radiumag <2043869032@qq.com>
2023-10-12 22:46:42 +0800

    chore: commit

--- 读书系列/jaascript权威指南/类型、值和变量/index.ts
+++ 读书系列/jaascript权威指南/类型、值和变量/index.ts
@@ -169,5 +169,13 @@
 
 // 函数定义
 (()=>{
-   function printprops(o){}
+   function printprops(o:Record<string,any>){
+    for(const p in o)
+        console.log(p + ":" + o[p] + "\n")
+   }
+
+   function distance(x1:number,y1:number,x2:number,y2:number) {
+     const dx = x2 - x1;
+     const dy  = y2 - y1
+   }
 })();
