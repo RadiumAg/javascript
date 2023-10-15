485f8519f1acffd21e4ad6687c1c2b0c3a575048
Radium <2043869032@qq.com>
2023-10-15 18:04:13 +0800

    chore: commit

--- 读书系列/jaascript权威指南/函数/inndex.ts
+++ 读书系列/jaascript权威指南/函数/inndex.ts
@@ -1,3 +1,38 @@
 // 函数定义
 // 函数名称标识符。函数名称是函数声明语句必须的部分
-(() => {})();
+(() => {
+  function a() {}
+})();
+
+// 判断是否是严格模式
+(() => {
+  const strict = (function () {
+    return !this;
+  })();
+})();
+
+(() => {
+  const calculator = {
+    operand: 1,
+    operand2: 1,
+    add() {
+      this.result = this.operand + this.operand2;
+    },
+  };
+
+  calculator.add();
+  console.log(calculator.result);
+})();
+
+(() => {
+  function f(x, y, z) {
+    if (arguments.length !== 3) {
+    }
+  }
+})();
+
+(() => {
+  function add(x, y) {
+    return x + y;
+  }
+})();
