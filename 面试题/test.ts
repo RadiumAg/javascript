00958bf844c06d6d4113cfc25d96b9c6975c198f
Radium <2043869032@qq.com>
2023-11-04 22:23:41 +0800

    chore: commit

--- 面试题/test.ts
+++ 面试题/test.ts
@@ -24,7 +24,7 @@
   };
 };
 
-(() => {
+() => {
   const myInstanceOf = (
     instance: Record<string, ay>,
     constructor: new (...args: any[]) => any,
@@ -47,4 +47,39 @@
   class a {}
 
   console.log(myInstanceOf(new a(), a));
+  console.log(myInstanceOf(new Object(), a));
+};
+
+(() => {
+  Promise.myAll = (promiseArr: Promise<any>[]) => {
+    const result: any[] = [];
+    let resolveFn: (value: unknown) => void;
+    let rejectFn: (reason?: any) => void;
+
+    const promise = new Promise((resolve, reject) => {
+      resolveFn = resolve;
+      rejectFn = reject;
+    });
+
+    promiseArr.forEach((promise, index) => {
+      promise
+        .then(value => {
+          result[index] = value;
+          if (result.filter(_ => _).length === promiseArr.length) {
+            resolveFn(result);
+          }
+        })
+        .catch(error => {
+          rejectFn(error);
+        });
+    });
+
+    return promise;
+  };
+
+  const errorPromise = Promise.myAll([Promise.resolve(1), Promise.reject(2)]);
+
+  setTimeout(() => {
+    console.log(errorPromise);
+  }, 1000);
 })();
