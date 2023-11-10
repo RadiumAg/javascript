2364dea8949322e5f333aef273348ad210eafb7c
Radium <2043869032@qq.com>
2023-11-10 12:14:39 +0800

    chore: commit

--- 读书系列/diadact-expain/index.js
+++ 读书系列/diadact-expain/index.js
@@ -1,3 +1,4 @@
+let rootInstance = null;
 const TEXT_ELEMENT = 'TEXT_ELEMENT';
 function createElement(type, config, ...args) {
     const props = Object.assign({}, config);
@@ -17,31 +18,84 @@ function createTextElement(value) {
  * render 函数
  *
  * @param {DidactElement} element
- * @param {Element} parentDom
+ * @param {Element} container
  */
-function render(element, parentDom) {
+function render(element, container) {
+    const prevInstace = rootInstance;
+    const nextInstance = reconcile(container, prevInstace, element);
+    rootInstance = nextInstance;
+}
+function reconcile(parentDom, instance, element) {
+    if (instance == null) {
+        const newInstance = instantiate(element);
+        parentDom.append(newInstance.dom);
+        return newInstance;
+    }
+    else if (instance.element.type === element.type) {
+        // 相同类型
+        updateDomProperties(instance.dom, instance.element.props, element.props);
+        instance.element = element;
+        return instance;
+    }
+    else {
+        // Replace instace
+        const newInstance = instantiate(element);
+        parentDom.replaceChild(newInstance.dom, instance.dom);
+        return newInstance;
+    }
+}
+// --------------- 运行一次 结束------
+// ------ 递归 - instantiate - 运行一次以上 -----
+function instantiate(element) {
     const { type, props } = element;
-    const isTextElement = type === 'TEXT ELEMENT'; // 文本类型判定
+    // Create DOM element
+    const isTextElement = type === TEXT_ELEMENT;
     const dom = isTextElement
         ? document.createTextNode('')
-        : document.createElement(type); // 创建-类型-element
-    const isListener = (name) => name.startsWith('on');
-    // 是否开头on
-    Object.keys(props)
-        .filter(_ => isListener(_))
+        : document.createElement(type);
+    updateDomProperties(dom, [], props);
+    // 1. dom 构造完成
+    // Instantiate and append children
+    const childElements = props.children || [];
+    // 2. 转折点-递归-孩子 -> 变 实例数组
+    const childInstances = childElements.map(_ => instantiate(_));
+    // 3. 获取 孩子-html-组件
+    const childDoms = childInstances.map(childInstance => childInstance.dom);
+    // eslint-disable-next-line unicorn/prefer-dom-node-append
+    childDoms.forEach(childDom => dom.appendChild(childDom));
+    const instance = { dom, element, childInstances };
+    return instance;
+}
+function updateDomProperties(dom, prevProps, nextProps) {
+    const isEvent = (name) => name.startsWith('on');
+    const isAttribute = (name) => !isEvent(name) && name !== 'children';
+    // preProps Remove
+    // Remove event listeners
+    Object.keys(prevProps)
+        .filter(_ => isEvent(_))
         .forEach(name => {
-        const eventType = name.toLowerCase().slice(2); // 取后两位
-        dom.addEventListener(eventType, props[name]);
+        const eventType = name.toLowerCase().slice(2);
+        dom.removeEventListener(eventType, prevProps[name]);
     });
-    const isAttribute = (name) => !isListener(name) && name !== 'children';
-    Object.keys(props)
+    // Remove attributes
+    Object.keys(prevProps)
         .filter(_ => isAttribute(_))
         .forEach(name => {
-        dom[name] = props[name];
+        dom[name] = null;
     });
-    const childElements = props.children || []; // 获取-孩子
-    childElements.forEach(childElement => render(childElement, dom));
-    if (parentDom instanceof HTMLElement)
-        parentDom.append(dom);
+    // nextProps Add
+    // Set attributes
+    Object.keys(nextProps)
+        .filter(_ => isAttribute(_))
+        .forEach(name => {
+        dom[name] = nextProps[name];
+    });
+    // Add event listeners
+    Object.keys(nextProps)
+        .filter(_ => isEvent(_))
+        .forEach(name => {
+        const eventType = name.toLowerCase().slice(2);
+        dom.addEventListener(eventType, nextProps[name]);
+    });
 }
-export { render };
+export { render, createElement };
--- 读书系列/diadact-expain/index.ts
+++ 读书系列/diadact-expain/index.ts
@@ -6,6 +6,7 @@ type DidactElement = {
   } & Record<string, any>;
 };
 
+let rootInstance: Record<string, any> | null = null;
 const TEXT_ELEMENT = 'TEXT_ELEMENT';
 
 function createElement(
@@ -36,36 +37,101 @@ function createTextElement(value: string) {
  * render 函数
  *
  * @param {DidactElement} element
- * @param {Element} parentDom
+ * @param {Element} container
  */
-function render(element: DidactElement, parentDom: HTMLElement | Text) {
-  const { type, props } = element;
-  const isTextElement = type === 'TEXT ELEMENT'; // 文本类型判定
-  const dom = isTextElement
-    ? document.createTextNode('')
-    : document.createElement(type); // 创建-类型-element
-
-  const isListener = (name: string) => name.startsWith('on');
-  // 是否开头on
-  Object.keys(props)
-    .filter(_ => isListener(_))
-    .forEach(name => {
-      const eventType = name.toLowerCase().slice(2); // 取后两位
-      dom.addEventListener(eventType, props[name]);
-    });
-
-  const isAttribute = (name: string) =>
-    !isListener(name) && name !== 'children';
-  Object.keys(props)
-    .filter(_ => isAttribute(_))
-    .forEach(name => {
-      (dom as Record<string, any>)[name] = props[name];
-    });
-
-  const childElements = props.children || []; // 获取-孩子
-  childElements.forEach(childElement => render(childElement, dom));
-
-  if (parentDom instanceof HTMLElement) parentDom.append(dom);
+function render(element: DidactElement, container: HTMLElement) {
+  const prevInstace = rootInstance;
+  const nextInstance = reconcile(container, prevInstace, element);
+  rootInstance = nextInstance;
 }
 
-export { render };
+function reconcile(
+  parentDom: HTMLElement,
+  instance: Record<string, any> | null,
+  element: DidactElement,
+) {
+  if (instance == null) {
+    const newInstance = instantiate(element);
+    parentDom.append(newInstance.dom);
+    return newInstance;
+  } else if (instance.element.type === element.type) {
+    // 相同类型
+    updateDomProperties(instance.dom, instance.element.props, element.props);
+    instance.element = element;
+    return instance;
+  } else {
+    // Replace instace
+    const newInstance = instantiate(element);
+    parentDom.replaceChild(newInstance.dom, instance.dom);
+    return newInstance;
+  }
+}
+
+// --------------- 运行一次 结束------
+// ------ 递归 - instantiate - 运行一次以上 -----
+function instantiate(element: DidactElement): Record<string, any> {
+  const { type, props } = element;
+
+  // Create DOM element
+  const isTextElement = type === TEXT_ELEMENT;
+  const dom = isTextElement
+    ? document.createTextNode('')
+    : document.createElement(type);
+
+  updateDomProperties(dom, [], props);
+
+  // 1. dom 构造完成
+  // Instantiate and append children
+  const childElements = props.children || [];
+  // 2. 转折点-递归-孩子 -> 变 实例数组
+  const childInstances = childElements.map(_ => instantiate(_));
+  // 3. 获取 孩子-html-组件
+  const childDoms = childInstances.map(childInstance => childInstance.dom);
+  // eslint-disable-next-line unicorn/prefer-dom-node-append
+  childDoms.forEach(childDom => dom.appendChild(childDom));
+  const instance = { dom, element, childInstances };
+  return instance;
+}
+
+function updateDomProperties(
+  dom: HTMLElement | Text,
+  prevProps: Record<string, any>,
+  nextProps: Record<string, any>,
+) {
+  const isEvent = (name: string) => name.startsWith('on');
+  const isAttribute = (name: string) => !isEvent(name) && name !== 'children';
+
+  // preProps Remove
+  // Remove event listeners
+  Object.keys(prevProps)
+    .filter(_ => isEvent(_))
+    .forEach(name => {
+      const eventType = name.toLowerCase().slice(2);
+      dom.removeEventListener(eventType, prevProps[name]);
+    });
+
+  // Remove attributes
+  Object.keys(prevProps)
+    .filter(_ => isAttribute(_))
+    .forEach(name => {
+      (dom as Record<string, any>)[name] = null;
+    });
+
+  // nextProps Add
+  // Set attributes
+  Object.keys(nextProps)
+    .filter(_ => isAttribute(_))
+    .forEach(name => {
+      (dom as Record<string, any>)[name] = nextProps[name];
+    });
+
+  // Add event listeners
+  Object.keys(nextProps)
+    .filter(_ => isEvent(_))
+    .forEach(name => {
+      const eventType = name.toLowerCase().slice(2);
+      dom.addEventListener(eventType, nextProps[name]);
+    });
+}
+
+export { render, createElement };
