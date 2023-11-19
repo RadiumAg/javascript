90825a3d0f901aabcbfaee9e7ca14fd9f1593bdf
Radium <2043869032@qq.com>
2023-11-19 22:16:55 +0800

    chore: commit

--- 读书系列/react设计原理/packages/react-reconclier/src/fiberFlags.ts
+++ 读书系列/react设计原理/packages/react-reconclier/src/fiberFlags.ts
@@ -1,10 +1,9 @@
 type Flags = number;
 
 const NoFlags = 0b0000001;
-const Flag = 0b0000010;
 const Placement = 0b000010;
 const Update = 0b000100;
 const ChildDeletion = 0b001000;
 
-export { NoFlags, Flag, Placement, Update, ChildDeletion };
+export { NoFlags, Placement, Update, ChildDeletion };
 export type { Flags };
