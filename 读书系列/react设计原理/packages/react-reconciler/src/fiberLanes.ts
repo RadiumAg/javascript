fb3cf68d49c7354e232b4ecb06237a431b5f1157
RadiumAg <2043869032@qq.com>
2023-12-13 10:32:27 +0000

    chore: commit

--- 读书系列/react设计原理/packages/react-reconciler/src/fiberLanes.ts
+++ 读书系列/react设计原理/packages/react-reconciler/src/fiberLanes.ts
@@ -1,5 +1,8 @@
 import {
+  unstable_IdlePriority,
   unstable_ImmediatePriority,
+  unstable_NormalPriority,
+  unstable_UserBlockingPriority,
   unstable_getCurrentPriorityLevel,
 } from 'scheduler';
 import { FiberRootNode } from './fiber';
@@ -9,11 +12,15 @@ type Lanes = number;
 const SyncLane = 0b0001;
 const NoLane = 0b0000;
 const NoLanes = 0b0000;
+const InputContinuonusLane = 0b0010;
+const DefaultLane = 0b0100;
+const IdleLane = 0b1000;
 
 function requestUpdateLanes() {
   const currentSchedulerPriority = unstable_getCurrentPriorityLevel();
+  const lane = schedulerPriorityToLane(currentSchedulerPriority);
 
-  return SyncLane;
+  return lane;
 }
 
 function lanesToSchedulePriority(lanes: Lanes) {
@@ -22,6 +29,31 @@ function lanesToSchedulePriority(lanes: Lanes) {
   if (lane === SyncLane) {
     return unstable_ImmediatePriority;
   }
+
+  if (lane === InputContinuonusLane) {
+    return unstable_UserBlockingPriority;
+  }
+  if (lane === DefaultLane) {
+    return unstable_NormalPriority;
+  }
+
+  return unstable_IdlePriority;
+}
+
+function schedulerPriorityToLane(schedulePriority: number) {
+  if (schedulePriority === unstable_ImmediatePriority) {
+    return SyncLane;
+  }
+
+  if (schedulePriority === unstable_UserBlockingPriority) {
+    return InputContinuonusLane;
+  }
+
+  if (schedulePriority === unstable_NormalPriority) {
+    return DefaultLane;
+  }
+
+  return NoLane;
 }
 
 function mergeLanes(laneA: Lane, laneB: Lane) {
@@ -40,9 +72,13 @@ export type { Lane, Lanes };
 export {
   SyncLane,
   NoLane,
+  IdleLane,
   NoLanes,
   mergeLanes,
+  DefaultLane,
   markRootFinished,
   requestUpdateLanes,
+  InputContinuonusLane,
   getHighestPriorityLane,
+  lanesToSchedulePriority,
 };
