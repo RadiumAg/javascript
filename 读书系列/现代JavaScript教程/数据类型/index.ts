b054d3833f7f2f868a6c02d97a49054bb872a493
Radium <2043869032@qq.com>
2023-12-30 21:52:09 +0800

    chore: commit

--- 读书系列/现代JavaScript教程/数据类型/index.ts
+++ 读书系列/现代JavaScript教程/数据类型/index.ts
@@ -3,3 +3,12 @@
   const str = 'Hello';
   console.log(str.toUpperCase());
 })();
+
+// 数组方法
+(() => {
+  // splice
+  const arr = ['1', 'go', 'home'];
+  delete arr[1];
+  console.log(arr[1]);
+  console.log(arr.length);
+})();
