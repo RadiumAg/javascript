65b75572faaae1705c1123b939f94dfd05de8b28
Radium <2043869032@qq.com>
2023-12-31 21:56:51 +0800

    chore: commit

--- 读书系列/现代JavaScript教程/数据类型/index.ts
+++ 读书系列/现代JavaScript教程/数据类型/index.ts
@@ -4,39 +4,56 @@
   console.log(str.toUpperCase());
 })();
 
-// 数组方法
-(() => {
-  const arr = ['1', 'go', 'home'];
-  delete arr[1];
-  console.log(arr[1]);
-  console.log(arr.length);
-})();
-
 // splice
-// 从索引1删除一个元素
 (() => {
-  const arr = ['I', 'study', 'JavaScript'];
-  arr.splice(1, 1); // 从索引1 开始删除1个元素
-  console.log(arr);
+  // 从索引1删除一个元素
+  (() => {
+    const arr = ['I', 'study', 'JavaScript'];
+    arr.splice(1, 1); // 从索引1 开始删除1个元素
+    console.log(arr);
+  })();
+
+  // 用另外两个元素替换它们
+  (() => {
+    const arr = ['I', 'study', 'JavaScript'];
+    arr.splice(0, 3, "Let's", 'dance');
+    console.log(arr);
+  })();
+
+  // 在这里我们可以看到splice返回了被删除的元素所组成的数组
+  (() => {
+    const arr = ['I', 'study', 'JavaScript', 'right', 'now'];
+    // 删除前两个元素
+    const removed = arr.splice(0, 2);
+    console.log(removed);
+  })();
+
+  (() => {
+    const arr = ['I', 'study', 'JavaScript'];
+    arr.splice(2, 0, 'complex', 'language');
+    console.log(arr);
+  })();
 })();
 
-// 用另外两个元素替换它们
+// concat
 (() => {
-  const arr = ['I', 'study', 'JavaScript'];
-  arr.splice(0, 3, "Let's", 'dance');
-  console.log(arr);
-})();
+  (() => {
+    const arr = [1, 2];
+    console.log(arr.concat([3, 4]));
+    console.log(arr.concat([3, 4], [5, 6]));
+    console.log(arr.concat([3, 4], 5, 6));
+  })();
 
-// 在这里我们可以看到splice返回了被删除的元素所组成的数组
-(() => {
-  const arr = ['I', 'study', 'JavaScript', 'right', 'now'];
-  // 删除前两个元素
-  const removed = arr.splice(0, 2);
-  console.log(removed);
-})();
+  // arrayLike
+  (() => {
+    const arr = [1, 2];
+    const arrayLike = {
+      0: 'something',
+      length: 1,
+    };
 
-(() => {
-  const arr = ['I', 'study', 'JavaScript'];
-  arr.splice(2, 0, 'complex', 'language');
-  console.log(arr);
+    console.log(arr.concat(arrayLike));
+  })();
+
+  
 })();
