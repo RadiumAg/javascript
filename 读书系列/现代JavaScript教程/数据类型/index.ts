00f5113fb1178813d41b4759e3117830887c60c0
RadiumAg <2043869032@qq.com>
2024-01-04 02:52:44 +0000

    chore: commit

--- 读书系列/现代JavaScript教程/数据类型/index.ts
+++ 读书系列/现代JavaScript教程/数据类型/index.ts
@@ -144,3 +144,29 @@
 
   console.log(users.filter(item => item.id < 3));
 })();
+
+// 可跌代对象
+(() => {
+  // Symbol.iterator
+  (() => {
+    const range = {
+      from: 1,
+      to: 5,
+    };
+
+    range[Symbol.iterator] = function () {
+      return {
+        current: this.from,
+        last: this.to,
+
+        next() {
+          if (this.current <= this.last) {
+            return { done: false, value: this.current++ };
+          } else {
+            return { done: true };
+          }
+        },
+      };
+    };
+  })();
+})();
