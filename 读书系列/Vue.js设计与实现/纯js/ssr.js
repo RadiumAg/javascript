6b1aebc75ecdb8636bed8cda1bba82720cb53e49
Radium <2043869032@qq.com>
2024-01-10 23:50:23 +0800

    chore: commit

--- 读书系列/Vue.js设计与实现/纯js/ssr.js
+++ 读书系列/Vue.js设计与实现/纯js/ssr.js
@@ -1,23 +1,27 @@
 /* eslint-disable no-restricted-syntax */
 const ElementVNode = {
-  type: 'div',
+  type: 'area',
   props: {
     id: 'foo',
   },
   children: [{ type: 'p', children: 'hello' }],
 };
+const VOID_TAGS =
+  'area,base,br,col,embed,hr,img,input,link,meta,param,source,track,wbr';
+
 function renderElementVNode(vnode) {
   const { type: tag, props, children } = vnode;
+  // 判断是否是闭合标签
+  const isVoidElement = VOID_TAGS.includes(tag);
 
   let ret = `<${tag}`;
 
   if (props) {
-    for (const k in props) {
-      ret += ` ${k}="${props[k]}"`;
-    }
+    ret += renderAttrs(props);
   }
 
-  ret += `>`;
+  ret += isVoidElement ? `/>` : `>`;
+  if (isVoidElement) return ret;
 
   if (typeof children === 'string') {
     ret += children;
@@ -30,4 +34,40 @@ function renderElementVNode(vnode) {
   ret += `</${tag}>`;
   return ret;
 }
+
+const shouldIgnoreProp = ['key', 'ref'];
+function renderAttrs(props) {
+  let ret = '';
+
+  for (const key in props) {
+    if (shouldIgnoreProp.includes(key) || /^on[^a-z]/.test(key)) {
+      continue;
+    }
+    const value = props[key];
+    ret += renderDynamicAttr(key, value);
+  }
+
+  return ret;
+}
+
+const isBooleanAttr = key =>
+  `itemscope,allowfullscreen,formnovalidate,ismap,nomodule,novalidate,readonly,async,autofocus,autoplay,controls,default,defer,disabled,hidden,loop,open,required,reversed,scoped,semaless,checked,muted,multiple,selected`
+    .split(',')
+    .includes(key);
+
+const isSSRSafeAttrName = key => !/[ "'/09=acu|]/.test(key);
+
+function renderDynamicAttr(key, value) {
+  if (isBooleanAttr(attr)) {
+    return value === false ? `` : ` ${key}`;
+  } else if (isSSRSafeAttrName(key)) {
+    return value === '' ? ` ${key}` : ` $[key]="${escapeHtml(value)}"`;
+  } else {
+    console.warn(
+      `[@vue/server-renderer] Skipped rendering unsafe attribute name: ${name}`,
+    );
+    return ``;
+  }
+}
+
 console.log(renderElementVNode(ElementVNode)); // <div id="foo"><p>hello</p></div>
