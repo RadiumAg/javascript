61b4217ddc69865c857b54bf50f059d8598f5af5
Radium <2043869032@qq.com>
2024-01-21 14:15:39 +0800

    chore: commit

--- 读书系列/JavaScript高级程序设计/变量、作用域与内存/index.ts
+++ 读书系列/JavaScript高级程序设计/变量、作用域与内存/index.ts
@@ -1,9 +1,5 @@
 // primitive value 原始值  Undefined ,Null ,Boolean,Number,String,Symbol
 // reference value 引用值
-
-(() => {
-  console.log(typeof null); //object
-})();
 // typeof 是判断原始类型最好的方式
 // instanceof 是判断应用类型的最好方式
 (() => {
