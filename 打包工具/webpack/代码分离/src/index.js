9fb0c31fc85c1abe0acaee998d5504f4aeb9e064
Radium <2043869032@qq.com>
2024-01-27 23:17:42 +0800

    chore: commit

--- 打包工具/webpack/代码分离/src/index.js
+++ 打包工具/webpack/代码分离/src/index.js
@@ -3,12 +3,9 @@ import _ from 'lodash';
 function component() {
   const elememt = document.createElement('div');
   const btn = document.createElement('button');
-
   elememt.innerHTML = _.join(['Hello', 'webpack'], ' ');
   elememt.classList.add('hello');
-
   btn.innerHTML = 'Click me and check the console!';
-
   elememt.append(btn);
 
   return elememt;
