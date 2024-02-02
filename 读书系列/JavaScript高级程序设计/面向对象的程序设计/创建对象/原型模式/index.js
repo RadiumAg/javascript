223d1114b07cb21770d2978c4664cc5240e80a2d
Radium <2043869032@qq.com>
2024-02-02 23:45:24 +0800

    chore: commit

--- 读书系列/JavaScript高级程序设计/面向对象的程序设计/创建对象/原型模式/index.js
+++ 读书系列/JavaScript高级程序设计/面向对象的程序设计/创建对象/原型模式/index.js
@@ -15,3 +15,7 @@ const person2 = new Person();
 person2.sayName(); //"Nicholas"
 
 console.log(person1.sayName == person2.sayName); //true
+
+// ECMA-262使用一些内部特性来描述属性的特征来描述属性的特征。
+// 这些特性是由为JavaScript实现引擎的规范定义的。因此，开发者不能在JavaScript中直接访问这些特性
+// 为了将某个特性标识为内部特性，规范会用两个中阔号把名称括起来，比如[[[Enumerable]]]
