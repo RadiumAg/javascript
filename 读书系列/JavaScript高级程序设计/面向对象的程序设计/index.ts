a744ecd2832fd114a713071996fa1b040ab28c77
Radium <2043869032@qq.com>
2024-02-04 22:46:42 +0800

    chore: commit

--- 读书系列/JavaScript高级程序设计/面向对象的程序设计/index.ts
+++ 读书系列/JavaScript高级程序设计/面向对象的程序设计/index.ts
@@ -1,4 +1,4 @@
-(() => {
+() => {
   function Person() {}
   Person.prototype.name = 'Nicholas';
   Person.prototype.age = 29;
@@ -14,9 +14,9 @@
   person2.sayName(); //"Nicholas"
 
   console.log(person1.sayName == person2.sayName); //true
-})();
+};
 
-(
+() => {
   // ECMA-262使用一些内部特性来描述属性的特征来描述属性的特征。
   // 这些特性是由为JavaScript实现引擎的规范定义的。因此，开发者不能在JavaScript中直接访问这些特性
   // 为了将某个特性标识为内部特性，规范会用两个中阔号把名称括起来，比如[[[Enumerable]]]
@@ -27,20 +27,18 @@
   // 2.[[Enumerable]：表示属性是否可以通过for in循环返回。默认情况下，所有直接定义在对象上的属性的这个特性都是true，如前面的例子所示
   // 3.[[Writable]]: 表示属性的值是否可以呗修改.默认情况下,所有直接定义在对象上的属性的这个特性都是true,如前面的例子所示
   // 4.[[Value]]: 包含属性实际的值.这就是前面提到的那个读取和写入属性值的位置.这个特性的默认值为undefined.
-  () => {
-    const person = {};
-    Object.defineProperty(person, 'name', {
-      writable: false,
-      value: 'Nicholas',
-    });
+  const person = {};
+  Object.defineProperty(person, 'name', {
+    writable: false,
+    value: 'Nicholas',
+  });
 
-    console.log(person.name);
-    person.name = 'Greg';
-    console.log(person.name);
-  }
-)();
+  console.log(person.name);
+  person.name = 'Greg';
+  console.log(person.name);
+};
 
-(() => {
+() => {
   const person = {};
   Object.defineProperty(person, 'name', {
     configurable: false,
@@ -51,10 +49,10 @@
     configurable: true,
     value: 'Nicholas',
   });
-})();
+};
 
 // 2. 属性访问器
-(() => {
+() => {
   const book = {
     year_: 2017,
     edition: 1,
@@ -73,12 +71,11 @@
   });
 
   book.year = 2018;
-})();
+};
 
 // 合并对象
 (() => {
   let dest, src, result;
-
   dest = {};
   src = { id: 'src' };
   result = Object.assign(dest, src);
@@ -90,5 +87,23 @@
 
   dest = {};
   src = { id: 'src' };
-  result = Object.assign(test, {a: 'foo'})
+  result = Object.assign(dest, { a: 'foo' });
+  console.log(result);
+
+  // 获取函数与设置函数
+
+  dest = {
+    set a(val) {
+      console.log(`Invoked dest setter with param ${val}`);
+    },
+  };
+
+  src = {
+    get a() {
+      console.log(`Invoked src getter`);
+      return 'fpp';
+    },
+  };
+
+  Object.assign(dest, src);
 })();
