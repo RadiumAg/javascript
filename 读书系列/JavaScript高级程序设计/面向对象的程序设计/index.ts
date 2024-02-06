f8238570c9612e3196a919625410513e52d0d30b
Radium <2043869032@qq.com>
2024-02-06 22:52:00 +0800

    chore: commit

--- 读书系列/JavaScript高级程序设计/面向对象的程序设计/index.ts
+++ 读书系列/JavaScript高级程序设计/面向对象的程序设计/index.ts
@@ -118,4 +118,12 @@
   console.log(-0 === 0); // true
   console.log(NaN === NaN);
   console.log(isNaN(NaN));
+
+  console.log(Object.is(true, 1)); // false
+  console.log(Object.is({}, {})); // false
+  console.log(Object.is('2', 2)); // false
+  console.log(Object.is(+0, -0)); // false
+  console.log(Object.is(+0, 0)); // true
+  console.log(Object.is(-0, 0)); // true
+  console.log(Object.is(NaN, NaN)); // true
 })();
