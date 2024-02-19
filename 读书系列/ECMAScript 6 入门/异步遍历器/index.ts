cefa7005f47c401229a6e6b0fab033ed260ce80e
RadiumAg <2043869032@qq.com>
2024-02-19 08:02:04 +0000

    chore: commit

--- 读书系列/ECMAScript 6 入门/异步遍历器/index.ts
+++ 读书系列/ECMAScript 6 入门/异步遍历器/index.ts
@@ -1,3 +1,36 @@
+async function* createAsyncIterable(syncIterable) {
+  for (const elem of syncIterable) {
+    yield elem;
+  }
+}
+
+(() => {
+  const asyncIterable = createAsyncIterable(['a', 'b']);
+  const asyncIterator = asyncIterable[Symbol.asyncIterator]();
+
+  asyncIterator
+    .next()
+    .then(iterResult1 => {
+      console.log(iterResult1);
+      return asyncIterator.next();
+    })
+    .then(iterResult2 => {
+      console.log(iterResult2);
+      return asyncIterable.next();
+    })
+    .then(iterResult3 => {
+      console.log(iterResult3);
+    });
+
+  async function f() {
+    const asyncIterable = createAsyncIterable(['a', 'b']);
+    const asyncIterator = asyncIterable[Symbol.asyncIterator]();
+    console.log(await asyncIterator.next());
+    console.log(await asyncIterator.next());
+    console.log(await asyncIterator.next());
+  }
+})();
+
 // 同步遍历器
 (() => {
   function idMaker() {
