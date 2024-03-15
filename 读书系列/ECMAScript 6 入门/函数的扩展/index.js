a132962891ffbf77f2db1bddb3e932a772c335f9
RadiumAg <2043869032@qq.com>
2024-03-15 08:09:58 +0000

    chore: op  code

--- 读书系列/ECMAScript 6 入门/函数的扩展/index.js
+++ 读书系列/ECMAScript 6 入门/函数的扩展/index.js
@@ -18,7 +18,6 @@
 })();
 
 // 箭头函数也不存在 arguments,super和new.target
-
 (() => {
   function foo() {
     setTimeout(() => {
@@ -27,4 +26,44 @@
   }
 
   console.log(foo(2, 3, 4, 6, 8)); //args: [2, 3, 4, 6, 8]
+})();
+
+// 函数参数默认值
+// 与解构赋值默认值结合使用
+(() => {
+  function foo({ x, y = 5 } = {}) {
+    console.log(x, y);
+  }
+
+  foo({});
+  foo({ x: 1 });
+  foo({ x: 1, y: 2 });
+  foo();
+})();
+
+(() => {
+  function foo({ x, y = 5 } = {}) {
+    console.log(x, y);
+  }
+  foo();
+})();
+
+// 参数默认值的位置
+(() => {
+  function f(x = 1, y) {
+    return [x, y];
+  }
+
+  f();
+  f(2);
+  f(undefined, 1);
+})();
+
+// 如果传入undefined，将触发该参数等于默认值，null则没有这个效果
+(() => {
+  function foo(x = 5, y = 6) {
+    console.log(x, y);
+  }
+
+  foo(undefined, null);
 })();
