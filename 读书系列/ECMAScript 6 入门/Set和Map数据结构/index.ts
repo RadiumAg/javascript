2ca92904e065e9340470fd2f4aefd98308e79548
RadiumAg <2043869032@qq.com>
2024-03-25 03:55:33 +0000

    chore: op code

--- 读书系列/ECMAScript 6 入门/Set和Map数据结构/index.ts
+++ 读书系列/ECMAScript 6 入门/Set和Map数据结构/index.ts
@@ -7,16 +7,28 @@
   }
 };
 
-(() => {
+() => {
   const set = new Set();
   // add 返回自身
   set.add(Number.NaN).add(Number.NaN);
 
   console.log(set.entries());
-})();
+};
 
 // WeakSet的成员只能是对象和Symbol值
 () => {
   const ws = new WeakSet();
   ws.add(Symbol());
 };
+
+// FinalizationRegistry
+(() => {
+  // 新建一个注册表实例
+  let a = { a: 1 };
+  const register = new FinalizationRegistry(value => {
+    console.log(value);
+  });
+
+  register.register(a, 'some value');
+  a = null;
+})();
