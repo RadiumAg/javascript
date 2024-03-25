b345989bdbcad21aad84c640b32999e67fd62d64
RadiumAg <2043869032@qq.com>
2024-03-25 06:38:33 +0000

    chore: op code

--- 读书系列/ECMAScript 6 入门/字符串的扩展/index.js
+++ 读书系列/ECMAScript 6 入门/字符串的扩展/index.js
@@ -43,4 +43,49 @@
   // U+00A: 换行符
 })();
 
-(() => {})();
+(() => {
+  () => {
+    // String.fromCodePoint()
+    // 这个方法不能识别码点大于0xFFFF的字符
+    console.log(String.fromCharCode(0x20bb7));
+
+    // String.fromCodePoint()方法，可以识别大于0xFFFF的马甸
+    console.log(String.fromCodePoint(0x20bb7));
+  };
+
+  // String.raw()
+  (() => {
+    console.log(String.raw`Hi\n${2 + 3}`);
+    // 实际返回“Hi\\n5!”，显示的是转义后的结构"HI\n5!"
+
+    console.log(String.raw`Hi\u000A!`);
+    // 实际发返回'Hi\\u000A!'，显示的是转以后的结果“Hi\u000A!”
+  })();
+})();
+
+(() => {
+  (() => {
+    'x'.repeat(3);
+    'hello'.repeat(2);
+    'na'.repeat(0);
+  })();
+
+  (() => {
+    'x'.repeat(3);
+    'hello'.repeat(2);
+    'na'.repeat(0);
+  })();
+})();
+
+/**
+$&：匹配的字符串。
+$` ：匹配结果前面的文本。
+$'：匹配结果后面的文本。
+$n：匹配成功的第n组内容，n是从1开始的自然数。这个参数生效的前提是，第一个参数必须是正则表达式。
+$$：指代美元符号$。
+*/
+(() => {
+  console.log('abbc'.replaceAll('b', '$&'));
+  console.log('abbc'.replaceAll('b', '$`'));
+  console.log('abbc'.replaceAll('b', '$`'));
+})();
