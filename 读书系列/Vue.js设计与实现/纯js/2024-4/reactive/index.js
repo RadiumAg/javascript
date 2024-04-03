25bee1c02a44dcf042343b61ccbb27f7695e77fe
zly01858966 <zly01858966@lazada.com>
2024-04-03 10:47:11 +0800

    chore: op code

---
+++ 读书系列/Vue.js设计与实现/纯js/2024-4/reactive/index.js
@@ -0,0 +1,76 @@
+let activeEffect;
+const bucket = new WeakMap();
+// 用一个全局变量存储被注册的副作用函数
+
+function effect(fn) {
+  const effectFn = () => {
+    // 调用 cleanup 函数完成清理工作
+    cleanup(effectFn);
+    activeEffect = effectFn;
+    fn();
+  };
+
+  // activeEffect.deps 用来存储所有与该副作用函数相关联的依赖集合
+  effectFn.deps = [];
+  // 执行副作用函数
+  effectFn();
+}
+// 原始数据
+const data = { text: 'hello world' };
+
+// 对原始数据的代理
+const obj = new Proxy(data, {
+  get(target, key) {
+    track(target, key);
+    return target[key];
+  },
+
+  set(target, key) {
+    trigger(target, key);
+    return true;
+  },
+});
+
+function track(target, key) {
+  // 没有 activeEffect, 直接return
+  if (!activeEffect) return;
+  let depsMap = bucket.get(target);
+  if (!depsMap) {
+    bucket.set(target, (depsMap = new Map()));
+  }
+  let deps = depsMap.get(key);
+  if (!deps) {
+    depsMap.set(key, (deps = new Set()));
+  }
+  deps.add(activeEffect);
+  // deps 就是一个与当前副作用函数存在联系的依赖集合
+  // 将其添加到 activeEffect.deps 数组中
+  activeEffect.deps.push(deps);
+}
+
+function cleanup(effectFn) {
+  for (let i = 0; i < effectFn.deps.length; i++) {
+    const deps = effectFn.deps[i];
+    deps.delete(effectFn);
+  }
+  // 最后需要重置 effectFn.deps 数组
+  effectFn.deps.length = 0;
+}
+
+function trigger(target, key) {
+  const depsMap = bucket.get(target);
+  if (!depsMap) return;
+
+  const effect = depsMap.get(key);
+  const effectsToRun = new Set(effect);
+  effectsToRun &&
+    effectsToRun.forEach(effectFn => {
+      effectFn();
+    });
+}
+
+effect(() => {
+  document.body.textContent = obj.text;
+});
+
+export { effect };
