716d2c53dcecf5131ecddb6da277a96429311c81
zly01858966 <zly01858966@lazada.com>
2024-04-22 11:05:26 +0800

    chore: op code

--- 读书系列/Vue.js设计与实现/2024-4/render/index.js
+++ 读书系列/Vue.js设计与实现/2024-4/render/index.js
@@ -1,49 +1,39 @@
-/* eslint-disable no-restricted-syntax */
-function mountElement(vnode, container) {
-  // 使用vnode.tag作为标签名称创建 DOM 元素
-  const el = document.createElement(vnode.tag);
-  // 遍历 vnode.props 将属性、事件添加到 DOM 元素
-  for (const key in vnode.props) {
-    if (key.startsWith('on')) {
-      // 如果key以on开头,说明他是事件
-      el.addEventListener(key.slice(2).toLowerCase(), vnode.props[key]);
+function createRenderer(options) {
+  const { createElement, insert, setElementText } = options;
+
+  function mountElement(vnode, container) {
+    const el = createElement(vnode.type);
+    if (typeof vnode.children === 'string') {
+      setElementText(el, vnode.children);
     }
+
+    insert(el, container);
   }
 
-  // 处理 children
-  if (typeof vnode.children === 'string') {
-    // 如果 children 是字符串,则将其设置为文本内容
-    el.append(document.createTextNode(vnode.children));
-  } else if (Array.isArray(vnode.children)) {
-    vnode.children.forEach(child => renderer(child, el));
+  function patch(n1, n2, container) {}
+
+  function hydrate(vnode, container) {}
+
+  function renderer(vnode, container) {
+    if (vnode) {
+      patch(container._vnode, vnode, container);
+    } else if (container._vnode) {
+      container.innerHTML = '';
+    }
+
+    container._vnode = vnode;
   }
 
-  // 将元素添加到挂载点下
-  container.append(el);
+  return {
+    hydrate,
+    renderer,
+  };
 }
 
-function mountOptionComponent(vnode, container) {
-  // 调用组件函数，获取组件要渲染的内容（虚拟DOM）
-  const subtree = vnode.tag.render();
-  //  递归地调用 renderer 渲染 subtree
-  renderer(subtree, container);
-}
-
-function mountFunctionComponent(vnode, container) {
-  // 调用组件函数，获取组件要渲染的内容（虚拟DOM）
-  const subtree = vnode.tag();
-  //  递归地调用 renderer 渲染 subtree
-  renderer(subtree, container);
-}
-
-function renderer(vnode, container) {
-  if (typeof vnode.tag === 'string') {
-    mountElement(vnode, container);
-  } else if (typeof vnode.tag === 'object') {
-    mountOptionComponent(vnode, container);
-  } else if (typeof vnode.tag === 'function') {
-    mountFunctionComponent(vnode, container);
+function patch(n1, n2, container) {
+  if (!n1) {
+    mountElement(n2, container);
   }
 }
 
-export { renderer };
+export { createRenderer };
