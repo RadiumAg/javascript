0058b61f9df4e559641311064894dc80b09b09cb
zly01858966 <zly01858966@lazada.com>
2024-05-14 16:17:21 +0800

    chore: op code

--- 掘金/mini-react/index.js
+++ 掘金/mini-react/index.js
@@ -1,34 +1,76 @@
+const ELEMENT_TYPE = {
+  TEXT_ELEMENT: 'TEXT_ELEMENT',
+};
+
 function createElement(type, props, ...children) {
   return {
     type,
     props: props || {},
-    children,
+    children: children.map(child => {
+      if (typeof child === 'string') {
+        return createTextElement(child);
+      } else {
+        return child;
+      }
+    }),
   };
 }
 
+/**
+ * 创建文本节点
+ * @param {*} text
+ * @returns
+ */
+function createTextElement(text) {
+  return {
+    type: ELEMENT_TYPE.TEXT_ELEMENT,
+    props: {
+      nodeValue: text,
+      children: [],
+    },
+  };
+}
+
+function updateComponent(element, container) {
+  const { type, children } = element;
+
+  let dom = null;
+
+  if (type === ELEMENT_TYPE.TEXT_ELEMENT) {
+    dom = document.createTextNode(element.props.nodeValue);
+  } else {
+    dom = document.createElement(type);
+  }
+
+  container.append(dom);
+  children?.forEach(child => {
+    render(child, dom);
+  });
+}
+
 function updateFunctionComponent(element, container) {
-  const { props, type } = element;
+  const { type, props } = element;
   const children = type(props);
 
   render(children, container);
 }
 
-function updateComponent(element, container) {
-  const { props, type, children } = element;
-  const dom = document.createElement(type);
-  container.append(dom);
-
-  children.forEach(child => {
-    render(child, container);
-  });
-}
-
+/**
+ * 渲染器
+ * @param {*} element
+ * @param {*} container
+ */
 function render(element, container) {
   const { type } = element;
-  if (typeof type === 'function') {
-    updateFunctionComponent(element, container);
-  } else {
-    updateComponent(element, container);
+
+  switch (typeof type) {
+    case 'string':
+      updateComponent(element, container);
+      break;
+
+    case 'function':
+      updateFunctionComponent(element, container);
+      break;
   }
 }
 
