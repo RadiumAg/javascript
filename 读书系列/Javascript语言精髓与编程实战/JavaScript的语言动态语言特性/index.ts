4d319a9f72b8e45f043b2bcf4c965c5220f99a5b
zly01858966 <zly01858966@lazada.com>
2024-05-23 11:36:42 +0800

    chore: op code

--- 读书系列/Javascript语言精髓与编程实战/JavaScript的语言动态语言特性/index.ts
+++ 读书系列/Javascript语言精髓与编程实战/JavaScript的语言动态语言特性/index.ts
@@ -3,3 +3,22 @@
   x.constructor;
   x['constructor'];
 };
+
+// 检测值类型数据，字面量与包装类的性质
+(() => {
+  Object.prototype.getSelf = function () {
+    return this;
+  };
+
+  Object.prototype.getClass = function () {
+    return this.constructor;
+  };
+
+  Object.prototype.getTypeof = function () {
+    return typeof this;
+  };
+
+  Object.prototype.getInstanceof = function () {
+    return this instanceof this.getClass();
+  };
+})();
