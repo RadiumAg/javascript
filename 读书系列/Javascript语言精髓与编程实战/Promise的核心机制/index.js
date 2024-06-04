24eb2df20b7e8b946a9e862ed897ce6a492c9ed9
zly01858966 <zly01858966@lazada.com>
2024-06-04 14:04:20 +0800

    chore: op code

--- 读书系列/Javascript语言精髓与编程实战/Promise的核心机制/index.js
+++ 读书系列/Javascript语言精髓与编程实战/Promise的核心机制/index.js
@@ -3,3 +3,14 @@
  * 1. 使用 new Promise() 来创建一个promise
  * 2. 使用类方法Promise.XXX()包括.resolve()，.reject，.all()或.race等来获得一个promise
  */
+
+/**
+ * 当执行器executor在执行过程触发异常时，JavaScript引擎也将调用reject并作为reasean
+ *
+ */
+(() => {
+  const p = new Promise((resolve, reject) => {
+    throw new Error('出错了'); // 创建异常并抛出，相当于reject(new Error)
+    resolve(1);
+  });
+})();
