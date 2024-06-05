9f0e64105b3a31c4cb7e44760c33adede063b104
Radium <2043869032@qq.com>
2024-06-05 23:16:08 +0800

    chore: op code

--- 读书系列/Javascript语言精髓与编程实战/Promise的核心机制/index.js
+++ 读书系列/Javascript语言精髓与编程实战/Promise的核心机制/index.js
@@ -14,3 +14,23 @@
     resolve(1);
   });
 })();
+
+// Promise的其它类方法
+
+(() => {
+  // 得到一个rejected promise
+  const p = Promise.reject(1); // x 是任意值，如果不指定定则为undefined
+
+  // 得到一个resolved promise
+  const p = Promise.resolve(1); // x 是任意值，如果不指定则为undefined
+
+  // 尝试resolve所有元素
+  // -当所有元素都resolved，则得到一个将所有结果作为resolved array的promise；或
+  // -当任意一个元素rejected，则得到一个该结果 resean 的 rejected promise
+  const p = Promise.all([1]); // x 必须是可迭代对象（集合对象，或有迭代器的对象）
+
+  // 尝试resolve所有元素
+  // - 当其任一元素resolved或rejected，都将以该结果作为结果promise
+  // - （注，所有其它元素的状态是未确定的，并且他们的执行过程与结果不确定）
+  const p = Promise.race([1]);
+})();
