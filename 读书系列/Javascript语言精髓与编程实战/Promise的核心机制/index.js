3bf58d172c5d72c90b531aab5a80e0ee3878111a
Radium <2043869032@qq.com>
2024-06-12 22:20:53 +0800

    chore: op code

--- 读书系列/Javascript语言精髓与编程实战/Promise的核心机制/index.js
+++ 读书系列/Javascript语言精髓与编程实战/Promise的核心机制/index.js
@@ -46,3 +46,31 @@
     yield sleep('yild 2nd', 1000, 'value 2 now');
   }
 })();
+
+(() => {
+  const onFinally = () => {
+    try {
+      return f2();
+    } catch (e) {
+      return Promise.reject(e);
+    }
+  };
+
+  const thenFinally = x => {
+    const result = onFinally(); // 模拟调用f2()并返回result的过程
+    const p3 = Promise.resolve(result); // result 可能是值或Promise对象
+    const valueThunk = () => x; // 忽略result，返回x
+    return p3.then(valueThunk);
+  };
+
+  const catchFinally = reason => {
+    const result = onFinally();
+    const p3 = Promise.resolve(result);
+    const thrower = () => {
+      throw reason; // 将reason作为异常抛出
+    };
+    return p3.then(thrower);
+  };
+
+  const p3 = p2.then(thenFinally, catchFinally);
+})();
