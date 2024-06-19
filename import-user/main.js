215f4ffae83d8038f638655983b72cfd773303ca
Radium <2043869032@qq.com>
2024-06-20 01:58:23 +0800

    chore: op code

--- import-user/main.js
+++ import-user/main.js
@@ -44,7 +44,27 @@ const getAllUers = async () => {
   const userResult = [];
 
   for (const department of departments) {
-    const { dept_id } = department;
+    const { dept_id, name: depName } = department;
+
+    // executeQuery(`INSERT INTO scm_puxi.department
+    // (dep_Id,
+    // dep_Name,
+    // dep_ParentId,
+    // dep_Sort,
+    // is_Enable,
+    // dep_CreateDate,
+    // dep_CreateUserId,
+    // dep_SalesChannelId)
+    // VALUES(
+    //  '${dept_id}',
+    //  '${depName}',
+    // NULL,
+    // NULL,
+    // NULL,
+    // NULL,
+    // NULL,
+    // NULL);
+    // `);
 
     const {
       result: { list },
@@ -54,56 +74,75 @@ const getAllUers = async () => {
       data: { dept_id, cursor: 1, size: 10 },
     }).then(result => result.data);
 
-    userResult.push(...list);
+    // userResult.push(...list);
+
+    for (const [index, user] of Object.entries(list)) {
+      executeQuery(`
+      INSERT INTO scm_puxi.userinfo_department
+(user_dep_RelationId,
+user_Id,
+dep_Id,
+is_Leader,
+user_dep_CreateDate,
+user_dep_CreateUserId)
+VALUES
+('${user.userid + dept_id}',
+'${user.userid}',
+'${dept_id}',
+NULL,
+NULL,
+NULL);
+`);
+    }
   }
 
-  for (const user of userResult) {
-    const { userid, name } = user;
-    executeQuery(`INSERT INTO scm_puxi.userinfo
-    (user_Id,
-    user_Name,
-    user_Password,
-    user_Avatar,
-    user_State,
-    user_Type,
-    user_Position,
-    user_Tel,
-    user_JobTitle,
-    user_JobNumber,
-    basic_Id_Company,
-    user_JoinDate,
-    user_CreateDate,
-    user_CreateUserId,
-    is_Insider,
-    managerId,
-    handoverUserId,
-    feishu_User_Id,
-    appid,
-    user_brandId,
-    user_salesChannelId)
-    VALUES
-    ('${userid}',
-     '${name}',
-      NULL,
-      NULL,
-    1,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL)`);
-  }
+  // for (const user of userResult) {
+  //   const { userid, name } = user;
+  //   executeQuery(`INSERT INTO scm_puxi.userinfo
+  //   (user_Id,
+  //   user_Name,
+  //   user_Password,
+  //   user_Avatar,
+  //   user_State,
+  //   user_Type,
+  //   user_Position,
+  //   user_Tel,
+  //   user_JobTitle,
+  //   user_JobNumber,
+  //   basic_Id_Company,
+  //   user_JoinDate,
+  //   user_CreateDate,
+  //   user_CreateUserId,
+  //   is_Insider,
+  //   managerId,
+  //   handoverUserId,
+  //   feishu_User_Id,
+  //   appid,
+  //   user_brandId,
+  //   user_salesChannelId)
+  //   VALUES
+  //   ('${userid}',
+  //    '${name}',
+  //     NULL,
+  //     NULL,
+  //   1,
+  //   NULL,
+  //   NULL,
+  //   NULL,
+  //   NULL,
+  //   NULL,
+  //   NULL,
+  //   NULL,
+  //   NULL,
+  //   NULL,
+  //   NULL,
+  //   NULL,
+  //   NULL,
+  //   NULL,
+  //   NULL,
+  //   NULL,
+  //   NULL)`);
+  // }
 };
 
 getAllUers();
