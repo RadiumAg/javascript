1c6b78a126d98d85381c87133d78a50f43921817
zly01858966 <zly01858966@lazada.com>
2024-07-11 17:00:50 +0800

    chore: op code

--- 读书系列/Javascript语言精髓与编程实战/JavaScript的对象系统/index.ts
+++ 读书系列/Javascript语言精髓与编程实战/JavaScript的对象系统/index.ts
@@ -50,3 +50,38 @@
   // 并且恢复到原型的值“prototype”'s name
   console.log(obj.name);
 })();
+
+// super对一般属性的意义
+(() => {
+  // 父类和父类的.prototype原型中的属性
+  class MyObject {}
+  MyObject.prototype.x = 100;
+
+  class MyObjectEx extends MyObject {
+    foo() {
+      console.log(super.x);
+    }
+  }
+  const obj = new MyObjectEx();
+
+  // 示例1
+  // - obj.foo()通过super访问到x值
+  obj.foo();
+  // obj 通过原型继承访问到的 x 值
+  console.log(obj.x); // 100
+
+  // 示例2
+  // - 修改对象实例的x值
+  obj.x = 200;
+  console.log(obj.x); // 200
+  // - 通过super访问到的原型值是不变化的
+  obj.foo();
+
+  // 示例3
+  // - 修改原型的x值
+  MyObject.prototype.x = 300;
+  // - 通过super访问到的原型值是变化的
+  obj.foo();
+  // - 对象的自由属性（这里是覆盖了继承属性）不受影响
+  console.log(obj.x);
+})();
