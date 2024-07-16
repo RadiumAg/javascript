d74903d98da6d9b13d43e60277da1da0433bd0be
zly01858966 <zly01858966@lazada.com>
2024-07-16 13:37:39 +0800

    chore: op code

---
+++ 读书系列/c#入门经典/Learn/Program.cs
@@ -0,0 +1,33 @@
+﻿var cows = new List<Cow>();
+// 协变
+class 协变
+{
+
+  class Animal { }
+
+  class Cow : Animal
+  {
+    public string Name { get; set; }
+
+    public Cow(string name) : base()
+    {
+      this.Name = name;
+    }
+
+  }
+
+  class SuperCow : Animal
+  {
+    public string Name { get; set; }
+
+    public SuperCow(string name) : base()
+    {
+      this.Name = name;
+    }
+
+  }
+  static Main()
+  {
+    var cows = new List<Cow>();
+  }
+}
