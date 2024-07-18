b8bb22b4eba344dc1f766e504042ccec206b398e
Radium <2043869032@qq.com>
2024-07-18 21:46:59 +0800

    chore: op code

--- 读书系列/react设计原理-2/demos/test-fc/main.tsx
+++ 读书系列/react设计原理-2/demos/test-fc/main.tsx
@@ -2,28 +2,7 @@ import * as React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-  const [state, setState] = React.useState([
-    { a: 1, key: 1 },
-    { a: 2, key: 2 },
-  ]);
-  const children = state.map((item, index) => (
-    <div className="item" key={index}>
-      {item.a}
-    </div>
-  ));
-
-  return (
-    <div
-      onClick={() => {
-        setState(() => {
-          const newState = [...state];
-          return newState.reverse();
-        });
-      }}
-    >
-      {children}
-    </div>
-  );
+  return <div>test-a</div>;
 };
 
 const root = ReactDOM.createRoot(document.querySelector('#root'));
