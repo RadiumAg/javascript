0c19fa518a2389c42f4aa21b8eca41ea3d7e3378
zly01858966 <zly01858966@lazada.com>
2024-07-22 16:16:27 +0800

    chore: op code

--- 读书系列/Javascript语言精髓与编程实战/Promise的核心机制/index.cjs
+++ 读书系列/Javascript语言精髓与编程实战/Promise的核心机制/index.cjs
@@ -8,32 +8,31 @@
  * 当执行器executor在执行过程触发异常时，JavaScript引擎也将调用reject并作为reasean
  *
  */
-(() => {
+() => {
   const p = new Promise((resolve, reject) => {
     throw new Error('出错了'); // 创建异常并抛出，相当于reject(new Error)
     resolve(1);
   });
-})();
+};
 
 // Promise的其它类方法
-
-(() => {
+() => {
   // 得到一个rejected promise
-  const p = Promise.reject(1); // x 是任意值，如果不指定定则为undefined
+  const p1 = Promise.reject(1); // x 是任意值，如果不指定定则为undefined
 
   // 得到一个resolved promise
-  const p = Promise.resolve(1); // x 是任意值，如果不指定则为undefined
+  const p2 = Promise.resolve(1); // x 是任意值，如果不指定则为undefined
 
   // 尝试resolve所有元素
   // -当所有元素都resolved，则得到一个将所有结果作为resolved array的promise；或
   // -当任意一个元素rejected，则得到一个该结果 resean 的 rejected promise
-  const p = Promise.all([1]); // x 必须是可迭代对象（集合对象，或有迭代器的对象）
+  const p3 = Promise.all([1]); // x 必须是可迭代对象（集合对象，或有迭代器的对象）
 
   // 尝试resolve所有元素
   // - 当其任一元素resolved或rejected，都将以该结果作为结果promise
   // - （注，所有其它元素的状态是未确定的，并且他们的执行过程与结果不确定）
-  const p = Promise.race([1]);
-})();
+  const p4 = Promise.race([1]);
+};
 
 (() => {
   function sleep(tag, n, value) {
@@ -47,7 +46,7 @@
   }
 })();
 
-(() => {
+() => {
   const onFinally = () => {
     try {
       return f2();
@@ -73,4 +72,17 @@
   };
 
   const p3 = p2.then(thenFinally, catchFinally);
+};
+
+// Promise.resolve处理thenable对象的具体方法
+(() => {
+  // 将x声明为一个thenable对象
+  const x = {
+    then() {
+      console.log('in thenable object ....');
+    },
+  };
+  const p2 = Promise.resolve(x);
+
+  console.log(p2 === x);
 })();
