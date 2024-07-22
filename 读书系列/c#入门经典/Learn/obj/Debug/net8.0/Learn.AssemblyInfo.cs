73899b014e62f49bd7d7ff6154a03780162bfa8e
zly01858966 <zly01858966@lazada.com>
2024-07-22 15:13:09 +0800

    chore: op code

--- 读书系列/c#入门经典/Learn/obj/Debug/net8.0/Learn.AssemblyInfo.cs
+++ 读书系列/c#入门经典/Learn/obj/Debug/net8.0/Learn.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Learn")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f14374359c244d4182dd52f1cd3e33a366bd5e8e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fd7cfc84d64691bc6ba65518a825e57a9291fd7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Learn")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Learn")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
