c6089db209073f909acf8a50502145f92eef67e1
Radium <2043869032@qq.com>
2024-08-13 21:30:58 +0800

    chore: op code

--- 读书系列/react设计原理-2/packages/shared/ReactSymbols.ts
+++ 读书系列/react设计原理-2/packages/shared/ReactSymbols.ts
@@ -3,3 +3,7 @@ const supportSymbol = typeof Symbol === 'function';
 export const REACT_ELEMENT_TYPE = supportSymbol
   ? Symbol.for('react.element')
   : 0xeac7;
+
+export const REACT_FRAGEMENT_TYPE = supportSymbol
+  ? Symbol.for('react.fragement')
+  : 0x3acb;
