3fba670eeeaad3687a20eaa63015920e4399024f
张镭银 <zly01858966@lazada.com>
2024-08-27 15:06:55 +0800

    chore: op code

--- 读书系列/你不知道的javascript/上卷/index.js
+++ 读书系列/你不知道的javascript/上卷/index.js
@@ -28,3 +28,26 @@
     console.log('button clicked');
   });
 })();
+
+// 现代的模块机制
+(() => {
+  const MyModules = (function Manager() {
+    const modules = {};
+
+    function define(name, deps, impl) {
+      for (let i = 0; i < deps.length; i++) {
+        deps[i] = modules[deps[i]];
+      }
+      modules[name] = impl.apply(impl, deps);
+    }
+
+    function get(name) {
+      return modules[name];
+    }
+
+    return {
+      define,
+      get,
+    };
+  })();
+})();
