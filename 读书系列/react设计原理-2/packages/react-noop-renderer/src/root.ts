96383491bd9a1f75a4db4616f50fe9bb7905714e
Radium <2043869032@qq.com>
2024-08-28 21:40:05 +0800

    chore: op code

--- 读书系列/react设计原理-2/packages/react-noop-renderer/src/root.ts
+++ 读书系列/react设计原理-2/packages/react-noop-renderer/src/root.ts
@@ -1,8 +1,10 @@
+/* eslint-disable unicorn/no-lonely-if */
 import {
   createContainer,
   updateContainer,
 } from 'react-reconciler/src/fiberReconciler';
 import { ReactElement } from 'shared/ReactTypes';
+import { REACT_ELEMENT_TYPE, REACT_FRAGEMENT_TYPE } from 'shared/ReactSymbols';
 import { Container, Instance } from './hostConfig';
 
 let idCounter = 0;
@@ -23,7 +25,75 @@ export function createRoot() {
     getChildren() {
       return getChildren(container);
     },
+
+    getChildrenAsJSX() {
+      return getChildrenAsJSX(container);
+    },
   };
+}
+
+function getChildrenAsJSX(root: Container) {
+  const children = childToJSX(getChildren(root));
+  if (Array.isArray(children)) {
+    return {
+      $$typeof: REACT_ELEMENT_TYPE,
+      type: REACT_FRAGEMENT_TYPE,
+      key: null,
+      ref: null,
+      prop: { children },
+      __mark: 'Radium',
+    };
+  }
+  return children;
+}
+
+function childToJSX(child: any): any {
+  if (typeof child === 'string' || typeof child === 'number') {
+    return child;
+  }
+
+  if (Array.isArray(child)) {
+    if (child.length === 0) {
+      return null;
+    }
+    if (child.length === 1) {
+      return childToJSX(child[0]);
+    }
+
+    const children = child.map(element => childToJSX(element));
+
+    if (
+      children.every(
+        child => typeof child === 'string' || typeof child === 'number',
+      )
+    ) {
+      return children.join('');
+    }
+    return children;
+  }
+
+  // Instance
+  if (Array.isArray(child.children)) {
+    const instance: Instance = child;
+    const children = childToJSX(instance.children);
+    const props = instance.props;
+
+    if (children !== null) {
+      props.children = children;
+    }
+
+    return {
+      $$typeof: REACT_ELEMENT_TYPE,
+      type: instance.type,
+      key: null,
+      ref: null,
+      props,
+      __mark: 'Radium',
+    };
+  }
+
+  // TextInstance
+  return child.text;
 }
 
 function getChildren(parent: Container | Instance) {
