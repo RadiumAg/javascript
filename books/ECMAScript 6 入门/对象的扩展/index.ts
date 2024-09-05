d3c5aaa8e68a1c50e06107ea1888223945c472b5
张镭银 <zly01858966@lazada.com>
2024-09-05 18:33:01 +0800

    chore: op code

--- books/ECMAScript 6 入门/对象的扩展/index.ts
+++ books/ECMAScript 6 入门/对象的扩展/index.ts
@@ -14,3 +14,13 @@
   const doSomething = function () {};
   doSomething.bind().name; // bound doSomething
 })();
+
+// 描述对象的enumerable属性，被称为enumerable属性，称为“可枚举性”
+// 目前，有四个操作会忽略enumerable为false的属性
+// for...in
+// Object.keys
+// JSON.stringify
+// Object.assign
+
+// super关键字的用法
+// super关键字，指向当前对象的原型对象
