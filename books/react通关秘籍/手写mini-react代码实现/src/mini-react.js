ad52a5334f3295704750d00e260bb0e956995358
Radium <2043869032@qq.com>
2024-09-20 23:24:32 +0800

    fix: correct initialization of fiber.dom in

--- books/react通关秘籍/手写mini-react代码实现/src/mini-react.js
+++ books/react通关秘籍/手写mini-react代码实现/src/mini-react.js
@@ -179,7 +179,7 @@ function updateFunctionComponent(fiber) {
 
 function updateHostComponent(fiber) {
   if (!fiber.dom) {
-    fiber.dom === createDom(fiber);
+    fiber.dom = createDom(fiber);
   }
   reconcileChildren(fiber, fiber.props.children);
 }
