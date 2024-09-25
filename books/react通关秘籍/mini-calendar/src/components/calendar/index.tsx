8d6b6f1edc0e33769d413220938a93370ed01e82
张镭银 <zly01858966@lazada.com>
2024-09-25 17:27:48 +0800

    feat: add function to get first day of month

--- books/react通关秘籍/mini-calendar/src/components/calendar/index.tsx
+++ books/react通关秘籍/mini-calendar/src/components/calendar/index.tsx
@@ -30,8 +30,29 @@ const Calendar: React.FC = () => {
     return new Date(year, month + 1, 0).getDate();
   };
 
+  const firstDayOfMonth = (year: number, month: number) => {
+    return new Date(year, month, 1).getDay();
+  };
+
   const renderDates = () => {
     const days = [];
+
+    const daysCount = daysOfMonth(date.getFullYear(), date.getMonth());
+    const firstDay = firstDayOfMonth(date.getFullYear(), date.getMonth());
+
+    for (let i = 1; i <= firstDay; i++) {
+      days.push(<div key={`empty-${i}`} className="empty"></div>);
+    }
+
+    for (let i = 1; i <= daysCount; i++) {
+      days.push(
+        <div key={i} className="day">
+          {i}
+        </div>
+      );
+    }
+
+    return days;
   };
 
   return (
@@ -51,39 +72,7 @@ const Calendar: React.FC = () => {
         <div className="day">四</div>
         <div className="day">五</div>
         <div className="day">六</div>
-        <div className="empty"></div>
-        <div className="empty"></div>
-        <div className="day">1</div>
-        <div className="day">2</div>
-        <div className="day">3</div>
-        <div className="day">4</div>
-        <div className="day">5</div>
-        <div className="day">6</div>
-        <div className="day">7</div>
-        <div className="day">8</div>
-        <div className="day">9</div>
-        <div className="day">10</div>
-        <div className="day">11</div>
-        <div className="day">12</div>
-        <div className="day">13</div>
-        <div className="day">14</div>
-        <div className="day">15</div>
-        <div className="day">16</div>
-        <div className="day">17</div>
-        <div className="day">18</div>
-        <div className="day">19</div>
-        <div className="day">20</div>
-        <div className="day">21</div>
-        <div className="day">22</div>
-        <div className="day">23</div>
-        <div className="day">24</div>
-        <div className="day">25</div>
-        <div className="day">26</div>
-        <div className="day">27</div>
-        <div className="day">28</div>
-        <div className="day">29</div>
-        <div className="day">30</div>
-        <div className="day">31</div>
+        {renderDates()}
       </div>
     </div>
   );
