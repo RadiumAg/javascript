ac6df37b6419d8932d693c1a0d5459face7582fc
张镭银 <zly01858966@lazada.com>
2024-10-11 18:57:56 +0800

    feat: add component settings with attributes tab

---
+++ books/react通关秘籍/lowcode-editor/src/editor/components/setting/component-attr.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ComponentAttr: React.FC = () => {
+  return <div>ComponentAttr</div>;
+};
+
+export default ComponentAttr;
---
+++ books/react通关秘籍/lowcode-editor/src/editor/components/setting/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useComponentsStore } from '../../stores/components';
+import { Segmented } from 'antd';
+import ComponentAttr from './component-attr';
+
+const Setting: React.FC = () => {
+  const { curComponentId } = useComponentsStore();
+  const [key, setKey] = React.useState('属性');
+
+  if (!curComponentId) return null;
+
+  return (
+    <div>
+      <Segmented
+        value={key}
+        onChange={setKey}
+        block
+        options={['属性', '样式', '事件']}
+      ></Segmented>
+
+      <div>{
+         key === '属性' && <ComponentAttr<}</div>
+    </div>
+  );
+};
+
+export default Setting;
