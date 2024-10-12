11fa8729519da6038c7dabc04402a006c08ebe22
张镭银 <zly01858966@lazada.com>
2024-10-12 13:45:04 +0800

    feat: add node proxy server using Express

---
+++ node-proxy.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const {
+  createProxyMiddleware,
+  loggerPlugin,
+} = require('http-proxy-middleware');
+
+// process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
+const app = express();
+
+const proxyMiddleware = createProxyMiddleware({
+  target: 'https://pre-seoadmin-arise.alibaba-inc.com/metatag/dataList',
+  changeOrigin: true,
+  cookiePathRewrite: true,
+  cookieDomainRewrite: true,
+  async pathRewrite(path, req) {},
+  // headers: {
+  //   host: 'seoadmin-arise.alibaba-inc.com',
+  //   Origin: 'https://seoadmin-arise.alibaba-inc.com',
+  //   Referer: 'https://seoadmin-arise.alibaba-inc.com',
+  // },
+  on: {
+    proxyReq: (proxyReq, req, res) => {
+      /* handle proxyReq */
+      proxyReq.setHeader('Host', 'pre-seoadmin-arise.alibaba-inc.com');
+      proxyReq.setHeader('Origin', 'https://workstation.miravia.net');
+      proxyReq.setHeader('Referer', 'https://workstation.miravia.net');
+
+      req.headers.host = 'pre-seoadmin-arise.alibaba-inc.com';
+      req.headers.origin = 'https://workstation.miravia.net';
+      req.headers.referer = 'https://workstation.miravia.net';
+    },
+    proxyRes: (proxyRes, req, res) => {
+      /* handle proxyRes */
+      proxyRes.headers['access-control-allow-origin'] = '*';
+      res.headers = {
+        'access-control-allow-origin': '*',
+      };
+    },
+    error: (err, req, res) => {
+      console.log(err);
+      /* handle error */
+    },
+  },
+  logger: console,
+  plugins: [loggerPlugin],
+  secure: false,
+});
+
+app.use('/metatag/dataList', proxyMiddleware);
+
+app.listen(4000);
