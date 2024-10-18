bbd73d5de4c8d510e9e370d2e5bf6dc896df8ec5
张镭银 <zly01858966@lazada.com>
2024-10-18 11:22:20 +0800

    feat: add custom object creation function

---
+++ 面试题/手写一个new/index2.ts
@@ -0,0 +1,19 @@
+// new
+interface Func {
+  new (...args: any[]): any;
+}
+function create1(fun: Func, ...params: any[]) {
+  // 用构造函数的prototype属性创建对象
+  const target = Object.create(fun.prototype);
+  const result = fun.call(target, ...params);
+
+  return typeof result === 'object' || typeof result === 'function'
+    ? result
+    : target;
+}
+
+console.log(
+  create1(function a() {
+    this.b = 1;
+  }, 111),
+);
