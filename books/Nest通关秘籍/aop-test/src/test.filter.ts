d7466278710360e1f777e14c77b61345eda075bf
张镭银 <zly01858966@lazada.com>
2024-11-08 18:07:36 +0800

    refactor: update test filter implementation in NestJS project

--- books/Nest通关秘籍/aop-test/src/test.filter.ts
+++ books/Nest通关秘籍/aop-test/src/test.filter.ts
@@ -1,6 +1,19 @@
-import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
+import {
+  ArgumentsHost,
+  BadRequestException,
+  Catch,
+  ExceptionFilter,
+} from '@nestjs/common';
+import { Response } from 'express';
 
 @Catch()
-export class TestFilter<T> implements ExceptionFilter {
-  catch(exception: T, host: ArgumentsHost) {}
+export class TestFilter implements ExceptionFilter {
+  catch(exception: BadRequestException, host: ArgumentsHost) {
+    const response: Response = host.switchToHttp().getResponse();
+
+    response.status(400).json({
+      statusCCode: 400,
+      message: 'test:' + exception.message,
+    });
+  }
 }
