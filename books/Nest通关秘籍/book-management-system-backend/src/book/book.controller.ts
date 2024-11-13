2d0a63e93665ee3c1cd647b4b529daeac0fb61d5
张镭银 <zly01858966@lazada.com>
2024-11-13 15:50:34 +0800

    feat: add book creation and update endpoints with multer support

--- books/Nest通关秘籍/book-management-system-backend/src/book/book.controller.ts
+++ books/Nest通关秘籍/book-management-system-backend/src/book/book.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Put } from '@nestjs/common';
 import { BookService } from './book.service';
+import { CreateBookDto } from './dto/create-book.dto';
+import { UpdateBookDto } from './dto/update-book.dto';
 
 @Controller('book')
 export class BookController {
@@ -8,5 +10,20 @@ export class BookController {
   @Get('list')
   async list() {
     return this.bookService.list();
+  }
+
+  @Get('create')
+  async create(@Body() createBookDto: CreateBookDto) {
+    return this.bookService.create(createBookDto);
+  }
+
+  @Put('update')
+  async update(@Body() updateBookDto: UpdateBookDto) {
+    return this.bookService.update(updateBookDto);
+  }
+
+  @Delete('delete')
+  async delete(@Param('id') id: string) {
+    return this.bookService.delete(+id);
   }
 }
--- books/Nest通关秘籍/book-management-system-backend/src/book/book.service.ts
+++ books/Nest通关秘籍/book-management-system-backend/src/book/book.service.ts
@@ -1,4 +1,4 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { BadRequestException, Inject, Injectable } from '@nestjs/common';
 import { DbService } from 'src/db/db.service';
 import { CreateBookDto } from './dto/create-book.dto';
 import { UpdateBookDto } from './dto/update-book.dto';
@@ -40,7 +40,29 @@ export class BookService {
 
   async update(updateBookDto: UpdateBookDto) {
     const books: Book[] = await this.dbService.read();
+
+    const targetBook = books.find((book) => book.id === updateBookDto.id);
+
+    if (!targetBook) {
+      throw new BadRequestException('没有找到对应的书籍');
+    }
+
+    targetBook.title = updateBookDto.author;
+    targetBook.cover = updateBookDto.cover;
+    targetBook.description = updateBookDto.description;
+    targetBook.name = updateBookDto.name;
+
+    await this.dbService.write(books);
+    return targetBook;
   }
 
-  delete(id: number) {}
+  async delete(id: number) {
+    const books: Book[] = await this.dbService.read();
+    const index = books.findIndex((book) => book.id === id);
+
+    if (index !== -1) {
+      books.splice(index, 1);
+      await this.dbService.write(books);
+    }
+  }
 }
