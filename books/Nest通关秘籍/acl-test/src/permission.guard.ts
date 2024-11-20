2fc5e0a8f196cd0f69bb64095db532caf279949a
Radium <2043869032@qq.com>
2024-11-20 20:08:56 +0800

    fix: types/v3-directive not published

--- books/Nest通关秘籍/acl-test/src/permission.guard.ts
+++ books/Nest通关秘籍/acl-test/src/permission.guard.ts
@@ -8,24 +8,35 @@ import {
 import { Observable } from 'rxjs';
 import { UserService } from './user/user.service';
 import { Request } from 'express';
+import { Reflector } from '@nestjs/core';
 
 @Injectable()
 export class PermissionGuard implements CanActivate {
   @Inject(UserService)
   private readonly userService: UserService;
 
+  @Inject(Reflector)
+  private reflector: Reflector;
+
   async canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
-    console.log(this.userService);
     const request: Request = context.switchToHttp().getRequest();
 
     const user = request.session.user;
+
     if (!user) {
       throw new UnauthorizedException('用户未登录');
     }
+
     const foundUser = await this.userService.findByUserName(user.username);
-    console.log(foundUser);
+    const permission = this.reflector.get('permission', context.getHandler());
+
+    if (foundUser.permission.some((item) => item.name === permission)) {
+      return true;
+    } else {
+      throw new UnauthorizedException('没有权限');
+    }
 
     return true;
   }
