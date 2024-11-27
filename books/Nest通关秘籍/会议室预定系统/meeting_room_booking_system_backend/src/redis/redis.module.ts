9980a2402f12ee2b1cfb8ad84bc1556a5c6bb4a6
Radium <2043869032@qq.com>
2024-11-27 17:51:18 +0800

    refactor: update Redis module and service implementation

--- books/Nest通关秘籍/会议室预定系统/meeting_room_booking_system_backend/src/redis/redis.module.ts
+++ books/Nest通关秘籍/会议室预定系统/meeting_room_booking_system_backend/src/redis/redis.module.ts
@@ -1,7 +1,25 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { RedisService } from './redis.service';
+import { createClient } from 'redis';
 
+@Global()
 @Module({
-  providers: [RedisService],
+  providers: [
+    RedisService,
+    {
+      provide: 'REDIS_CLIENT',
+      async useFactory() {
+        const client = createClient({
+          socket: {
+            host: '127.0.0.1',
+            port: 6379,
+          },
+        });
+
+        await client.connect();
+        return client;
+      },
+    },
+  ],
 })
 export class RedisModule {}
--- books/Nest通关秘籍/会议室预定系统/meeting_room_booking_system_backend/src/user/user.service.ts
+++ books/Nest通关秘籍/会议室预定系统/meeting_room_booking_system_backend/src/user/user.service.ts
@@ -1,13 +1,50 @@
-import { Injectable, Logger } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Inject,
+  Injectable,
+  Logger,
+} from '@nestjs/common';
 import { RegisterUserDto } from './dto/register-user.dto';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
+import { RedisService } from 'src/redis/redis.service';
 
 @Injectable()
 export class UserService {
+  @Inject(RedisService)
+  private redisService: RedisService;
+
   private readonly logger = new Logger();
 
   constructor(private userRepository: Repository<User>) {}
 
-  register(user: RegisterUserDto) {}
+  async register(user: RegisterUserDto) {
+    const captcha = await this.redisService.get(`captcha_${user.email}`);
+
+    if (!captcha) {
+      throw new HttpException('验证码已失效', HttpStatus.BAD_REQUEST);
+    }
+
+    if (user.captcha !== captcha) {
+      throw new HttpException('验证码不正确', HttpStatus.BAD_REQUEST);
+    }
+
+    const foundUser = await this.userRepository.findOneBy({
+      username: user.username,
+    });
+
+    if (foundUser) {
+      throw new HttpException('用户已存在', HttpStatus.BAD_REQUEST);
+    }
+
+    const newUser = new User();
+    newUser.username = user.username;
+    newUser.password = user.password;
+    newUser.email = user.email;
+    newUser.nickName = user.nickName;
+
+    try {
+    } catch (e) {}
+  }
 }
