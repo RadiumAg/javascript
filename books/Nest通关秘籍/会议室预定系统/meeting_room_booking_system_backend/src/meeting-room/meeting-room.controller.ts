e6aeafccc04e9edac34175334941a7b30a14f555
Radium <2043869032@qq.com>
2024-12-20 16:11:25 +0800

    refactor: add validation to create meeting room DTO

--- books/Nest通关秘籍/会议室预定系统/meeting_room_booking_system_backend/src/meeting-room/meeting-room.controller.ts
+++ books/Nest通关秘籍/会议室预定系统/meeting_room_booking_system_backend/src/meeting-room/meeting-room.controller.ts
@@ -1,6 +1,14 @@
-import { Controller, DefaultValuePipe, Get, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  DefaultValuePipe,
+  Get,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { MeetingRoomService } from './meeting-room.service';
 import { generateParseIntPip } from 'src/utils';
+import { CreateMeetingRoomDto } from './dto/create-meeting-room.dto';
 
 @Controller('meeting-room')
 export class MeetingRoomController {
@@ -14,5 +22,10 @@ export class MeetingRoomController {
     pageSize: number,
   ) {
     return await this.meetingRoomService.find(pageNo, pageSize);
+  }
+
+  @Post('create')
+  async create(@Body() meetingRoomDto: CreateMeetingRoomDto) {
+    return await this.meetingRoomService.create(meetingRoomDto);
   }
 }
