ccd47fa8fd697e38d0ffe7233f76ad9be50f0bf7
Radium <2043869032@qq.com>
2024-12-25 17:38:44 +0800

    refactor: simplify IIFE syntax in index.ts

--- books/现代JavaScript教程/函数进阶内容/index.ts
+++ books/现代JavaScript教程/函数进阶内容/index.ts
@@ -7,8 +7,7 @@
  * pow(2, 4) = 16
  *
  */
-
-(() => {
+() => {
   function pow(x, n) {
     if (n === 1) {
       return x;
@@ -18,12 +17,12 @@
   }
 
   console.log(pow(2, 3));
-})();
+};
 
 /**
  * Rest参数与Spread语法
  */
-(() => {
+() => {
   function sumAll(...args) {
     let sum = 0;
 
@@ -36,4 +35,51 @@
 
   const arr = [3, 5, 1];
   console.log(Math.max(...arr));
+};
+
+/**
+ * 浏览器会将setTimeout或SetInterval的五层或更多嵌套调用的最小延时限制在4ms
+ */
+(() => {
+  const start = Date.now();
+  const times = [];
+
+  setTimeout(function run() {
+    times.push(Date.now() - start); // 保存前一个调用的延时
+
+    if (start + 100 < Date.now()) {
+      console.log(times); // 100 毫秒之后，显示延时信息
+    } else setTimeout(run); // 否则重新调度
+  });
+
+  // 输出示例：
+  // 1,1,1,1,9,15,20,24,30,35,40,45,50,55,59,64,70,75,80,85,90,95,100
+})();
+
+/**
+ * 装饰器模式和转发，call/apply
+ */
+(() => {
+  function slow(x) {
+    console.log('Called with ${x');
+    return x;
+  }
+
+  function cachingDecorator(func) {
+    const cache = new Map();
+
+    return function (x) {
+      if (cache.has(x)) {
+        return cache.get(x);
+      }
+
+      const result = func(x);
+
+      cache.set(x, result);
+      return result;
+    };
+  }
+
+  const slowD = cachingDecorator(slow);
+  console.log(slowD(1));
 })();
