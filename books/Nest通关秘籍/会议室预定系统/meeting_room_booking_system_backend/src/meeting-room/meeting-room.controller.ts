e5132b6b8a7db90611a86009a79d575d4e221c8b
Radium <2043869032@qq.com>
2024-12-27 13:38:50 +0800

    refactor: remove unused list method in meeting room controller

--- books/Nest通关秘籍/会议室预定系统/meeting_room_booking_system_backend/src/meeting-room/meeting-room.controller.ts
+++ books/Nest通关秘籍/会议室预定系统/meeting_room_booking_system_backend/src/meeting-room/meeting-room.controller.ts
@@ -18,16 +18,6 @@ import { UpdateMeetingRoomDto } from './dto/update-meeting-room.dto';
 export class MeetingRoomController {
   constructor(private readonly meetingRoomService: MeetingRoomService) {}
 
-  @Get('list')
-  async list(
-    @Query('pageNo', new DefaultValuePipe(1), generateParseIntPip('pageNo'))
-    pageNo: number,
-    @Query('paeSize', new DefaultValuePipe(2), generateParseIntPip('pageSize'))
-    pageSize: number,
-  ) {
-    return await this.meetingRoomService.find(pageNo, pageSize);
-  }
-
   @Post('create')
   async create(@Body() meetingRoomDto: CreateMeetingRoomDto) {
     return await this.meetingRoomService.create(meetingRoomDto);
