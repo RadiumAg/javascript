ecb777de79287d1d24b11e37b8a49081b4abd02f
Radium <2043869032@qq.com>
2025-01-14 10:56:56 +0800

    feat: add array placeholder example in index.ts

--- books/现代JavaScript教程/数组的扩展/index.ts
+++ books/现代JavaScript教程/数组的扩展/index.ts
@@ -6,3 +6,13 @@
     return num % 2 === 0 ? 'even' : 'odd';
   });
 })();
+
+// 数组空位
+(() => {
+  // eslint-disable-next-line unicorn/new-for-builtins
+  console.log(Array(3));
+
+  //注意，空位不是undefined，某一个位置的值等于undefined，依然是有值的。空位是没有任何值，in运算符可以说明这一点。
+  0 in [undefined, undefined, undefined]; // true
+  0 in [[, , ,]]; // false
+})();
