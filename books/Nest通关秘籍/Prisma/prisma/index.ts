9515df79b6a14dc4412e9c29abcb407e33367082
Radium <2043869032@qq.com>
2025-01-16 18:35:21 +0800

    feat(prisma): add initial prisma setup and seed data

---
+++ books/Nest通关秘籍/Prisma/prisma/index.ts
@@ -0,0 +1,26 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient({
+  log: [{ emit: 'stdout', level: 'query' }],
+});
+
+async function main() {
+  const user = await prisma.user.create({
+    data: {
+      name: '东东东',
+      email: 'dongdong@dong.com',
+      Post: {
+        create: [
+          {
+            title: 'aaa',
+            content: 'aaaa',
+          },
+          {
+            title: 'bbb',
+            content: 'bbbb',
+          },
+        ],
+      },
+    },
+  });
+}
