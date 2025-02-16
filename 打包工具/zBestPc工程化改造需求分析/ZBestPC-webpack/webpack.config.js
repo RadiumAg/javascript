399c12368bedc8ce32c1c39be09dad262998fc61
Radium <2043869032@qq.com>
2025-02-16 11:19:06 +0800

    chore: 更新依赖并优化构建配置以提升性能

--- 打包工具/zBestPc工程化改造需求分析/ZBestPC-webpack/webpack.config.js
+++ 打包工具/zBestPc工程化改造需求分析/ZBestPC-webpack/webpack.config.js
@@ -1,9 +1,10 @@
 const path = require('path');
 const webpack = require('webpack');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 /**
@@ -86,6 +87,8 @@ module.exports = {
       filename: 'css/[name].css',
       chunkFilename: 'css/[name].chunk.css',
     }),
+
+    new CleanWebpackPlugin(),
   ],
   optimization: {
     minimize: true,
