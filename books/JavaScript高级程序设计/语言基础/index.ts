ae01fbe0c6b5dd58977c981ffd49eb9520359ae1
Radium <2043869032@qq.com>
2025-02-18 16:18:45 +0800

    feat: add number conversion examples in index.ts

--- books/JavaScript高级程序设计/语言基础/index.ts
+++ books/JavaScript高级程序设计/语言基础/index.ts
@@ -114,4 +114,6 @@
   console.log(num.toString()); // "10"
   console.log(num.toString(2)); // "1010"
   console.log(num.toString(8)); // "12"
+  console.log(num.toString(10)); // "10"
+  console.log(num.toString(16)); // "a"
 })();
