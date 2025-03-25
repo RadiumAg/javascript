1220edbe73f726a6764ce941880175f8887114cd
Radium <2043869032@qq.com>
2025-03-25 10:19:00 +0800

    refactor: change variable declaration in render-plus.js

--- books/Vue.js设计与实现/2025-2/render/render-plus.js
+++ books/Vue.js设计与实现/2025-2/render/render-plus.js
@@ -264,12 +264,12 @@ function createRenderer(options) {
     const newChildren = newVnode.children;
 
     // 四个索引
-    const oldStartIdx = 0;
+    let oldStartIdx = 0;
     let oldEndIdx = oldChildren.length - 1;
     let newStartIdx = 0;
     let newEndIdx = newChildren.length - 1;
     // 四个索引指向的 vnode 节点
-    const oldStartVNode = oldChildren[oldStartIdx];
+    let oldStartVNode = oldChildren[oldStartIdx];
     let oldEndVNode = oldChildren[oldEndIdx];
     let newStartVNode = newChildren[newStartIdx];
     let newEndVNode = newChildren[newEndIdx];
@@ -277,6 +277,10 @@ function createRenderer(options) {
     while (oldStartIdx <= oldEndIdx && newStartIdx <= newEndIdx) {
       if (oldStartVNode.key === newStartVNode.key) {
         // 第一步：oldStartVNode 和 newStartVNode 比较
+        // 调用 patch 函数在 oldStartVNode 与 newStartVVNode 之间打补丁
+        patch(oldStartVNode, newStartVNode, container);
+        oldStartVNode = oldChildren[++oldStartIdx];
+        newStartVNode = newChildren[++newStartIdx];
       } else if (oldEndVNode.key === newEndVNode.key) {
         // 第二步：oldEndVNode 和 newEndVNode 比较
         // 节点在新的顺序中仍然处于尾部，不需要移动，但仍需要打补丁
@@ -286,6 +290,14 @@ function createRenderer(options) {
         newEndVNode = newChildren[--newEndIdx];
       } else if (oldStartVNode.key === newEndVNode.key) {
         // 第三步：oldStartVNode 和 newEndVNode 比较
+        // 调用 patch 函数正在 oldStarttVnoode 和 newEndVNode之间
+        patch(oldStartVNode, newEndVNode, container);
+        // 将旧的一组子节点的头部节点对应的真实 DOM 节点 oldStartVNode.el 移动到
+        // 旧的一组子节点的尾部对应的真实 DOM 节点后面
+        options.insert(oldStartVNode.el, container, oldEndVNode.el.nextSibling);
+        // 更新相关索引到下一个位置
+        oldStartVNode = oldChildren[++oldStartIdx];
+        newEndVNode = newChildren[--newEndIdx];
       } else if (oldEndVNode.key === newStartVNode.key) {
         // 第四步： oldEndVNode和newStartVNode 比较
         // 仍然需要调用 patch 函数进行补丁
