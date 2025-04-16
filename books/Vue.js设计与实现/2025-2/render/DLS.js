ad44cb525d3fae888d58abd8140268f0ff62a973
Radium <2043869032@qq.com>
2025-04-16 22:15:06 +0800

    feat: 在 DLS.js 中添加词法分析功能并实现状态机逻辑，支持字符处理 feat: 添加 jsconfig.json 配置文件以支持 TypeScript 和 ESNext 模块

--- books/Vue.js设计与实现/2025-2/render/DLS.js
+++ books/Vue.js设计与实现/2025-2/render/DLS.js
@@ -7,3 +7,58 @@ const State = {
   tagEnd: 5,
   tagEndName: 6,
 };
+
+function isAlpha(char) {
+  return (char > 'a' && char <= 'z') || (char > 'A' && char <= 'Z');
+}
+
+function tokenize(str) {
+  // 状态机的当前状态：初始状态
+  let currentState = State.initial;
+  // 用于缓存字符
+  const chars = [];
+  const tokens = [];
+
+  while (str) {
+    const char = str[0];
+    switch (currentState) {
+      case State.initial:
+        // 遇到字符 <
+        if (char === '<') {
+          currentState = State.tagOpen;
+          str = str.slice(1);
+          break;
+        } else if (isAlpha(char)) {
+          // 1. 遇到字符，切换到文本状态
+          currentState = State.text;
+          // 2. 将当前字母缓存到 char 数组
+          chars.push(char);
+          // 3. 消费当前字符
+          str = str.slice(1);
+        }
+        break;
+        s;
+
+      case State.tagOpen:
+        // 遇到字符 <
+        if (isAlpha(char)) {
+          currentState = State.tagName;
+          str = str.slice(1);
+        } else if (char === '/') {
+          // 1. 遇到字符，切换到文本状态
+          currentState = State.text;
+          // 2. 将当前字母缓存到 char 数组
+          chars.push(char);
+          // 3. 消费当前字符
+          str = str.slice(1);
+        }
+        break;
+
+      case State.text:
+        if (isAlpha(char)) {
+          // 1. 遇到字母，保持状态不变，但应该将当前字符缓存到 chars 数组
+          chars.push(char);
+        }
+    }
+  }
+}
