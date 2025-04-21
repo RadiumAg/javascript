035dd062aa05bfa1df521be103f7eab3e6421143
Radium <2043869032@qq.com>
2025-04-21 18:42:52 +0800

    feat(render): add dump function for node visualization

--- books/Vue.js设计与实现/2025-2/render/DLS.js
+++ books/Vue.js设计与实现/2025-2/render/DLS.js
@@ -8,6 +8,18 @@ const State = {
   tagEndName: 6,
 };
 
+function dump(node, indent = 0) {
+  // 节点的类型
+  const type = node.type;
+  // 节点的描述，如果是根节点，则没有描述
+  // 如果是 Element 类型的节点，则使用 node.tag 作为节点的描述
+  // 如果是 Text 类型的节点，则是使用 node.content 作为节点的描述
+  const desc = node.type === 'Root'? node.type === 'Element'? node.tag ? node.content
+
+  // 打印节点等待类型和描述信息
+  console.log(`${'_'.repeat(indent)}${type}: `)
+},
+
 function isAlpha(char) {
   return (char >= 'a' && char <= 'z') || (char > 'A' && char <= 'Z');
 }
