b4c08d66b27be550eff4957e82381a607105e241
Radium <2043869032@qq.com>
2025-05-13 13:58:38 +0800

    feat: add adapter pattern implementation in TypeScript

--- books/refactoringguru/适配器模式/index.ts
+++ books/refactoringguru/适配器模式/index.ts
@@ -1,7 +1,54 @@
 /**
  * 适配器模式是一种结构型设计模式，它能使接口不兼容的对象能够相互合作
- *
+ * 适配器可以通过以不同抽象或接口类型实例为参数的构造函数来识别。 当适配器的任何方法被调用时， 它会将参数转换为合适的格式， 然后将调用定向到其封装对象中的一个或多个方法。
  *
  *
  *
  */
+
+class Target {
+  public request(): string {
+    return "Target: The default target's behavior.";
+  }
+}
+
+class Adaptee {
+  public specificRequest(): string {
+    return '.eetpadA eht fo roivaheb laicepS';
+  }
+}
+
+class Adapter extends Target {
+  private adaptee: Adaptee;
+
+  constructor(adaptee: Adaptee) {
+    super();
+    this.adaptee = adaptee;
+  }
+
+  public request(): string {
+    return this.adaptee.specificRequest().split('').reverse().join('');
+  }
+}
+
+function clientCode(target: Target) {
+  console.log(target.request());
+}
+
+console.log('Client: I can work just fine with the Target objects:');
+const target = new Target();
+clientCode(target);
+
+console.log('');
+
+const adaptee = new Adaptee();
+console.log(
+  "Client: The Adaptee class has a weird interface. See, I don't understand it:"
+);
+console.log(`Adaptee: ${adaptee.specificRequest()}`);
+
+console.log('');
+
+console.log('Client: But I can work with it via the Adapter:');
+const adapter = new Adapter(adaptee);
+clientCode(adapter);
