a27c5d2b1d5f35f65b2db7942b4c094520376577
Radium <2043869032@qq.com>
2025-05-23 18:28:55 +0800

    feat: add iterator interface for collection access

--- books/refactoringguru/迭代器模式/index.ts
+++ books/refactoringguru/迭代器模式/index.ts
@@ -2,3 +2,72 @@
  *  它都必须提供某种访问元素的方式， 便于其他代码使用其中的元素。 集合应提供一种能够遍历元素的方式， 且保证它不会周而复始地访问同一个元素。
  *
  */
+
+interface Iterator<T> {
+  // Return the current element.
+  current(): T;
+
+  // Return the current element and move forward to next element.
+  next(): T;
+
+  // Return the key of the current element.
+  key(): number;
+
+  // Checks if current position is valid
+  valid(): boolean;
+
+  // Rewind the Iterator to the first element.
+  rewind(): void;
+}
+
+interface Aggregator {
+  // Retwrite an external iterator
+  getIterator(): Iterator<string>;
+}
+
+class AlphabeticalOrderIterator implements Iterator<string> {
+  private collection: WordsCollection;
+
+  private position: number = 0;
+
+  private reverse: boolean = false;
+
+  constructor(collection: WordsCollection, reverse: boolean = false) {
+    this.collection = collection;
+    this.reverse = reverse;
+
+    if (reverse) {
+      this.position = collection.getCount() - 1;
+    }
+  }
+
+  public rewind() {
+    this.position = this.reverse ? this.collection.getCount() - 1 : 0;
+  }
+
+  public current(): string {
+    return this.collection.getItems()[this.position];
+  }
+
+  public key(): number {
+    return this.position;
+  }
+
+  public next(): string {
+    const item = this.collection.getItems()[this.position];
+    this.position += this.reverse ? -1 : 1;
+    return item;
+  }
+
+  public valid(): boolean {
+    if (this.reverse) {
+      return this.position >= 0;
+    }
+
+    return this.position < this.collection.getCount();
+  }
+}
+
+class WoordsCollection implements Aggregator {
+  private items: string[] = [];
+}
