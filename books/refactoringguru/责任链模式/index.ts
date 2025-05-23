686a8562ac3daf046a7bdd33a52f6de05a93936f
Radium <2043869032@qq.com>
2025-05-23 17:27:29 +0800

    feat: add abstract handler class for request handling

--- books/refactoringguru/责任链模式/index.ts
+++ books/refactoringguru/责任链模式/index.ts
@@ -7,3 +7,71 @@ interface Handler<Request = string, Result = string> {
 
   handle(request: Request): Result;
 }
+
+abstract class AbstractHandler implements Handler {
+  private nextHandler: Handler;
+
+  public setNext(handler: Handler): Handler {
+    this.nextHandler = handler;
+    // Returning a handler from here will let us link handlers in a
+    // convenient way like this:
+    // monkey.setNext(squirrel).setNext(dog);
+    return handler;
+  }
+
+  public handle(request: string) {
+    if (this.nextHandler) {
+      return this.nextHandler.handle(request);
+    }
+
+    return '';
+  }
+}
+
+class MonkeyHandler extends AbstractHandler {
+  public handle(request: string): string {
+    if (request === 'Banana') {
+      return `Monkey: I'll eat the ${request}.`;
+    }
+    return super.handle(request);
+  }
+}
+
+class SquirrelHandler extends AbstractHandler {
+  public handle(request: string): string {
+    if (request === 'Nut') {
+      return `Squirrel: I'll eat the ${request}.`;
+    }
+    return super.handle(request);
+  }
+}
+
+class DotHandler extends AbstractHandler {
+  public handle(request: string): string {
+    if (request === 'MeatBall') {
+      return `Dog: I'll eat the ${request}.`;
+    }
+    return super.handle(request);
+  }
+}
+
+function clientCode(handler: Handler) {
+  const foods = ['Nut', 'Banana', 'Cup of coffee'];
+
+  for (const food of foods) {
+    console.log(`Client: Who wants a ${food}?`);
+    const result = handler.handle(food);
+    if (result) {
+      console.log(`${result}`);
+    } else {
+      console.log(`${food} was left untouched.`);
+    }
+  }
+}
+
+const monkey = new MonkeyHandler();
+const squirrel = new SquirrelHandler();
+const dog = new DotHandler();
+
+// 调用链
+monkey.setNext(squirrel).setNext(dog);
