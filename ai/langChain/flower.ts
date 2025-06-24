e4f10d6b7a39b1da58ab089e1d3098a86986c04e
Radium <2043869032@qq.com>
2025-06-24 15:06:49 +0800

    feat: add PDF parsing functionality in flower.ts

--- ai/langChain/flower.ts
+++ ai/langChain/flower.ts
@@ -1,1 +1,25 @@
+/**
+ * rag实例
+ *
+ *
+ */
 import path from 'path';
+import fs from 'fs';
+import PdfParse from 'pdf-parse/lib/pdf-parse';
+
+const loadFile = async (path: string) => {
+  if (path.endsWith('.pdf')) {
+    const dataBuffer = fs.readFileSync(path);
+    const data = await PdfParse(dataBuffer);
+    return data;
+  }
+};
+
+const filePath = path.resolve(
+  './doc',
+  '2208_professional-javascript-for-web-developers-5-ed.pdf'
+);
+
+const docContent = await loadFile(filePath);
+
+console.log(docContent?.text);
