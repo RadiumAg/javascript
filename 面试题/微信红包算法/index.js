b6fa5d894c47799268de4d6b8ff4e5cb3ff8781b
Radium <2043869032@qq.com>
2025-07-10 09:22:19 +0800

    feat(redpacket): implement double mean algorithm for red packet distribution

--- 面试题/微信红包算法/index.js
+++ 面试题/微信红包算法/index.js
@@ -1,3 +1,10 @@
+/**
+ *
+ * 二倍均值法
+ *
+ *
+ */
+
 () => {
   class RedPackage {
     money = 0;
@@ -58,5 +65,52 @@
   redpkg.openRedPackge(); // 第六次应该返回0
 };
 
+(() => {
+  class RedPackage {
+    money = 0;
+    count = 0;
+    _remain = 0;
 
-()=>{}
+    constructor(money, count) {
+      this.money = money;
+      this.count = count;
+      this._remain = money; // 初始化剩余金额
+    }
+
+    openRedPackge() {
+      if (this.count === 1) {
+        const lastMoney = this.money.toFixed(2);
+        this.money = 0;
+        this.count = 0;
+
+        console.log(lastMoney);
+
+        return lastMoney;
+      }
+
+      if (this.count === 0) {
+        return 0;
+      }
+
+      const maxMoney = (this.money / this.count) * 2; // 最大值
+      const finalMoney = Math.max((Math.random() * maxMoney).toFixed(2), 0.1); // 如果是小于0.1，则返回0.1
+
+      this.money -= finalMoney;
+      this.count--;
+
+      console.log(finalMoney);
+
+      return finalMoney;
+    }
+  }
+
+  // 测试
+  const redpkg = new RedPackage(100, 5);
+  console.log('开始抢红包：');
+  redpkg.openRedPackge();
+  redpkg.openRedPackge();
+  redpkg.openRedPackge();
+  redpkg.openRedPackge();
+  redpkg.openRedPackge();
+  redpkg.openRedPackge(); //
+})();
