c7f1a8f1109bf07d239a217c48e83f81e268cd86
Radium <2043869032@qq.com>
2025-07-18 11:00:46 +0800

    chore: op code

--- 算法/获取最大值/index.js
+++ 算法/获取最大值/index.js
@@ -7,11 +7,12 @@ function process(arr = [], L = 0, R = 0) {
     return arr[L];
   }
 
-  const mid = L + ((R - L) >> 1);
+  const mid = L + ((R - L) >> 1); //  表示将数组长度除以2并向下取整
+   
   const leftMax = process(arr, L, mid);
   const rightMax = process(arr, mid + 1, R);
   return Math.max(leftMax, rightMax);
 }
 
 
-console.log(getMax([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+console.log(getMax([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))  
