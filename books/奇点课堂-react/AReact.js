5455826e9ee1cb4b22262cb656601111108bfb49
Radium <2043869032@qq.com>
2025-07-27 16:15:57 +0800

    chore: op code

--- books/奇点课堂-react/AReact.js
+++ books/奇点课堂-react/AReact.js
@@ -1,3 +1,6 @@
+let workkInProgress = null;
+let workkInProgressRoot = null; // 存储根fiberRoot
+
 function createElement(type, props, ...children) {
   return {
     type,
@@ -23,12 +26,28 @@ function createTextElement(text) {
 const isProperty = (key) => key !== 'children';
 
 class AReactDomRoot {
+  _internalRoot = null;
   constructor(container) {
-    this.container = container;
+    // this.container = container;
+    this._internalRoot = {
+      current: null,
+      containerInfo: container,
+    };
   }
 
   render(element) {
-    this.renderImpl(element, this.container);
+    this._internalRoot.current = {
+      alternate: {
+        stateNode: this._internalRoot.containerInfo,
+        props: {
+          children: [element],
+        },
+      },
+    };
+    workkInProgressRoot = this._internalRoot;
+    workkInProgress = workkInProgressRoot.current.alternate;
+    setTimeout(workLoop);
+    // this.renderImpl(element, this.container);
   }
 
   renderImpl(element, parent) {
@@ -51,6 +70,80 @@ class AReactDomRoot {
 
 function createRoot(container) {
   return new AReactDomRoot(container);
+}
+
+/**
+ *  循环创建 fiber
+ * @param {*} deadline
+ */
+function workLoop() {
+  while (workkInProgress) {
+    workkInProgress = performUnitOfWork(workkInProgress);
+  }
+}
+
+function getNextFiber(fiber) {
+  // 遍历顺序 child -> sibling -> return -> return.sibling
+  // 先遍历 child
+  // 然后是 sibling
+  // 然后是 return 并找下一个 sibling
+  if (fiber.child) {
+    return fiber.child;
+  }
+  let nextFiber = fiber;
+
+  while (nextFiber) {
+    if (nextFiber.sibling) {
+      return nextFiber.sibling;
+    } else {
+      nextFiber = nextFiber.return;
+    }
+  }
+
+  return null;
+}
+
+function performUnitOfWork(fiber) {
+  // 1. 处理当前 fiber: 创建 DOM，以及设置 props，插入当前 dom 到 pareant
+  if (!fiber.stateNode) {
+    fiber.stateNode =
+      fiber.type === 'HostText'
+        ? document.createTextNode('')
+        : document.createElement(fiber.type);
+
+    Object.keys(fiber.props)
+      .filter(isProperty)
+      .forEach((key) => {
+        fiber.stateNode[key] = fiber.props[key];
+      });
+  }
+
+  if (fiber.return) {
+    fiber.return.stateNode.appendChild(fiber.stateNode);
+  }
+
+  let prevSibling = null;
+
+  // 2. 初始化 children 的 fibber
+  fiber.props.children.forEach((child, index) => {
+    // 创建子fiber
+    const newFiber = {
+      type: child.type,
+      stateNode: null,
+      return: fiber,
+      props: child.props,
+    };
+
+    if (index === 0) {
+      fiber.child = newFiber;
+    } else {
+      prevSibling.sibling = newFiber;
+    }
+
+    prevSibling = newFiber;
+  });
+  // 3. 返回下一个处理的 fiber
+  return getNextFiber(fiber);
 }
 
 export { createRoot, createElement };
--- books/奇点课堂-react/test/jsx.test.jsx
+++ books/奇点课堂-react/test/jsx.test.jsx
@@ -1,6 +1,8 @@
 import { describe, it, expect } from 'vitest';
 import * as AReact from '../AReact';
 
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
 describe('AReact JSX', () => {
   it('should render jsx', () => {
     const container = document.createElement('div');
@@ -48,7 +50,27 @@ describe('should render jsx with different props', () => {
     const root = AReact.createRoot(container);
     root.render(element);
     expect(container.innerHTML).toBe(
-      '<div id="foo"></div><button>Add</button></div>'
+      '<div id="foo" class="bar"><button>Add</button></div>'
+    );
+  });
+});
+
+describe('AReact Concurrent', () => {
+  it.only('should render in async', async () => {
+    const container = document.createElement('div');
+    const element = (
+      <div id="foo" className="bar">
+        <button>Add</button>
+      </div>
+    );
+
+    const root = AReact.createRoot(container);
+    root.render(element);
+
+    expect(container.innerHTML).toBe('');
+    await sleep(1000);
+    expect(container.innerHTML).toBe(
+      '<div id="foo" class="bar"><button>Add</button></div>'
     );
   });
 });
