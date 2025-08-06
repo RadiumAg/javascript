a037120877397208a64c15aeacd7e597a0d18c0c
Radium <2043869032@qq.com>
2025-08-06 10:53:09 +0800

    feat: add generator function example with yield keyword

--- books/JavaScript高级程序设计/迭代器和生成器/生成器.ts
+++ books/JavaScript高级程序设计/迭代器和生成器/生成器.ts
@@ -9,3 +9,39 @@
 })();
 
 // 通过yield中断程序
+/**
+ *
+ * yield 关键字必须直接位于生成器函数定义中，出现在嵌套的非生成器中会抛出语法错误
+ *
+ *
+ */
+() => {
+  function* generatorFn() {
+    yield 'foo';
+    yield 'bar';
+
+    return 'baz';
+  }
+
+  const generatorObject = generatorFn();
+  console.log(generatorObject.next());
+  console.log(generatorObject.next());
+  console.log(generatorObject.next());
+};
+
+/**
+ * 生成器对象作为可迭代对象
+ *
+ *
+ */
+(() => {
+  function* generatorFn() {
+    yield 1;
+    yield 2;
+    yield 3;
+  }
+
+  for (const x of generatorFn()) {
+    console.log(x);
+  }
+})();
