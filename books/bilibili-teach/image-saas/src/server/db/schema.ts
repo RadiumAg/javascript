7468512d59c5eacb3ab45c30efdc03864445ca81
Radium <2043869032@qq.com>
2025-10-21 22:43:13 +0800

    chore: op code

--- books/bilibili-teach/image-saas/src/server/db/schema.ts
+++ books/bilibili-teach/image-saas/src/server/db/schema.ts
@@ -12,6 +12,16 @@ import {
 } from 'drizzle-orm/pg-core';
 import type { AdapterAccount } from 'next-auth/adapters';
 
+export const apps = pgTable('apps', {
+  id: uuid('id').notNull().primaryKey(),
+  name: varchar('name', { length: 100 }).notNull(),
+  description: varchar('description', { length: 500 }),
+  deleteAt: timestamp('deleted_at', { mode: 'date' }),
+  createAt: timestamp('created_at', { mode: 'date' }).defaultNow(),
+  userId: text('user_id').notNull(),
+  storageId: integer('storage_id'),
+});
+
 export const users = pgTable('user', {
   id: text('id')
     .primaryKey()
@@ -107,16 +117,19 @@ export const files = pgTable(
     url: varchar('url', { length: 1024 }).notNull(),
     userId: text('user_id').notNull(),
     contentType: varchar('content_type', { length: 100 }).notNull(),
+    appId: uuid(),
   },
   (table) => ({
     cursorIndex: index('cursor_idx').on(table.id, table.createdAt),
   }),
 );
 
-export const photosRelations = relations(files, ({ one }) => ({
-  photos: one(users, { fields: [files.id], references: [users.id] }),
-}));
-
 export const filesRelations = relations(files, ({ one }) => ({
   files: one(users, { fields: [files.userId], references: [users.id] }),
+  app: one(apps, { fields: [files.appId], references: [apps.id] }),
+}));
+
+export const appRelations = relations(apps, ({ one, many }) => ({
+  user: one(users, { fields: [apps.userId], references: [users.id] }),
+  files: many(files),
 }));
