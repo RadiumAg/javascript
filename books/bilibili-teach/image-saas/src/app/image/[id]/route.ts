a4ae9c5513a41cd400de1e39ad6f9a796e29b701
Radium <2043869032@qq.com>
2025-10-29 00:14:53 +0800

    feat: add image processing and response handling in route for image retrieval

--- books/bilibili-teach/image-saas/src/app/image/[id]/route.ts
+++ books/bilibili-teach/image-saas/src/app/image/[id]/route.ts
@@ -1,5 +1,6 @@
 import { db } from '@/server/db/db';
 import { GetObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import { TRPCError } from '@trpc/server';
 import { NextRequest, NextResponse } from 'next/server';
 import sharp from 'sharp';
 
@@ -7,25 +8,44 @@ const GET = async (request: NextRequest, data: { params: { id: string } }) => {
   const { id } = await data.params;
   const file = await db.query.files.findFirst({
     where: (files, { eq }) => eq(files.id, id),
+    with: {
+      app: {
+        with: {
+          storage: true,
+        },
+      },
+    },
   });
+  if (!file?.app?.storage) {
+    throw new TRPCError({
+      code: 'BAD_REQUEST',
+    });
+  }
 
   if (!file || !file.contentType.startsWith('image')) {
     return new NextResponse('', {
       status: 400,
     });
   }
+  const app = file.app;
+
+  if (app.storage == null) {
+    throw new TRPCError({
+      code: 'BAD_REQUEST',
+    });
+  }
 
   const command = new GetObjectCommand({
-    Bucket: process.env.OSS_BUCKET!,
+    Bucket: app.storage.configuration.bucket,
     Key: decodeURIComponent(file.path),
   });
 
   const s3Client = new S3Client({
-    region: process.env.OSS_REGION,
-    endpoint: process.env.OSS_ENDPOINT,
+    region: app.storage.configuration.region,
+    endpoint: app.storage.configuration.apiEndPoint,
     credentials: {
-      accessKeyId: process.env.OOS_SECRET_ID!,
-      secretAccessKey: process.env.OOS_SECRET_KEY!,
+      accessKeyId: app.storage.configuration.accessKeyId,
+      secretAccessKey: app.storage.configuration.secretAccessKey,
     },
   });
 
