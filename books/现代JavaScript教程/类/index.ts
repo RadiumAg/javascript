5cd3f7545ea1afdf5a309b7f758c9c47b2b01b4e
Radium <2043869032@qq.com>
2025-11-26 15:54:31 +0800

    feat: add private and protected properties example in TypeScript
    Co-developed-by: Aone Copilot <noreply@alibaba-inc.com>

--- books/现代JavaScript教程/类/index.ts
+++ books/现代JavaScript教程/类/index.ts
@@ -16,3 +16,29 @@
   const button = new Button('hello');
   setTimeout(button.click, 1000);
 })();
+
+// 私有的和受保护的属性和方法
+(() => {
+  class CoffeeMachine {
+    _waterAmount = 0;
+
+    set waterAmount(value) {
+      if (value < 0) {
+        value = 0;
+      }
+      this._waterAmount = value;
+    }
+
+    get waterAmount() {
+      return this._waterAmount;
+    }
+
+    constructor(power) {
+      this._power = power;
+    }
+  }
+
+  const coffeeMachine = new CoffeeMachine();
+
+  coffeeMachine.waterAmount = 10;
+})();
