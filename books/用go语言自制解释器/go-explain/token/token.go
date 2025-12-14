4c9ae8bfb391c7258426a03637b22a62aa82b60a
Radium <2043869032@qq.com>
2025-12-14 17:15:27 +0800

    feat: add token constants in token.ts

--- books/用go语言自制解释器/go-explain/token/token.go
+++
@@ -1,4 +0,0 @@
-package token
-
-type TokenType string
-
---
+++ books/用go语言自制解释器/go-explain/token/token.ts
@@ -0,0 +1,27 @@
+// token/token.go
+
+const Token = {
+  ILLEGAL: 'ILLEGAL',
+  EOF: 'EOF',
+
+  // 标识符+字面量
+  IDENT: 'IDENT', // add, foobar, x, y, ...
+  INT: 'INT', // 1343456
+
+  // 运算符
+  ASSIGN: '=',
+  PLUS: '+',
+
+  // 分隔符
+  COMMA: ',',
+  SEMICOLON: ';',
+
+  LPAREN: '(',
+  RPAREN: ')',
+  LBRACE: '{',
+  RBRACE: '}',
+
+  // 关键字
+  FUNCTION: 'FUNCTION',
+  LET: 'LET',
+};
