1e14badaeaf6897dfe8bbc1953d401ad54db26dd
Radium <2043869032@qq.com>
2025-12-17 23:10:05 +0800

    feat: extend AST with Program, Identifier and LetStatement classes; enhance TokenLiteral function

--- books/用go语言自制解释器/go-explain/ast/indext.ts
+++ books/用go语言自制解释器/go-explain/ast/indext.ts
@@ -1,3 +1,5 @@
+import { Token } from '../token';
+
 interface Node {
   TokenLiteral(): string;
 }
@@ -13,16 +15,35 @@ interface Expression {
   expressionNode(): void;
 }
 
-interface Program {
-  statements: Statement[];
+class Program {
+  statements: Statement[] = [];
 }
 
-function TokenLiteral(p: Program) {
-  if (p.statements.length > 0) {
-    return p.statements[0].node.TokenLiteral();
+class Identifier {
+  token?: Token;
+  value?: string;
+}
+
+class LetStatement {
+  token?: Token;
+  name?: Identifier;
+  value?: Expression;
+}
+
+function TokenLiteral(value: Program | LetStatement | Identifier) {
+  if (value instanceof Program) {
+    return value.statements[0].node.TokenLiteral();
+  } else if (value instanceof LetStatement) {
+    return value.token?.literal;
+  } else if (value instanceof Identifier) {
+    return value.token?.literal;
   }
 
   return '';
 }
 
-export { TokenLiteral };
+function statementNode(ls: LetStatement) {}
+
+function expressionNode() {}
+
+export { TokenLiteral, statementNode };
