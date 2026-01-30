f4938db86b6c943b484d1e36fd06d91378ab93f3
Radium <2043869032@qq.com>
2026-01-30 17:37:43 +0800

    feat: add map examples in index.ts
    Co-developed-by: Aone Copilot <noreply@alibaba-inc.com>

--- books/JavaScript高级程序设计/语言基础/index.ts
+++ books/JavaScript高级程序设计/语言基础/index.ts
@@ -369,4 +369,22 @@
 };
 
 // Map
-() => {};
+(() => {
+  const m1 = new Map([
+    ['key1', 'val1'],
+    ['key2', 'val2'],
+    ['key3', 'val3'],
+  ]);
+  console.log(m1.size);
+  const m2 = new Map({
+    [Symbol.iterator]: function* () {
+      yield ['key1', 'val1'];
+      yield ['key2', 'val2'];
+      yield ['key3', 'val3'];
+    },
+  });
+  console.log(m2.size);
+  const m3 = new Map([]);
+  console.log(m3.has(undefined));
+  console.log(m3.get(undefined));
+})();
