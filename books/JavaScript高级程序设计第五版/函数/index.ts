fe27009fe3274a6d070ef08c4b8393fd2718db3d
Radium <2043869032@qq.com>
2026-04-17 15:30:45 +0800

    feat: add function with multiple argument handling logic
    Co-developed-by: Aone Copilot <noreply@alibaba-inc.com>

--- books/JavaScript高级程序设计第五版/函数/index.ts
+++ books/JavaScript高级程序设计第五版/函数/index.ts
@@ -6,3 +6,16 @@
 
   sayHi(1, 2, 3, 3, 4, 4, 5, 12312, 56, 6);
 })();
+
+(() => {
+  function doAdd() {
+    if (arguments.length === 1) {
+      console.log(arguments[0] + 10);
+    } else if (arguments.length === 2) {
+      console.log(arguments[0] + arguments[1]);
+    }
+  }
+
+  doAdd(10);
+  doAdd(30, 20);
+})();
