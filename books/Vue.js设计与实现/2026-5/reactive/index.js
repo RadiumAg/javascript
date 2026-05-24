8e3212a9a1575c96e78976288bd7b7ebe6b5a62f
RadiumAg <2043869032@qq.com>
2026-05-24 23:09:40 +0800

    chore: op code

--- books/Vue.js设计与实现/2026-5/reactive/index.js
+++ books/Vue.js设计与实现/2026-5/reactive/index.js
@@ -1,5 +1,5 @@
 // 存储服务作用函数的桶
-const bucket = new Set();
+const bucket = new WeakMap();
 // effec 函数用于注册副作用函数
 let activeEffect = null;
 // 原始数据
@@ -9,6 +9,19 @@ const data = { text: 'hello world' };
 const obj = new Proxy(data, {
   // 拦截读取操作
   get(target, key) {
+    // 没有 activeEffect，直接 return
+    if (!activeEffect) return target[key];
+    // 根据 target 从桶中取得 depsMap，depsMap 是 key 到副作用函数集合的映射
+    let depsMap = bucket.get(target);
+    if (!depsMap) {
+      // 如果 deps 不存在，那么新建一个 Map 与target关联
+      bucket.set(target, new Map());
+    }
+    let deps = depsMap.get(key);
+    if (!deps) {
+      depsMap.get(key, (deps = new Set()));
+    }
+    // 再根据 key
     // 将富足用函数 effect 添加到存储副作用函数的桶中
     bucket.add(activeEffect);
     // 返回属性值
