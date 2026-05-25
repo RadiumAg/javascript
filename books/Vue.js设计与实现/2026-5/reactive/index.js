7158ca0d5a34ea84dac7c307853e71f493f8acbc
Radium <2043869032@qq.com>
2026-05-25 11:08:58 +0800

    refactor: add tracking and triggering functions for reactive object properties
    Co-developed-by: Aone Copilot <noreply@alibaba-inc.com>

--- books/Vue.js设计与实现/2026-5/reactive/index.js
+++ books/Vue.js设计与实现/2026-5/reactive/index.js
@@ -10,36 +10,47 @@ const obj = new Proxy(data, {
   // 拦截读取操作
   get(target, key) {
     // 没有 activeEffect，直接 return
-    if (!activeEffect) return target[key];
-    // 根据 target 从桶中取得 depsMap，depsMap 是 key 到副作用函数集合的映射
-    let depsMap = bucket.get(target);
-    if (!depsMap) {
-      // 如果 deps 不存在，那么新建一个 Map 与target关联
-      bucket.set(target, new Map());
-    }
-    let deps = depsMap.get(key);
-    if (!deps) {
-      depsMap.get(key, (deps = new Set()));
-    }
-
-    // 最后将当前激活的副作用函数添加到桶中
-    deps.add(activeEffect);
-
+    track(target, key);
     // 返回属性值
     return target[key];
   },
   // 拦截设置操作
   set(target, key, newVal) {
-    // 设置属性值
-    target[key] = newVal;
-    // 根据 target 从桶中取得depsMap，它是key --> effects
-    const depsMap = bucket.get(target);
-    // 根据key取得所有副作用函数 effects
-    const effects = depsMap.get(key);
+    trigger(target, key);
     // 执行副作用函数
     effects && effects.forEach((fn) => fn());
   },
 });
+
+/**
+ * 追踪
+ * @param {*} target
+ * @param {*} key
+ * @return {*}
+ */
+function track(target, key) {
+  if (!activeEffect) return target[key];
+  // 根据 target 从桶中取得 depsMap，depsMap 是 key 到副作用函数集合的映射
+  let depsMap = bucket.get(target);
+  if (!depsMap) {
+    // 如果 deps 不存在，那么新建一个 Map 与target关联
+    bucket.set(target, new Map());
+  }
+  let deps = depsMap.get(key);
+  if (!deps) {
+    depsMap.get(key, (deps = new Set()));
+  }
+
+  // 最后将当前激活的副作用函数添加到桶中
+  deps.add(activeEffect);
+}
+
+function trigger(target, key) {
+  const depMaps = bucket.get(target);
+  if (!depMaps) return;
+  const effects = depMaps.get(key);
+  effects && effects.forEach((fn) => fn());
+}
 
 function effect(fn) {
   activeEffect = fn;
