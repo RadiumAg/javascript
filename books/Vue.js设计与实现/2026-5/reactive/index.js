0b3acef4c0547b6009f8437fd3714ef006d669ca
Radium <2043869032@qq.com>
2026-05-25 13:57:22 +0800

    refactor: comment out and modify effect function for testing purposes
    Co-developed-by: Aone Copilot <noreply@alibaba-inc.com>

--- books/Vue.js设计与实现/2026-5/reactive/index.js
+++ books/Vue.js设计与实现/2026-5/reactive/index.js
@@ -77,12 +77,33 @@ function effect(fn) {
   effectFn();
 }
 
-effect(() => {
-  console.log('effect run');
-  document.body.innerText = obj.ok ? obj.text : 'not';
+// test
+// effect(() => {
+//   console.log('effect run');
+//   document.body.innerText = obj.ok ? obj.text : 'not';
+// });
+
+// setTimeout(() => {
+//   obj.ok = false;
+//   obj.text = '1';
+// }, 1000);
+
+// 全局变量
+let temp1, temp2;
+
+// effectFn1 嵌套了 effectFn2
+effect(function effectFn1() {
+  console.log('effectFn1 执行');
+
+  effect(function effectFn2() {
+    console.log('effectFn2 执行');
+    // 在 effectFn2 中读取 obj.bar 属性
+    temp2 = obj.bar;
+  });
+  // 在 effectFn1 中读取 obj.foo 属性
+  temp1 = obj.foo;
 });
 
 setTimeout(() => {
-  obj.ok = false;
-  obj.text = '1';
+  obj.foo = 1;
 }, 1000);
