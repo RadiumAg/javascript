e5a4cc2b89da55039602c84d767eeb7b5aae5cb6
Radium <2043869032@qq.com>
2026-08-06 17:36:17 +0800

    feat(node-proxy): add http-proxy-middleware config with CORS
    Replace inline proxy setup with detailed middleware configuration
    including custom headers, CORS handling, and request logging.

---
+++ books/JavaScript高级程序设计第五版/对象、类与面向对象编程/index.ts
@@ -0,0 +1,30 @@
+// 属性描述符
+(() => {
+  // writeable: 控制可修改性
+  (() => {
+    const persion = {};
+    Object.defineProperty(persion, 'name', {
+      writable: false,
+      value: 'Alice',
+    });
+    console.log(persion.name);
+    persion.name = 'Greg';
+    console.log(persion.name);
+  })();
+
+  // configurable 不可修改
+  (() => {
+    (() => {
+      const persion = {};
+      Object.defineProperty(persion, 'name', {
+        configurable: false,
+        value: 'Alice',
+      });
+
+      Object.defineProperty(persion, 'name', {
+        configurable: false,
+        value: 'Alice',
+      });
+    })();
+  })();
+})();
--- node-proxy.js
+++ node-proxy.js
@@ -88,3 +88,93 @@ app.use('/metatag', proxyMiddleware);
 app.use('/permissionAccess', proxyMiddleware1);
 
 app.listen(4000);
+const express = require('express');
+const {
+  createProxyMiddleware,
+  loggerPlugin,
+} = require('http-proxy-middleware');
+
+// process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
+const app = express();
+
+const proxyMiddleware = createProxyMiddleware({
+  target: 'https://pre-seoadmin-arise.alibaba-inc.com/metatag',
+  changeOrigin: true,
+  cookiePathRewrite: true,
+  cookieDomainRewrite: true,
+  // headers: {
+  //   host: 'seoadmin-arise.alibaba-inc.com',
+  //   Origin: 'https://seoadmin-arise.alibaba-inc.com',
+  //   Referer: 'https://seoadmin-arise.alibaba-inc.com',
+  // },
+  on: {
+    proxyReq: (proxyReq, req, res) => {
+      /* handle proxyReq */
+      proxyReq.setHeader('Host', 'pre-seoadmin-arise.alibaba-inc.com');
+      proxyReq.setHeader('Origin', 'https://workstation.miravia.net');
+      proxyReq.setHeader('Referer', 'https://workstation.miravia.net');
+
+      req.headers.host = 'pre-seoadmin-arise.alibaba-inc.com';
+      req.headers.origin = 'https://workstation.miravia.net';
+      req.headers.referer = 'https://workstation.miravia.net';
+    },
+    proxyRes: (proxyRes, req, res) => {
+      /* handle proxyRes */
+      proxyRes.headers['access-control-allow-origin'] = '*';
+      res.headers = {
+        'access-control-allow-origin': '*',
+      };
+    },
+    error: (err, req, res) => {
+      console.log(err);
+      /* handle error */
+    },
+  },
+  logger: console,
+  plugins: [loggerPlugin],
+  secure: false,
+});
+
+const proxyMiddleware1 = createProxyMiddleware({
+  target: 'https://seoadmin-arise.alibaba-inc.com/permissionAccess',
+  changeOrigin: true,
+  cookiePathRewrite: true,
+  cookieDomainRewrite: true,
+  // headers: {
+  //   host: 'seoadmin-arise.alibaba-inc.com',
+  //   Origin: 'https://seoadmin-arise.alibaba-inc.com',
+  //   Referer: 'https://seoadmin-arise.alibaba-inc.com',
+  // },
+  on: {
+    proxyReq: (proxyReq, req, res) => {
+      /* handle proxyReq */
+      proxyReq.setHeader('Host', 'seoadmin-arise.alibaba-inc.com');
+      proxyReq.setHeader('Origin', 'https://workstation.miravia.net');
+      proxyReq.setHeader('Referer', 'https://workstation.miravia.net');
+
+      req.headers.host = 'seoadmin-arise.alibaba-inc.com';
+      req.headers.origin = 'https://workstation.miravia.net';
+      req.headers.referer = 'https://workstation.miravia.net';
+    },
+    proxyRes: (proxyRes, req, res) => {
+      /* handle proxyRes */
+      proxyRes.headers['access-control-allow-origin'] = '*';
+      res.headers = {
+        'access-control-allow-origin': '*',
+      };
+    },
+    error: (err, req, res) => {
+      console.log(err);
+      /* handle error */
+    },
+  },
+  logger: console,
+  plugins: [loggerPlugin],
+  secure: false,
+});
+
+app.use('/metatag', proxyMiddleware);
+app.use('/permissionAccess', proxyMiddleware1);
+
+app.listen(4000);
