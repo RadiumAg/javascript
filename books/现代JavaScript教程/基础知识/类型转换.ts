9d6f48c055c318ac72ac8a43e0fb353a3b9a0d22
Radium <2043869032@qq.com>
2026-08-19 15:10:13 +0800

    docs: update type conversion examples

--- books/现代JavaScript教程/基础知识/类型转换.ts
+++ books/现代JavaScript教程/基础知识/类型转换.ts
@@ -8,7 +8,19 @@
 })();
 
 // 数字型转换
+// number转换规则
+// undefined NaN
+// null 0
+// true or false 0 /1
+// 去掉首尾字符再转换，失败就是NaN
 (() => {
   console.log('6' / '3'); // 在算数和表达式中，会自动进行number转换
   const str = '123';
+  const num = Number(str);
+  console.log(Number(undefined)); // NaN
+  console.log(Number(true)); // 1
+  console.log(Number(false)); // 0
+  console.log(Number(null)); // 0
+  console.log('aaaaaa'); // NaN
+  console.log(' 1231232 '); // 1231232
 })();
