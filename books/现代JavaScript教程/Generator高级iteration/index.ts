7d5763aa23cad5ddc7d4451df2f65c550e0cd653
RadiumAg <2043869032@qq.com>
2026-09-05 22:57:51 +0800

    docs: add async iterator example in Generator tutorial

--- books/现代JavaScript教程/Generator高级iteration/index.ts
+++ books/现代JavaScript教程/Generator高级iteration/index.ts
@@ -15,7 +15,7 @@ import { triggerAsyncId } from 'async_hooks';
 };
 
 // 异步迭代和generator
-(() => {
+() => {
   let range = {
     from: 1,
     to: 5,
@@ -37,4 +37,29 @@ import { triggerAsyncId } from 'async_hooks';
   for (let value of range) {
     console.log(value);
   }
+};
+(() => {
+  let range = {
+    from: 1,
+    to: 5,
+    [Symbol.asyncIterator]() {
+      return {
+        current: this.from,
+        last: this.to,
+        async next() {
+          await new Promise((resolve) => setTimeout(resolve, 1000));
+          if (this.current <= this.last) {
+            return { done: false, value: this.current++ };
+          } else {
+            return { done: true };
+          }
+        },
+      };
+    },
+  };
+  (async () => {
+    for await (let value of range) {
+      console.log(value);
+    }
+  })();
 })();
